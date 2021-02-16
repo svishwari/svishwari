fd3eb4c6a1e35aff3664c007afaa332e962cd86b
Rahul Goel <rahgoel@deloitte.com>
2021-02-17 03:16:06 +0530

    CTDataGrid working now.

---
+++ UI/src/components/Table/CTDataGrid.js
@@ -0,0 +1,76 @@
+import React, { Component } from "react";
+import { DataGrid } from "@material-ui/data-grid";
+import IconButton from "@material-ui/core/IconButton";
+import { ReactComponent as StarEmpty } from '../../assets/icons/NotStarred.svg';
+import { ReactComponent as Starred } from '../../assets/icons/Starred.svg';
+import './CTDataGrid.scss'
+
+export default class CTDataGrid extends Component {
+  constructor(props) {
+    super(props);
+    const { data } = props;
+    this.state = {
+      dataGridData: data,
+      isEditing: false,
+    };
+    // this.setState({ dataGridData: this.props.data });
+  }
+  updateItem(id, itemAttributes) {
+    var index = this.state.dataGridData.findIndex((x) => x.id === id);
+    if (index === -1) {
+    } else
+      this.setState({
+        dataGridData: [
+          ...this.state.dataGridData.slice(0, index),
+          Object.assign({}, this.state.dataGridData[index], itemAttributes),
+          ...this.state.dataGridData.slice(index + 1),
+        ],
+      });
+  }
+
+  updateStarring = (params) => {
+    //   console.log(params.row.id)
+    this.updateItem(params.row.id, { starred: !params.row.starred });
+    // this.setState({ dataGridData: this.dataGridData });
+  };
+
+  starredColumn = {
+    field: "starred",
+    headerName: " ",
+    width: 100,
+    renderCell: (params) => {
+      const updateStar = () => {
+        console.log(params);
+        this.updateStarring(params);
+      };
+      return (
+        <IconButton aria-label="starring" size="small" onClick={updateStar}>
+          {params.getValue("starred") ? <Starred/>: <StarEmpty/>}
+        </IconButton>
+      );
+    },
+  };
+  applicableColumns = this.props.hasStarring
+    ? [this.starredColumn, ...this.props.columns]
+    : this.props.columns;
+  render() {
+    return (
+      <>
+        <DataGrid
+          columns={this.applicableColumns}
+          rows={this.state.dataGridData}
+          checkboxSelection={this.isEditing}
+          disableColumnFilter={true}
+          autoHeight={true}
+          disableColumnMenu={true}
+          showColumnRightBorder={false}
+          hideFooterRowCount={true}
+          hideFooter={true}
+          disableSelectionOnClick={true}
+          rowsPerPageOptions={[25]}
+          className="ct-table-wrapper"
+        />
+      </>
+    );
+  }
+}
--- UI/src/modules/dashboard/Dashboard.js
+++ UI/src/modules/dashboard/Dashboard.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { connect } from "react-redux";
+import React, { useState } from "react";
+import { connect, useDispatch } from "react-redux";
 // import CTPrimaryButton from '../../components/Button/CTPrimaryButton';
 // import CTSecondaryButton from '../../components/Button/CTSecondaryButton';
 // import CTTertiaryButton from '../../components/Button/CTTertiaryButton';
@@ -11,139 +11,189 @@ import { connect } from "react-redux";
 // import CTImageCard from '../../components/Cards/ImageCard/CTImageCard';
 // import { ReactComponent as VideoIcon } from "../../assets/icons/video-shooting.svg";
 // import CTSimpleCard from '../../components/Cards/SimpleCard/CTSimpleCard';
-import CTTable from '../../components/Table/CTTable';
 import { ReactComponent as FBIcon } from "../../assets/icons/fb-icon.svg";
+import CTPrimaryButton from "../../components/Button/CTPrimaryButton";
+import CTDataGrid from "../../components/Table/CTDataGrid";
+import { Link } from "react-router-dom";
+import { Button, Chip, IconButton } from "@material-ui/core";
 
 const data = [
-    {
-      name: 'Audience Name',
-      status: 'Delivered.',
-      size: '1.2M',
-      created: '8/23/20 11:59PM',
-      delivered: true,
-      starred: true,
-      draft: false,
-      id: 1
-    },
-    {
-      name: 'Audience Name',
-      status: 'Not Delivered.',
-      size: '1.2M',
-      created: '8/23/20 11:59PM',
-      delivered: false,
-      starred: true,
-      draft: true,
-      id: 2
-    },
-    {
-      name: 'Audience Name',
-      status: 'Not Delivered.',
-      size: '1.2M',
-      created: '8/23/20 11:59PM',
-      delivered: false,
-      starred: true,
-      draft: false,
-      id: 3
-    },
-    {
-      name: 'Audience Name',
-      status: 'Not Delivered.',
-      size: '1.2M',
-      created: '8/23/20 11:59PM',
-      delivered: false,
-      starred: false,
-      draft: false,
-      id: 4
-    },
-    {
-      name: 'Audience Name',
-      status: 'Not Delivered.',
-      size: '1.2M',
-      created: '8/23/20 11:59PM',
-      delivered: false,
-      starred: false,
-      draft: false,
-      id: 5
-    },
-    {
-      name: 'Audience Name',
-      status: 'Delivered.',
-      size: '1.2M',
-      created: '8/23/20 11:59PM',
-      delivered: true,
-      starred: true,
-      draft: false,
-      id: 6
-    },
-    {
-      name: 'Audience Name',
-      status: 'Not Delivered.',
-      size: '1.2M',
-      created: '8/23/20 11:59PM',
-      delivered: false,
-      starred: false,
-      draft: false,
-      id: 7
-    },
-    {
-      name: 'Audience Name',
-      status: 'Delivered.',
-      size: '1.2M',
-      created: '8/23/20 11:59PM',
-      delivered: true,
-      starred: true,
-      draft: false,
-      id: 8
-    },
-    {
-      name: 'Audience Name',
-      status: 'Not Delivered.',
-      size: '1.2M',
-      created: '8/23/20 11:59PM',
-      delivered: false,
-      starred: false,
-      draft: false,
-      id: 9
-    },
-  ];
+  {
+    name: "Audience Name",
+    status: "Delivered.",
+    size: "1.2M",
+    created: "8/23/20 11:59PM",
+    delivered: true,
+    starred: true,
+    draft: false,
+    id: 1,
+  },
+  {
+    name: "Audience Name",
+    status: "Not Delivered.",
+    size: "1.2M",
+    created: "8/23/20 11:59PM",
+    delivered: false,
+    starred: true,
+    draft: true,
+    id: 2,
+  },
+  {
+    name: "Audience Name",
+    status: "Not Delivered.",
+    size: "1.2M",
+    created: "8/23/20 11:59PM",
+    delivered: false,
+    starred: true,
+    draft: false,
+    id: 3,
+  },
+  {
+    name: "Audience Name",
+    status: "Not Delivered.",
+    size: "1.2M",
+    created: "8/23/20 11:59PM",
+    delivered: false,
+    starred: false,
+    draft: false,
+    id: 4,
+  },
+  {
+    name: "Audience Name",
+    status: "Not Delivered.",
+    size: "1.2M",
+    created: "8/23/20 11:59PM",
+    delivered: false,
+    starred: false,
+    draft: false,
+    id: 5,
+  },
+  {
+    name: "Audience Name",
+    status: "Delivered.",
+    size: "1.2M",
+    created: "8/23/20 11:59PM",
+    delivered: true,
+    starred: true,
+    draft: false,
+    id: 6,
+  },
+  {
+    name: "Audience Name",
+    status: "Not Delivered.",
+    size: "1.2M",
+    created: "8/23/20 11:59PM",
+    delivered: false,
+    starred: false,
+    draft: false,
+    id: 7,
+  },
+  {
+    name: "Audience Name",
+    status: "Delivered.",
+    size: "1.2M",
+    created: "8/23/20 11:59PM",
+    delivered: true,
+    starred: true,
+    draft: false,
+    id: 8,
+  },
+  {
+    name: "Audience Name",
+    status: "Not Delivered.",
+    size: "1.2M",
+    created: "8/23/20 11:59PM",
+    delivered: false,
+    starred: false,
+    draft: false,
+    id: 9,
+  },
+];
 
-  const columns = [
-    { 
-      field: 'name', 
-      headerName: 'Name', 
-      flex: 0.1,
-      renderCell: (params) => (
-        <div><FBIcon /> Hello world</div>
-      )
-    },
-    { 
-      field: 'status', 
-      headerName: 'Status',
-    }, 
-    { field: 'size', headerName: 'Size' },
-    { field: 'created', headerName: 'Created' },
-    { 
-      field: 'open', 
-      headerName: ' ', 
-    },
-    { 
-      field: 'extra', 
-      headerName: ' ', 
-    },
+const columns = [
+  {
+    field: "name",
+    headerName: "Name",
+    width: 300,
+    renderCell: (params) => <Link to="#">{params.getValue("name")}</Link>,
+  },
+  {
+    field: "status",
+    headerName: "Status",
+    width: 300,
+    renderCell: (params) => (
+      <>
+        <Chip
+          label={params.getValue("status")}
+          className={params.getValue("status")}
+        />
+        {params.getValue("delivered") === true ? (
+          <IconButton>
+            <span
+              className="iconify"
+              data-icon="mdi:refresh-circle"
+              data-inline="false"
+            ></span>
+          </IconButton>
+        ) : (
+          <IconButton>
+            <span
+              className="iconify"
+              data-icon="mdi:arrow-right-circle"
+              data-inline="false"
+            ></span>
+          </IconButton>
+        )}
+      </>
+    ),
+  },
+  { field: "size", headerName: "Size", width: 100 },
+  { field: "created", headerName: "Created", width: 300 },
+  {
+    field: "delivered",
+    headerName: " ",
+    width: 150,
+    renderCell: (params) => (
+      <Button color="primary">
+        Open In
+        <span
+          className="iconify"
+          data-icon="ic-baseline-arrow-drop-down"
+          data-inline="false"
+          size="large"
+        ></span>
+      </Button>
+    ),
+  },
+  {
+    field: "draft",
+    headerName: " ",
+    width: 50,
+    renderCell: (params) => (
+      <>
+        <IconButton>
+          <span
+            className="iconify"
+            data-icon="mdi:dots-vertical"
+            data-inline="false"
+          ></span>
+        </IconButton>
+      </>
+    ),
+  },
 ];
 
 const Dashboard = (props) => {
-
-    const handleDataChange = ()=> {
-        data[3].starred = true;
-        console.log(data[3].starred);
-    }
-
-    return (
-        <div>
-            <h1>StyleGuide</h1>
-            {/* <button onClick={() => retrieveMetrics() } >Click Me</button> */}
-            {/* <div className="btnExamples">
+  const handleDataChange = () => {
+    data[3].starred = true;
+    console.log(data[3].starred);
+  };
+  const [dataState, setDataState] = useState(data);
+  return (
+    <div>
+      <h1>StyleGuide</h1>
+      {/* <button onClick={() => retrieveMetrics() } >Click Me</button> */}
+      {/* <div className="btnExamples">
                 <h2>Buttons</h2>
                 <div className='mb-2'>
                 <CTPrimaryButton>Primary</CTPrimaryButton>
@@ -184,16 +234,28 @@ const Dashboard = (props) => {
             <h2>Slider</h2>
             <CTSlider />
             </div> */}
-            <div>
-                <h2>Table</h2>
-                <CTTable data={data} columns={columns}></CTTable>
-            </div>
-        </div>
-    )
-}
-const mapStateToProps = state => {
-    return {
-        posts: state.dashboardReducer.user || []
-    };
+      <div>
+        <h2>Table</h2>
+        <CTDataGrid
+          data={dataState}
+          columns={columns}
+          hasStarring={true}
+        ></CTDataGrid>
+        <CTPrimaryButton
+          onClick={() => {
+            console.log(dataState)
+            dataState.pop();
+            console.log(dataState)
+            setDataState(dataState);
+          }}
+        />
+      </div>
+    </div>
+  );
+};
+const mapStateToProps = (state) => {
+  return {
+    posts: state.dashboardReducer.user || [],
+  };
 };
 export default connect(mapStateToProps)(Dashboard);
