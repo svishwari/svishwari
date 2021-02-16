d787f9c13b14228f507f5233c811adf320492dce
Bansal <mobansal@deloitte.com>
2021-02-16 18:33:46 +0530

    added okta user info

--- UI/src/layouts/topHeader.js
+++ UI/src/layouts/topHeader.js
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { ReactComponent as Collapse } from "../assets/icons/collapse.svg";
 import { UserAvatar } from "../components/UserAvatar";
 import "./topHeader.scss";
+import { useOktaAuth } from "@okta/okta-react";
 
 const TopHeader = ({ isCollapsed, collapsed }) => {
+  const { authState, oktaAuth } = useOktaAuth();
+  const [userInfo, setUserInfo] = useState({name: ''});
+
+  useEffect(() => {
+    if (!authState.isAuthenticated) {
+      // When user isn't authenticated, forget any user info
+      setUserInfo(null);
+    } else {
+      oktaAuth.getUser().then(info => {
+        setUserInfo(info);
+      });
+    }
+  }, [authState, oktaAuth]);
+
   const toggle = () => {
     isCollapsed();
   };
   return (
     <div className="app-header ">
       <Collapse onClick={() => toggle()} className={"trigger " + (collapsed ? 'extra-space' : '')} />
-      <UserAvatar username={"Rahul Goel"}/>
+      <UserAvatar username={userInfo.name}/>
     </div>
   );
 };
