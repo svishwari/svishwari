d8a6c2518577063f4c9c7122eee2b4e155d088b5
Bansal <mobansal@deloitte.com>
2021-02-24 21:20:50 +0530

    removed filter

--- UI/src/components/Filter/CTFilter.js
+++ UI/src/components/Filter/CTFilter.js
@@ -1,77 +1,16 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-import React, { useState } from "react";
+import React from "react";
 import "./CTFilter.scss";
 import CTPopover from "../Popover/CTPopover";
 
 
 // THIS IS A TODO FILE
 // MOST OF THE FUNCTIONALITY STILL NEEDS TO BE CHANGED
-const CTFilter = (props) => {
-
-  const [activeFilters, setActiveFilters] = useState({});
-  const [selectedFilter, setSelectedFilter] = useState("");
-
-  const handleClearAll = () => {
-    setSelectedFilter("");
-    setActiveFilters({});
-
-    props.onClearAll();
-  }
-
-  const toggleFilter = (type, value) => {
-    const _activeFilters = activeFilters;
-
-    // removing a filter
-    if (activeFilters[type] && activeFilters[type].includes(value)) {
-      _activeFilters[type] = _activeFilters[type].filter(f => f !== value)
-
-      if (!_activeFilters[type].length) { delete _activeFilters[type] }
-      setActiveFilters({..._activeFilters})
-    }
-    // adding a filter
-    else {
-      if (!_activeFilters[type]) { _activeFilters[type] = [] }
-    
-      if (props.filterTypes[selectedFilter].selectMultiple === true) {
-        setActiveFilters({...activeFilters, [type]: [..._activeFilters[type], value]})
-      } else {
-        setActiveFilters({...activeFilters, [type]: [value]})
-      }
-    }
-  }
+const CTFilter = () => {
 
   const filterContent = (
     <div>
-      {/* Go up to the filter types list */}
-      <span onClick={() => setSelectedFilter("")}>
-        { selectedFilter && '<' }
-        {props.label.toUpperCase()}
-        {Object.keys(activeFilters).length > 0 && ` (${Object.keys(activeFilters).length})`}
-      </span>
-
-      {/* Clear All */}
-      { Object.keys(activeFilters).length > 0 && <span onClick={handleClearAll}>{props.clearAllLabel}</span> }
-
-      {/* List of filter values for a filter type */}
-      {Object.keys(props.filterTypes).map((type) => (
-        <div>
-          {/* Top-level, showing filter type */}
-          {!selectedFilter && 
-            <div onClick={() => { if (props.disabled !== true) setSelectedFilter(type) }}>
-              {type}
-            </div>
-          }
-        
-          {/* This filter type's values */}
-          {selectedFilter === type && props.filterTypes[type].values.map(value => (
-            <div onClick={() => toggleFilter(type, value)}>
-              {activeFilters[type] && activeFilters[type].includes(value) && <span>Tick</span>}
-              {value}
-            </div>
-          ))
-          }
-        </div>
-      ))}
+      Filter content
     </div>
   );
 
--- UI/src/modules/connections/data-sources/DataSources.js
+++ UI/src/modules/connections/data-sources/DataSources.js
@@ -1,17 +1,12 @@
 import React from "react";
 import { connect, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
-import Select from "@material-ui/core/Select";
 import CTDataGrid from "../../../components/Table/CTDataGrid";
 import {
   fetchDataSources,
   triggerIngestion,
   triggerConnectionCheck,
-  addNewDataSource,
 } from "../store/action";
-import CTModal from "../../../components/Modal/CTModal";
-import CTLabel from "../../../components/Label/CTLabel";
-import CTInput from "../../../components/Input/CTInput";
 import CTChip from "../../../components/Chip/CTChip";
 import "./DataSource.scss";
 
@@ -174,83 +169,6 @@ const DataSources = (props) => {
   React.useEffect(() => {
     retrieveDataSources();
   }, []);
-  const [selectedDataSource, setselectedDataSource] = React.useState(
-    "Amazon S3"
-  );
-  const handleSelectedDataSourceChange = (event) => {
-    setselectedDataSource(event.target.value);
-  };
-  const addDataSourceContent = (
-    <div className="ct-datasource-modal">
-      <CTLabel>Data Source</CTLabel>
-      <Select
-        value={selectedDataSource}
-        onChange={(e) => handleSelectedDataSourceChange(e)}
-        label="Account ID"
-        className="ct-datasource-modal-select"
-      >
-        <option value="Amazon S3">Amazon S3</option>
-        <option value="CDP">CDP</option>
-        <option value="Facebook">Facebook</option>
-      </Select>
-
-      {selectedDataSource === "Amazon S3" ? (
-        <div className="ct-datasource-fields">
-          <span className="ct-datasource-field-card">
-            <CTLabel>IAM User Name</CTLabel>
-            <CTInput placeholder="IAM User Name" />
-          </span>
-          <span className="ct-datasource-field-card">
-            <CTLabel>Password / Key</CTLabel>
-            <CTInput placeholder="Password / Key" />
-          </span>
-          <span className="ct-datasource-field-card">
-            <CTLabel>Filename</CTLabel>
-            <CTInput placeholder="Unique name for your file" />
-          </span>
-          <span className="ct-datasource-field-card">
-            <CTLabel>Filepath</CTLabel>
-            <CTInput placeholder="example.csv" />
-          </span>
-        </div>
-      ) : (
-        ""
-      )}
-      {selectedDataSource === "Facebook" ? (
-        <div className="ct-datasource-fields">
-          <span className="ct-datasource-field-card">
-            <CTLabel>IAM User Name</CTLabel>
-            <CTInput placeholder="IAM User Name" />
-          </span>
-          <span className="ct-datasource-field-card">
-            <CTLabel>Password / Key</CTLabel>
-            <CTInput placeholder="Password / Key" />
-          </span>
-          <span className="ct-datasource-field-card">
-            <CTLabel>Filename</CTLabel>
-            <CTInput placeholder="Unique name for your file" />
-          </span>
-        </div>
-      ) : (
-        ""
-      )}
-      {selectedDataSource === "CDP" ? (
-        <div className="ct-datasource-fields">
-          <span className="ct-datasource-field-card">
-            <CTLabel>IAM User Name</CTLabel>
-            <CTInput placeholder="IAM User Name" />
-          </span>
-          <span className="ct-datasource-field-card">
-            <CTLabel>Password / Key</CTLabel>
-            <CTInput placeholder="Password / Key" />
-          </span>
-        </div>
-      ) : (
-        ""
-      )}
-    </div>
-  );
-  const childRef = React.useRef();
 
   return (
     <>
@@ -259,9 +177,6 @@ const DataSources = (props) => {
         columns={columns}
         hasStarring
         loading={!props.dataSources.length}
-        onAddClick={() => {
-          childRef.current.handleOpen();
-        }}
         pageName="Data Source"
         isTopVisible
         isSummaryEnabled
@@ -279,14 +194,6 @@ const DataSources = (props) => {
           {name: "Configure", function: ()=> {} },
         ]}
         enableMoreIcon
-      />
-      <CTModal
-        ref={childRef}
-        modalTitle="Add Data Source"
-        modalSubtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-        modalBody={addDataSourceContent}
-        mainCTAText="Verify and Add"
-        onComplete={() => dispatch(addNewDataSource())}
       />
     </>
   );
