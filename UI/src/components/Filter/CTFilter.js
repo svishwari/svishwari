dde698041e604f477d57c72a33a276658d189f90
Bansal <mobansal@deloitte.com>
2021-02-24 15:20:48 +0530

    filter base

--- UI/src/components/Filter/CTFilter.js
+++ UI/src/components/Filter/CTFilter.js
@@ -1,16 +1,77 @@
-import React from "react";
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import React, { useState } from "react";
 import "./CTFilter.scss";
 import CTPopover from "../Popover/CTPopover";
 
 
 // THIS IS A TODO FILE
 // MOST OF THE FUNCTIONALITY STILL NEEDS TO BE CHANGED
-const CTFilter = (
-) => {
+const CTFilter = (props) => {
+
+  const [activeFilters, setActiveFilters] = useState({});
+  const [selectedFilter, setSelectedFilter] = useState("");
+
+  const handleClearAll = () => {
+    setSelectedFilter("");
+    setActiveFilters({});
+
+    props.onClearAll();
+  }
+
+  const toggleFilter = (type, value) => {
+    const _activeFilters = activeFilters;
+
+    // removing a filter
+    if (activeFilters[type] && activeFilters[type].includes(value)) {
+      _activeFilters[type] = _activeFilters[type].filter(f => f !== value)
+
+      if (!_activeFilters[type].length) { delete _activeFilters[type] }
+      setActiveFilters({..._activeFilters})
+    }
+    // adding a filter
+    else {
+      if (!_activeFilters[type]) { _activeFilters[type] = [] }
+    
+      if (props.filterTypes[selectedFilter].selectMultiple === true) {
+        setActiveFilters({...activeFilters, [type]: [..._activeFilters[type], value]})
+      } else {
+        setActiveFilters({...activeFilters, [type]: [value]})
+      }
+    }
+  }
 
   const filterContent = (
     <div>
-      Filter content
+      {/* Go up to the filter types list */}
+      <span onClick={() => setSelectedFilter("")}>
+        { selectedFilter && '<' }
+        {props.label.toUpperCase()}
+        {Object.keys(activeFilters).length > 0 && ` (${Object.keys(activeFilters).length})`}
+      </span>
+
+      {/* Clear All */}
+      { Object.keys(activeFilters).length > 0 && <span onClick={handleClearAll}>{props.clearAllLabel}</span> }
+
+      {/* List of filter values for a filter type */}
+      {Object.keys(props.filterTypes).map((type) => (
+        <div>
+          {/* Top-level, showing filter type */}
+          {!selectedFilter && 
+            <div onClick={() => { if (props.disabled !== true) setSelectedFilter(type) }}>
+              {type}
+            </div>
+          }
+        
+          {/* This filter type's values */}
+          {selectedFilter === type && props.filterTypes[type].values.map(value => (
+            <div onClick={() => toggleFilter(type, value)}>
+              {activeFilters[type] && activeFilters[type].includes(value) && <span>Tick</span>}
+              {value}
+            </div>
+          ))
+          }
+        </div>
+      ))}
     </div>
   );
 
@@ -24,3 +85,10 @@ const CTFilter = (
 };
 
 export default CTFilter;
+
+CTFilter.defaultProps = {
+  open: true,
+  disabled: false,
+  label: 'Filters',
+  clearAllLabel: 'Clear All',
+}
