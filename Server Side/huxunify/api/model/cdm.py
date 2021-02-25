cdf0a00c84221721cc2a500b8d5a41c008707597
seahawks8 <douglaslong11@gmail.com>
2021-02-24 21:26:41 -0700

    added cdm call for getting ingested data

--- Server Side/huxunify/api/model/cdm.py
+++ Server Side/huxunify/api/model/cdm.py
@@ -1,6 +1,8 @@
 """
 The purpose of this file is for housing the Customer Data Management (CDM) related API models
 """
+import logging
+from api.data_connectors.snowflake_client import SnowflakeClient, PROCESSED_DATABASE
 
 
 class CdmModel:
@@ -9,3 +11,39 @@ class CdmModel:
     """
     def __init__(self):
         self.message = "Hello cdm"
+        self.db = SnowflakeClient(username='', password='')
+        self.ctx = self.db.connect()
+
+    def get_data_sources(self):
+        """A function to get all CDM processed files.
+        Args:
+            client_table: name of the snowflake client table
+        Returns:
+            list(dict): processed client data sources
+        """
+        data_sources = []
+        if self.ctx is None:
+            return data_sources, 200
+
+        # setup the cursor object
+        cs = self.ctx.cursor()
+
+        try:
+            # execute the fetch all query
+            cs.execute(f"""
+                select data_source, filename, count(*) as record_count
+                from {PROCESSED_DATABASE}.LTD.NETSUITE_ITEMS_205FD81AFAAB9B858EDA8E503BE224AC_LTD
+                group by data_source, filename 
+            """)
+            results = cs.fetchall()
+
+            # port the data back into the list
+            data_sources = [{'src': rec[0], 'filename': rec[1], 'record_count': rec[2]}
+                            for rec in results]
+        finally:
+            cs.close()
+        return data_sources
+
+
+if __name__ == '__main__':
+    pass
--- Server Side/huxunify/api/route/cdm.py
+++ Server Side/huxunify/api/route/cdm.py
@@ -1,6 +1,7 @@
 """
 purpose of this script is for housing the cdm routes for the API
 """
+import json
 from http import HTTPStatus
 from flask import Blueprint
 from flasgger import swag_from
@@ -12,7 +13,7 @@ cdm_bp = Blueprint('cdm_bp', __name__)
 
 @cdm_bp.route('/')
 @swag_from({
-    "tags": ["customer data"],
+    "tags": ["cdm"],
     'responses': {
         HTTPStatus.OK.value: {
             'description': 'cdm api',
@@ -27,3 +28,25 @@ def index():
     """
     result = CdmModel()
     return CdmSchema().dump(result), 200
+
+
+@cdm_bp.route('/ingested_data', methods=['get'])
+@swag_from({
+    "parameters": [],
+    "tags": ["cdm"],
+    'responses': {
+        HTTPStatus.OK.value: {
+            'description': 'list all ingested data',
+        }
+    }
+})
+def get_ingested_data():
+    """
+    list all ingested data, record count and blob path
+
+    Args:
+    Returns:
+        The return list of ingested data from the snowflake database API
+
+    """
+    return json.dumps(CdmModel().get_data_sources()), 200
