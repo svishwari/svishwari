07afb8b89a766c68a9331e04f408782c0fdcd9c8
Henrik Rypkema <HenrikR@users.noreply.github.com>
2021-03-04 18:50:13 -0500

    Make it configurable.

--- UI/src/modules/auth/AuthConfig.js
+++ UI/src/modules/auth/AuthConfig.js
@@ -1,15 +1,17 @@
+const getenv = (name) => process.env[name];
+
 const oktaAuthConfig = {
   // Note: If your app is configured to use the Implicit Flow
   // instead of the Authorization Code with Proof of Code Key Exchange (PKCE)
   // you will need to add pkce: false
-  issuer: "https://dev-7152793.okta.com/oauth2/default",
-  clientId: "0oa1h4ueql60Xc4Dk5d6",
+  issuer: `${getenv("REACT_APP_OKTA_ISSUER")}/oauth2/default`,
+  clientId: getenv("REACT_APP_OKTA_CLIENT_ID"),
   redirectUri: `${window.location.origin}/login/callback`,
 };
 
 const oktaSignInConfig = {
-  baseUrl: "https://dev-7152793.okta.com",
-  clientId: "0oa1h4ueql60Xc4Dk5d6",
+  baseUrl: getenv("REACT_APP_OKTA_ISSUER"),
+  clientId: getenv("REACT_APP_OKTA_CLIENT_ID"),
   redirectUri: `${window.location.origin}/login/callback`,
   authParams: {
     // If your app is configured to use the Implicit Flow
