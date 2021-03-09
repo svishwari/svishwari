43c67a6aba4b8cf4dc78d822af1c6767ef291e75
Henrik Rypkema <HenrikR@users.noreply.github.com>
2021-03-09 11:39:45 -0500

    Use teh orchestration reducer.

--- UI/src/modules/orchestration/Audiences/Audiences.js
+++ UI/src/modules/orchestration/Audiences/Audiences.js
@@ -1,51 +1,36 @@
 import React from 'react';
 import { connect, useDispatch } from 'react-redux';
-import { Typography } from '@material-ui/core';
 import CTDataGrid from '../../../components/Table/CTDataGrid';
-import { fetchAudiences } from './store/action';
+import { fetchAudiences } from '../store/action';
 
 const Audiences = (props) => {
   const dispatch = useDispatch();
 
-  const getAudiences = () => {
-    if (props.audiences.audiences.length > 0) {
-      return props.audiences.audiences;
-    }
-    return [];
-  };
-
   const columns = [
-    { field: 'audience_name', headerName: 'Name', flex: 0.3 },
+    { field: 'audience_name', headerName: 'Audience Name', flex: 0.3 },
     { field: 'updated', headerName: 'Last Updated', flex: 0.3 },
   ];
 
   React.useEffect(() => {
     dispatch(fetchAudiences());
   }, []);
+
   return (
-    <div className="dashboard-wrapper">
-      <div className="content-wrapper">
-        <Typography variant="h5" className="recent-heading">
-          Audiences
-        </Typography>
-        <CTDataGrid
-          autoHeight
-          hasStarring
-          isTopVisible={false}
-          hideFooterPagination
-          showTopBar={false}
-          editing={false}
-          columns={columns}
-          data={getAudiences()}
-        />
-      </div>
-    </div>
+    <CTDataGrid
+      data={props.audiences}
+      columns={columns}
+      hasStarring
+      loading={!props.audiences.length}
+      pageName="Audiences"
+      autoHeight
+      isTopVisible={false}
+      hideFooterPagination
+    />
   );
 };
 
 const mapStateToProps = (state) => ({
-  userInfo: state.user.loggedInUser || [],
-  audiences: state.audiences,
+    audiences: state.orchestration.audiences || [],
 });
 
 export default connect(mapStateToProps)(Audiences);
