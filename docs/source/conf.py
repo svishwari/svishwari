03696d313c205f783da22b810ef39402f2b33250
Raj K <rakommaraju@deloitte.com>
2021-03-30 10:48:17 -0400

    removed tests from documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -53,7 +53,7 @@ templates_path = ["_templates"]
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = [".*", "_*", "Thumbs.db", ".DS_Store", "README.rst"]
+exclude_patterns = [".*", "_*", "Thumbs.db", ".DS_Store", "README.rst", "*test*"]
 
 
 # -- Options for HTML output -------------------------------------------------
--- docs/source/huxunify.rst
+++ docs/source/huxunify.rst
@@ -8,7 +8,6 @@ Subpackages
    :maxdepth: 4
 
    huxunify.api
-   huxunify.test
 
 Submodules
 ----------
