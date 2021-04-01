8de7baf0b7d0537c159d40a67fce8ec1a468d7e1
McMahon <jimcmahon@deloitte.com>
2021-04-01 14:30:39 -0500

    dockerfile added

--- hux/api/huxunify/test/data_connectors/test_snowflake_client.py
+++ hux/api/huxunify/test/data_connectors/test_snowflake_client.py
@@ -2,9 +2,9 @@
 purpose of this file is testing the snowflake client
 """
 from unittest import TestCase
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 from snowflake import connector
-from huxunify.api.data_connectors.snowflake_client import SnowflakeClient
+from api.data_connectors.snowflake_client import SnowflakeClient
 
 ACCOUNT = "TEST_ACCOUNT"
 WAREHOUSE = "TEST_WAREHOUSE"
@@ -18,32 +18,43 @@ class TestSnowflakeClient(TestCase):
     """
 
     def setUp(self) -> None:
-        """
-        Setup initial test client
+        """Setup initial test client
+        Args:
+
+        Returns:
+            None
         """
         self.client = SnowflakeClient(
             account=ACCOUNT, warehouse=WAREHOUSE, username=USER, password=PASSWORD
         )
 
     @patch("snowflake.connector.connect")
-    def test_connection_good(self, mock_connect):
-        """
-        Tests a good connection to snowflake database
+    def test_connection_good(self, mock_connect: MagicMock) -> None:
+        """Tests a good connection to snowflake database
+        Args:
+            mock_connect
+        Returns:
+            None
         """
         self.client.connect()
         mock_connect.assert_called_once_with(
             account=ACCOUNT, warehouse=WAREHOUSE, user=USER, password=PASSWORD
         )
 
-    @patch(
-        "snowflake.connector.connect",
-        side_effect=connector.errors.DatabaseError("DB ERROR"),
-    )
-    def test_connection_bad(self, mock_connect):
+    @patch("snowflake.connector.connect")
+    def test_connection_bad(self, mock_connect: MagicMock) -> None:
+        """Tests a bad connection to snowflake database
+        Args:
+            mock_connect
+        Returns:
+            None
         """
-        Tests a bad connection to snowflake database
-        """
-        self.assertRaises(connector.errors.DatabaseError, self.client.connect())
+        # apply side effect
+        connector.connect.side_effect = connector.errors.DatabaseError("DB ERROR")
+
+        # test that base exception is raised
+        with self.assertRaises(BaseException):
+            self.client.connect()
         mock_connect.assert_called_once_with(
             account=ACCOUNT, warehouse=WAREHOUSE, user=USER, password=PASSWORD
         )
