24c5ecbd64ee75698979b8fc461ebbc7ee75f8db
Henrik Rypkema <henrikr@users.noreply.github.com>
2021-04-13 17:58:38 -0400

    Add theme stuff.

--- hux/frontend/src/plugins/vuetify.js
+++ hux/frontend/src/plugins/vuetify.js
@@ -1,20 +1,11 @@
 import Vue from "vue"
 import Vuetify from "vuetify/lib/framework"
+import lightTheme from "./theme"
 
 Vue.use(Vuetify)
 
 export default new Vuetify({
   theme: {
-    themes: {
-      light: {
-        primary: "#005587",
-        secondary: "#00A3E0",
-        accent: "#82B1FF",
-        error: "#DA291C",
-        info: "#0076A8",
-        success: "#009A44",
-        warning: "#FFCD00",
-      },
-    },
+    themes: { light: lightTheme },
   },
 })
