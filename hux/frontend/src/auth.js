b1f184df48ab12cafbaffdae3946ff3f9648b273
Rahul Goel <rahgoel@deloitte.com>
2021-04-13 22:51:10 +0530

    small minor correction

--- hux/frontend/src/auth.js
+++ hux/frontend/src/auth.js
@@ -1,5 +1,5 @@
 import { OktaAuth } from "@okta/okta-auth-js";
-import config from "./config";
+const config = require("@/config");
 
 const authClient = new OktaAuth({ issuer: config.default.oidc.issuer });
 
