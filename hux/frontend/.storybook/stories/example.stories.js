fa514de58df189603df1385517153e87b4638187
Abdullah-Azad <mdazad@deloitte.com>
2021-04-13 20:40:32 +0530

    Added vue-cli-plugin-vuetify-storybook

--- hux/frontend/.storybook/stories/example.stories.js
+++ hux/frontend/.storybook/stories/example.stories.js
@@ -4,11 +4,10 @@ import { text, boolean } from '@storybook/addon-knobs'
 
 export default { title: 'BaseCard' }
 
-function genComponent (name) {
+function genComponent(name) {
   return {
     name,
-
-    render (h) {
+    render(h) {
       return h('div', this.$slots.default)
     },
   }
@@ -20,41 +19,22 @@ const story = storyFactory({
 })
 
 export const asDefault = () => story({
-  props: {
-    actions: {
-      default: boolean('Actions', false),
-    },
-    cardText: {
-      default: text('Card text', 'Sed augue ipsum, egestas nec, vestibulum et, malesuada adipiscing, dui. Donec sodales sagittis magna. Vestibulum dapibus nunc ac augue. Donec sodales sagittis magna. Duis vel nibh at velit scelerisque suscipit.'),
-    },
-    divider: {
-      default: boolean('Divider', false),
-    },
-    text: {
-      default: boolean('Text', true),
-    },
-    title: {
-      default: boolean('Show title', true),
-    },
-    titleText: {
-      default: text('Title text', 'Card title'),
-    },
-  },
   template: `
-    <base-card>
-      <v-card-title v-if="title">{{ titleText }}</v-card-title>
-
-      <v-card-text v-if="text">{{ cardText }}</v-card-text>
-
-      <v-divider v-if="divider"></v-divider>
-
-      <v-card-actions v-if="actions">
-        <v-btn text>Cancel</v-btn>
-
-        <v-spacer></v-spacer>
-
-        <base-btn depressed>Accept</base-btn>
-      </v-card-actions>
-    </base-card>
+    <template>
+      <v-row align="center" justify="space-around">
+        <v-btn depressed>
+          Normal
+        </v-btn>
+        <v-btn depressed color="primary" >
+          Primary
+        </v-btn>
+        <v-btn depressed color="error" >
+          Error
+        </v-btn>
+        <v-btn depressed disabled >
+          Disabled
+        </v-btn>
+      </v-row>
+    </template>
   `,
 })
