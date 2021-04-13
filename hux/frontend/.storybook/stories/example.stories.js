5000516c0b1327d950bbd85cfd4ef92532a269c3
Abdullah-Azad <mdazad@deloitte.com>
2021-04-13 20:59:53 +0530

    Added props at button

--- hux/frontend/.storybook/stories/example.stories.js
+++ hux/frontend/.storybook/stories/example.stories.js
@@ -2,12 +2,13 @@
 import { storyFactory } from '../util/helpers'
 import { text, boolean } from '@storybook/addon-knobs'
 
-export default { title: 'BaseCard' }
+export default { title: 'Button' }
 
-function genComponent(name) {
+function genComponent (name) {
   return {
     name,
-    render(h) {
+
+    render (h) {
       return h('div', this.$slots.default)
     },
   }
@@ -15,26 +16,52 @@ function genComponent(name) {
 
 const story = storyFactory({
   BaseBtn: genComponent('BaseBtn'),
-  BaseCard: genComponent('BaseCard'),
+  Button: genComponent('Button'),
 })
 
 export const asDefault = () => story({
+  props: {
+    normalButtonText: {
+      default: text('Normal button text', 'Normal'),
+    },
+    primaryButtonText: {
+      default: text('Primary button text', 'Primary'),
+    },
+    errorButtonText: {
+      default: text('Error button text', 'Error'),
+    },
+    disabledButtonText: {
+      default: text('Disable button text', 'Disabled'),
+    },
+  },
   template: `
-    <template>
-      <v-row align="center" justify="space-around">
-        <v-btn depressed>
-          Normal
-        </v-btn>
-        <v-btn depressed color="primary" >
-          Primary
-        </v-btn>
-        <v-btn depressed color="error" >
-          Error
-        </v-btn>
-        <v-btn depressed disabled >
-          Disabled
-        </v-btn>
-      </v-row>
-    </template>
+  <template>
+  <v-row
+    align="center"
+    justify="space-around"
+  >
+    <v-btn depressed>
+      {{normalButtonText}}
+    </v-btn>
+    <v-btn
+      depressed
+      color="primary"
+    >
+    {{primaryButtonText}}
+    </v-btn>
+    <v-btn
+      depressed
+      color="error"
+    >
+    {{errorButtonText}}
+    </v-btn>
+    <v-btn
+      depressed
+      disabled
+    >
+    {{disabledButtonText}}
+    </v-btn>
+  </v-row>
+</template>
   `,
 })
