643896705fdf7bf031c4bdfdb0b137e9e51e5b48
Bansal <mobansal@deloitte.com>
2021-04-14 15:13:11 +0530

    fixed linting errors

--- hux/frontend/src/components/SideMenu.vue
+++ hux/frontend/src/components/SideMenu.vue
@@ -103,109 +103,109 @@ export default {
 </script>
 
 <style lang="scss">
-  .logo-holder {
-    height: 105px;
-  }
-  .v-navigation-drawer {
-    width: 220px;
-  }
-  .theme--light.v-navigation-drawer {
-    background-color: #005587 !important;
-  }
-  .v-list-item__title {
-    font-size: 13px;
-    font-weight: 600;
-    font-family: Open Sans;
-    font-style: normal;
-    font-weight: 600;
-    line-height: 22px;
-    letter-spacing: 0px;
-    text-align: left;
-  }
-  div.v-list-item__title.primary--text, i.v-icon.notranslate.mdi {
-    color: #ffffff !important;
-    caret-color: #ffffff !important;
-  }
-  .v-list .v-list-item--active {
-    color: inherit;
-  }
-  div .v-list .v-list-item--active::before {
-    background-color: #A0DCFF !important;
-    opacity: 0.2 !important;
-  }
-  .v-navigation-drawer .v-list {
-    border-top: 1px solid rgba(255, 255, 255, 0.1);
-    padding-bottom: 10px;
-  }
+.logo-holder {
+  height: 105px;
+}
+.v-navigation-drawer {
+  width: 220px;
+}
+.theme--light.v-navigation-drawer {
+  background-color: #005587 !important;
+}
+.v-list-item__title {
+  font-size: 13px;
+  font-weight: 600;
+  font-family: Open Sans;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 22px;
+  letter-spacing: 0px;
+  text-align: left;
+}
+div.v-list-item__title.primary--text,
+i.v-icon.notranslate.mdi {
+  color: #ffffff !important;
+  caret-color: #ffffff !important;
+}
+.v-list .v-list-item--active {
+  color: inherit;
+}
+div .v-list .v-list-item--active::before {
+  background-color: #a0dcff !important;
+  opacity: 0.2 !important;
+}
+.v-navigation-drawer .v-list {
+  border-top: 1px solid rgba(255, 255, 255, 0.1);
+  padding-bottom: 10px;
+}
 
-  div.v-list-item__icon.v-list-group__header__prepend-icon {
-    margin-right: 8.75px !important;
-  }
-  .list-group {
-    margin-left: 17px;
-    height: 40px;
-    width: 199px;
-    left: 17px;
-    top: 218px;
-    font-family: Open Sans;
-    font-style: normal;
-    font-weight: 600;
-    font-size: 12px;
-    line-height: 16px;
-    display: flex;
-    align-items: center;
-    color: #FFFFFF;
-    opacity: 0.5;
-  }
-  .v-list-item.v-list-item--link.theme--light {
-    padding-left: 25px;
-  }
-  .v-navigation-drawer--mini-variant {
-    width: 90px !important;
-  }
-  .v-btn.v-btn--icon.v-btn--round.theme--light.v-size--small {
-    margin-right: 25px;
-  }
-  .v-list-item__content.text-truncate {
-    width: 148px;
-    margin-left: calc(50% - 148px/2 - 11px);
-    font-family: Open Sans;
-    font-style: normal;
-    font-weight: 600;
-    font-size: 13px;
-    line-height: 22px;
-    display: flex;
-    align-items: center;
-    color: #FFFFFF;
-  }
-  .px-2.v-list-item.v-list-item--link.theme--light{
-    background: rgba(0, 0, 0, 0.25);
-  }
-  .hux_logo {
-    background-image: url("../assets/images/hux_logo_2.svg");
-    width: 55px;
-    height: 55px;
-    margin-top: 17px;
-    margin-left: 17px;
-  }
-  .nav-footer {
-    height: 35px;
-    left: 6.82%;
-    right: 4.55%;
-    bottom: 0px;
-    font-family: Open Sans;
-    font-style: normal;
-    font-weight: 600;
-    font-size: 12px;
-    line-height: 16px;
-    color: #FFFFFF;
-    opacity: 0.8;
-    margin-left: 15px;
-    margin-right: 10px;
-  }
-  .profile-name {
-    background: rgba(0, 0, 0, 0.25);
-  }
+div.v-list-item__icon.v-list-group__header__prepend-icon {
+  margin-right: 8.75px !important;
+}
+.list-group {
+  margin-left: 17px;
+  height: 40px;
+  width: 199px;
+  left: 17px;
+  top: 218px;
+  font-family: Open Sans;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 12px;
+  line-height: 16px;
+  display: flex;
+  align-items: center;
+  color: #ffffff;
+  opacity: 0.5;
+}
+.v-list-item.v-list-item--link.theme--light {
+  padding-left: 25px;
+}
+.v-navigation-drawer--mini-variant {
+  width: 90px !important;
+}
+.v-btn.v-btn--icon.v-btn--round.theme--light.v-size--small {
+  margin-right: 25px;
+}
+.v-list-item__content.text-truncate {
+  width: 148px;
+  margin-left: calc(50% - 148px / 2 - 11px);
+  font-family: Open Sans;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 13px;
+  line-height: 22px;
+  display: flex;
+  align-items: center;
+  color: #ffffff;
+}
+.px-2.v-list-item.v-list-item--link.theme--light {
+  background: rgba(0, 0, 0, 0.25);
+}
+.hux_logo {
+  background-image: url("../assets/images/hux_logo_2.svg");
+  width: 55px;
+  height: 55px;
+  margin-top: 17px;
+  margin-left: 17px;
+}
+.nav-footer {
+  height: 35px;
+  left: 6.82%;
+  right: 4.55%;
+  bottom: 0px;
+  font-family: Open Sans;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 12px;
+  line-height: 16px;
+  color: #ffffff;
+  opacity: 0.8;
+  margin-left: 15px;
+  margin-right: 10px;
+}
+.profile-name {
+  background: rgba(0, 0, 0, 0.25);
 }
 .profile-chevron-icon {
   margin-right: 25px !important;
