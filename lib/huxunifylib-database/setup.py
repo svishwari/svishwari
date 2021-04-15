8bd0bfdae1d0cf323ed5a3b1546dd98a585c9e07
seahawks8 <douglaslong11@gmail.com>
2021-04-15 10:14:23 -0600

    setup.py - moved find into the setup.cfg and tested setup.cfg - added version, setup/test requires

--- lib/huxunifylib-database/setup.py
+++ lib/huxunifylib-database/setup.py
@@ -1,15 +1,13 @@
 """
 database setup code for the huxunify package
 """
-from setuptools import setup, find_namespace_packages
+from setuptools import setup
 
 setup(
-    name="huxunifylib",
     use_scm_version={
         "root": "../..",
         "relative_to": __file__,
         "local_scheme": "node-and-timestamp",
         "fallback_version": "0.1.0",
-    },
-    packages=find_namespace_packages(include=["huxunifylib.*"]),
+    }
 )
