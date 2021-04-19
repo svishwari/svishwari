0fba45a138d1d500454ad43a1431fff8a4531fe3
Bansal <mobansal@deloitte.com>
2021-04-19 17:27:58 +0530

    added initial drawer

---
+++ hux/frontend/src/components/common/Drawer.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-navigation-drawer
+    v-model="drawer"
+    :right="toRight"
+    :width="width"
+    @input="onChange"
+    temporary
+    floating
+    app
+  >
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  name: "drawer",
+
+  data() {
+    return {
+      drawer: this.isOpen,
+    }
+  },
+
+  props: {
+    toRight: {
+      type: Boolean,
+      required: false,
+      default: true,
+    },
+
+    isOpen: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+
+    width: {
+      type: String,
+      required: false,
+      default: "600",
+    },
+  },
+  methods: {
+    onChange(x) {
+      console.log(x)
+    },
+    toggleDrawer() {
+      this.drawer = !this.drawer
+    }
+  },
+}
+</script>
