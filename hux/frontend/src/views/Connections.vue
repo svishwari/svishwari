a5fc219113298b1742f3d1b0dd5e509066400ed2
Abdullah-Azad <mdazad@deloitte.com>
2021-04-20 14:34:34 +0530

    Added column layout

--- hux/frontend/src/views/Connections.vue
+++ hux/frontend/src/views/Connections.vue
@@ -1,5 +1,15 @@
 <template>
-  <div>Connections</div>
+  <!-- <div>Connections</div> -->
+  <v-container class="grey lighten-5">
+    <v-row>
+      <v-col cols="6">
+        <!-- <v-card class="pa-2" outlined tile> .col-6 </v-card> -->
+      </v-col>
+      <v-col cols="6">
+        <!-- <v-card class="pa-2" outlined tile> .col-6 </v-card> -->
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
