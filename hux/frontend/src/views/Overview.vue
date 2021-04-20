734a155599c1fb3ddb83edfe96a9243b77e12c39
Rahul Goel <rahgoel@deloitte.com>
2021-04-20 13:30:09 +0530

    basic layout!

--- hux/frontend/src/views/Overview.vue
+++ hux/frontend/src/views/Overview.vue
@@ -1,10 +1,31 @@
 <template>
-  <div>Overview</div>
+  <PageHeader
+    :title="'Welcome back, ' + firstName + '!'"
+    icon="mdi-bullhorn-outline"
+  >
+    <template slot="description">
+      <div>
+        Hux is here to help you make better, faster decisions to improve your
+        Customer Experiences.
+        <a>Learn More ></a>
+      </div>
+    </template>
+  </PageHeader>
 </template>
 
 <script>
+import PageHeader from "@/components/PageHeader"
+
 export default {
   name: "overview",
+  components: {
+    PageHeader,
+  },
+  computed: {
+    firstName() {
+      return this.$store.getters.getFirstname
+    },
+  },
 }
 </script>
 
