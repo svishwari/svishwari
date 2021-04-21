7d5490fcf9bb1cc04608d29bc91545463b40668b
Bansal <mobansal@deloitte.com>
2021-04-21 16:04:00 +0530

    added breadcrumb

---
+++ hux/frontend/src/components/common/Breadcrumb.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-breadcrumbs :items="items" class="pl-0">
+    <template v-slot:item="{ item }">
+      <v-breadcrumbs-item :href="item.href" :disabled="item.disabled" class="font-weight-light">
+        <v-icon color="black" size="21" class="pr-1">
+          {{ item.icon }}
+        </v-icon>
+        {{ item.text }}
+      </v-breadcrumbs-item>
+    </template>
+    <template v-slot:divider>
+      <v-icon size="21" color="primary">mdi-chevron-right</v-icon>
+    </template>
+  </v-breadcrumbs>
+</template>
+
+<script>
+export default {
+  name: "breadcrumb",
+
+  props: {
+    items: {
+      type: Array,
+      required: true,
+      default: () => [],
+    },
+  },
+}
+</script>
