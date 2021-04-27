9cca311dab20fcda6313af4a2202c766c0876de9
Abdullah-Azad <mdazad@deloitte.com>
2021-04-27 11:20:34 +0530

    added menu in menucell

--- hux/frontend/src/components/common/huxTable/MenuCell.vue
+++ hux/frontend/src/components/common/huxTable/MenuCell.vue
@@ -5,7 +5,16 @@
       <v-spacer></v-spacer>
       <span class="action-icon font-weight-light float-right">
         <v-icon class="mr-2 action-favroite" color="primary" @click="addToFavorite($event)"> mdi-star </v-icon>
-        <v-icon class="mr-2 more-action" color="primary" @click="takeActions($event)"> mdi-dots-vertical </v-icon>
+        <v-menu bottom offset-y>
+          <template v-slot:activator="{ on, attrs }">
+             <v-icon v-bind="attrs" v-on="on" class="mr-2 more-action" color="primary" @click="takeActions($event)"> mdi-dots-vertical </v-icon>
+          </template>
+          <v-list>
+            <v-list-item v-for="(item, index) in items" :key="index">
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
       </span>
     </v-col>
   </v-row>
@@ -17,6 +26,14 @@ export default Vue.extend({
   data() {
     return {
       cellValue: null,
+      items: [
+        { title: 'Export' },
+        { title: 'Edit' },
+        { title: 'Duplicate' },
+        { title: 'Open' },
+        { title: 'Pause delivery' },
+        { title: 'Delete' },
+      ],
     }
   },
   beforeMount() {
