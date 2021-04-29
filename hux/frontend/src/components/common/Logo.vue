2a803d41b5bc1eb50a34802dcc64f2df5cb19d7a
Bansal <mobansal@deloitte.com>
2021-04-29 19:54:11 +0530

    added inital logo support

---
+++ hux/frontend/src/components/common/Logo.vue
@@ -0,0 +1,26 @@
+<template>
+  <component :is="dynamicIcon" :width="size" :height="size"/>
+</template>
+
+<script>
+export default {
+  name: "logo",
+  props: {
+    icon: {
+      type: String,
+      required: true,
+      default: null,
+    },
+    size: {
+      type: String,
+      required: false,
+      default: "18",
+    },
+  },
+  computed: {
+    dynamicIcon() {
+      return () => import(`../../assets/logos/${this.icon}.svg`)
+    },
+  },
+}
+</script>
