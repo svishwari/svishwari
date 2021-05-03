c2397f37ad416e00dc76b0887d6431e2395859df
Abdullah-Azad <mdazad@deloitte.com>
2021-05-03 14:31:24 +0530

    lint fixes

--- hux/frontend/src/components/common/Index.vue
+++ hux/frontend/src/components/common/Index.vue
@@ -138,25 +138,24 @@
 
     <Logo type="tableau"></Logo>
     <Logo type="tableau" :size="48"></Logo>
-    
+
     <Logo type="google-ads"></Logo>
     <Logo type="google-ads" :size="48"></Logo>
 
     <Logo type="google-analytics"></Logo>
     <Logo type="google-analytics" :size="48"></Logo>
 
-    <Logo type="aqfer" ></Logo>
+    <Logo type="aqfer"></Logo>
     <Logo type="aqfer" :size="48"></Logo>
 
-    <Logo type="netsuite" ></Logo>
+    <Logo type="netsuite"></Logo>
     <Logo type="netsuite" :size="48"></Logo>
 
-    <Logo type="salesforce" ></Logo>
+    <Logo type="salesforce"></Logo>
     <Logo type="salesforce" :size="48"></Logo>
 
-    <Logo type="twillio" ></Logo>
+    <Logo type="twillio"></Logo>
     <Logo type="twillio" :size="48"></Logo>
-
   </v-container>
 </template>
 
