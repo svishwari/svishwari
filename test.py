6ad4dd1fadd7a6621667100e2fbce73722eb58da
panch mukesh <ppanchmukesh@deloitte.com>
2021-05-07 19:44:43 +0530

    Fixing errors in config file

---
+++ test.py
@@ -0,0 +1,13 @@
+"""
+This is a sample module which has single function
+"""
+
+
+def home() -> str:
+    """
+    THis function returns the sample string
+    """
+    return "Mukesh"
+
+
+print(home())
