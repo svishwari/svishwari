39bf9cf616a100e2e8262429626eef6af38760e8
Rahul Goel <rahgoel@deloitte.com>
2021-05-24 19:46:49 +0530

    Code Formatting fix!

---
+++ hux/frontend/src/views/Engagements/Index.vue
@@ -0,0 +1,37 @@
+<template>
+  <PageHeader>
+      <template slot="left">
+        <Breadcrumb :items="breadcrumbItems" />
+      </template>
+      <template slot="right">
+      </template>
+  </PageHeader>
+</template>
+
+<script>
+import PageHeader from "../../components/PageHeader.vue"
+export default {
+  name: "engagements",
+  components: {
+    PageHeader,
+  },
+  data() {
+      return {
+          breadcrumbItems: [
+        {
+          text: "Engagements",
+          disabled: true,
+          href: this.$route.path,
+          icon: "audiences",
+        },
+        {
+          text: "Engagements",
+          disabled: true,
+        },
+      ],
+      }
+  },
+}
+</script>
+
+<style lang="sass" scoped></style>
