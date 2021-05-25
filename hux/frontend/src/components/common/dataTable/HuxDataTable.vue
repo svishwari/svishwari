e12e308820cb53f3ceaa919f164c51884ff9b154
Abdullah-Azad <mdazad@deloitte.com>
2021-05-26 00:32:27 +0530

    UI completed for level 1

--- hux/frontend/src/components/common/dataTable/HuxDataTable.vue
+++ hux/frontend/src/components/common/dataTable/HuxDataTable.vue
@@ -6,29 +6,35 @@
       :expanded.sync="expanded"
       show-expand
       item-key="name"
-      :search="search"
       :hide-default-footer="true"
     >
       <template v-slot:item="{ item, expand, isExpanded }">
         <tr>
-          <td>
-            <v-btn @click="expand(!isExpanded)" v-if="item.child">Expand</v-btn>
-          </td>
-          <td
-            class="d-block d-sm-table-cell"
-            v-for="field in Object.keys(item)"
-            :key="field.name"
-          >
+          <td></td>
+          <td v-for="field in Object.keys(item)" :key="field.name">
             <span v-if="field == 'engagementName'" class="primary--text">
-                {{ item[field] }}
+              <v-icon
+                color="primary"
+                :class="{ 'rotate-icon': isExpanded }"
+                @click="expand(!isExpanded)"
+              >
+                mdi-chevron-right
+              </v-icon>
+              {{ item[field] }}
             </span>
             <span v-else-if="field == 'status'">
-                <v-icon v-if="item[field] == 'Active'" class="material-icons delivered">
-                    mdi-checkbox-blank-circle
-                </v-icon>
-                <v-icon v-if="item[field] == 'Delivering'" class="material-icons alert">
-                    mdi-alert-circle
-                </v-icon>
+              <v-icon
+                v-if="item[field] == 'Active'"
+                class="material-icons delivered"
+              >
+                mdi-checkbox-blank-circle
+              </v-icon>
+              <v-icon
+                v-if="item[field] == 'Delivering'"
+                class="material-icons alert"
+              >
+                mdi-alert-circle
+              </v-icon>
             </span>
             <span v-else>
               <span v-if="field != 'child'">
@@ -40,23 +46,29 @@
       </template>
       <template v-slot:expanded-item="{ item }">
         <tr v-for="(field, index) in item.child" :key="index">
-          <td class="text-left"></td>
-          <td class="text-left primary--text">{{ field.engagementName }}</td>
-          <td class="text-start">{{ field.audiences }}</td>
-          <td class="text-start">
-            <v-icon v-if="field.status == 'Active'" class="material-icons delivered">
-                mdi-checkbox-blank-circle
+          <td></td>
+          <td class="primary--text">{{ field.engagementName }}</td>
+          <td>{{ field.audiences }}</td>
+          <td>
+            <v-icon
+              v-if="field.status == 'Active'"
+              class="material-icons delivered"
+            >
+              mdi-checkbox-blank-circle
             </v-icon>
-            <v-icon v-if="field.status == 'Delivering'" class="material-icons alert">
-                mdi-alert-circle
+            <v-icon
+              v-if="field.status == 'Delivering'"
+              class="material-icons alert"
+            >
+              mdi-alert-circle
             </v-icon>
           </td>
-          <td class="text-start">{{ field.size }}</td>
-          <td class="text-start">{{ field.deliverySchedule }}</td>
-          <td class="text-start">{{ field.lastUpdated }}</td>
-          <td class="text-start">{{ field.lastUpdatedBy }}</td>
-          <td class="text-start">{{ field.created }}</td>
-          <td class="text-start">{{ field.createdBy }}</td>
+          <td>{{ field.size }}</td>
+          <td>{{ field.deliverySchedule }}</td>
+          <td>{{ field.lastUpdated }}</td>
+          <td>{{ field.lastUpdatedBy }}</td>
+          <td>{{ field.created }}</td>
+          <td>{{ field.createdBy }}</td>
         </tr>
       </template>
     </v-data-table>
@@ -100,27 +112,27 @@ export default {
           createdBy: "JS",
           child: [
             {
-                engagementName: "Frozen goods",
-                audiences: 159,
-                status: "Delivering",
-                size: "565k",
-                deliverySchedule: "-",
-                lastUpdated: "1 week ago",
-                lastUpdatedBy: "JS",
-                created: "1 month ago",
-                createdBy: "JS",
+              engagementName: "Frozen goods",
+              audiences: 159,
+              status: "Delivering",
+              size: "565k",
+              deliverySchedule: "-",
+              lastUpdated: "1 week ago",
+              lastUpdatedBy: "JS",
+              created: "1 month ago",
+              createdBy: "JS",
             },
             {
-                engagementName: "Texas",
-                audiences: 159,
-                status: "Active",
-                size: "30M",
-                deliverySchedule: "-",
-                lastUpdated: "1 week ago",
-                lastUpdatedBy: "JS",
-                created: "1 month ago",
-                createdBy: "JS",
-            }
+              engagementName: "Texas",
+              audiences: 159,
+              status: "Active",
+              size: "30M",
+              deliverySchedule: "-",
+              lastUpdated: "1 week ago",
+              lastUpdatedBy: "JS",
+              created: "1 month ago",
+              createdBy: "JS",
+            },
           ],
         },
       ],
@@ -143,6 +155,18 @@ export default {
   }
   .material-icons.alert {
     color: var(--v-error-base);
+  }
+  .rotate-icon {
+    transition: 0.3s;
+    -webkit-transition: 0.3s;
+    -moz-transition: 0.3s;
+    -ms-transition: 0.3s;
+    -o-transition: 0.3s;
+    -webkit-transform: rotate(90deg);
+    -moz-transform: rotate(90deg);
+    -o-transform: rotate(90deg);
+    -ms-transform: rotate(90deg);
+    transform: rotate(90deg);
   }
 }
 </style>
