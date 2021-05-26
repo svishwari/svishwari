9afcc9b85eaa84f436eb3cb954a72c0b7b39cecc
Doug <dolong@deloitte.com>
2021-05-26 00:43:12 -0600

    added the auth, fixed an issue

--- hux/api/huxunify/api/constants.py
+++ hux/api/huxunify/api/constants.py
@@ -237,4 +237,3 @@ INVALID_AUTH_HEADER = "Authorization header must start with Bearer."
 MISSING_TOKEN = "Token not found."
 AUTH_HEADER_FORMAT = "Authorization header must be 'Bearer token'."
 INVALID_AUTH = "You are not authorized to visit this page."
-SECURITY_TAG = {"Bearer": ["Authorization"]}
