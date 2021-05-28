f495fd5fbd3e7be683cbf30709b866aa5891be9d
Abdullah-Azad <mdazad@deloitte.com>
2021-05-28 14:59:28 +0530

    PR comment addressed

--- hux/frontend/src/components/common/Avatar.vue
+++ hux/frontend/src/components/common/Avatar.vue
@@ -5,7 +5,7 @@
         class="blue-grey d-flex align-center justify-center"
         v-bind="attrs"
         v-on="on"
-        v-bind:style="{ 'border-color': userInfo.color }"
+        :style="{ 'border-color': userInfo.color }"
       >
         {{ userInfo.fullName | shortName }}
       </span>
