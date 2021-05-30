7584875fab8cd920815baa3160c3e308a99f6d85
panch mukesh <ppanchmukesh@deloitte.com>
2021-05-30 15:45:56 +0530

    audiencerouter batch addition to aws_envs

--- hux/api/huxunify/test/data_connectors/test_courier.py
+++ hux/api/huxunify/test/data_connectors/test_courier.py
@@ -258,7 +258,8 @@ class CourierTest(TestCase):
                 batch_destination = get_destination_config(
                     self.database, destination_id, audience_id
                 )
-
+                batch_destination.aws_envs[c.AUDIENCE_ROUTER_BATCH_SIZE] = 1
+                batch_destination.aws_envs[c.AUDIENCE_ROUTER_STUB_TEST] = 1
                 self.assertIsNotNone(batch_destination)
 
                 # Register job
