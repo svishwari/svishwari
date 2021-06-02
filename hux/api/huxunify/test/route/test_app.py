668d27f3d0a7c1be671d4031d9476a98e71de84e
Doug <dolong@deloitte.com>
2021-06-02 12:52:51 -0600

    added unit test for HUS-521

---
+++ hux/api/huxunify/test/route/test_app.py
@@ -0,0 +1,34 @@
+"""
+purpose of this file is for testing that the app can be created without any issues.
+"""
+from unittest import TestCase
+
+from flask.testing import FlaskClient
+
+from huxunify.api.config import get_config
+from huxunify.app import create_app
+
+
+class CreateAppTest(TestCase):
+    """
+    Test Creating an App.
+    """
+
+    def setUp(self) -> None:
+        """Setup tests
+
+        Returns:
+
+        """
+        self.config = get_config("TEST")
+
+    def test_secured_all_endpoints_invalid_response(self):
+        """Test Creating the APP test client
+
+        Args:
+
+        Returns:
+
+        """
+
+        self.assertIsInstance(create_app().test_client(), FlaskClient)
