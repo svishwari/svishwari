d5b5e5564a417da9fd09be6832d464e430a12fae
Doug <dolong@deloitte.com>
2021-06-10 12:09:15 -0600

    revert

--- hux/api/huxunify/api/constants.py
+++ hux/api/huxunify/api/constants.py
@@ -364,11 +364,6 @@ CUSTOMERS_TAG = "customers"
 CUSTOMERS_ENDPOINT = "/customers"
 CUSTOMERS_DESCRIPTION = "Customers API"
 CUSTOMERS_OVERVIEW_ENDPOINT = "overview"
-CUSTOMERS_DASHBOARD_OVERVIEW_ENDPOINT = "dashboard_overview"
-
-# IDR Fields
-IDR_ENDPOINT = "/idr"
-IDR_OVERVIEW_ENDPOINT = "overview"
 
 # ERROR
 INVALID_AUTH_HEADER = "Authorization header is invalid."
