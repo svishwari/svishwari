d8d43631f4d0629999d97600421f29a3beed6bd1
McMahon <jimcmahon@deloitte.com>
2021-06-11 13:24:20 -0500

    black formatting

--- hux/api/huxunify/test/route/test_orchestration.py
+++ hux/api/huxunify/test/route/test_orchestration.py
@@ -95,7 +95,7 @@ class OrchestrationRouteTest(TestCase):
                     "does_not_equal": "Does not equal",
                     "equals": "Equals",
                 }
-            }
+            },
         )
 
         response = self.test_client.get(
