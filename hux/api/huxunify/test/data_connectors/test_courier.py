ffe6d22bb54547f4a9f5714bb0a19305c28b5d5e
Doug <dolong@deloitte.com>
2021-06-10 23:52:18 -0600

    updated to object id instead of _id

--- hux/api/huxunify/test/data_connectors/test_courier.py
+++ hux/api/huxunify/test/data_connectors/test_courier.py
@@ -105,14 +105,14 @@ class CourierTest(TestCase):
             c.NOTIFICATION_FIELD_DESCRIPTION: "Former fun forest amusement park.",
             c.AUDIENCES: [
                 {
-                    c.ID: self.audience_one[c.ID],
+                    c.OBJECT_ID: self.audience_one[c.ID],
                     c.DESTINATIONS: [
                         {c.DELIVERY_PLATFORM_ID: x}
                         for x in self.audience_one[c.DESTINATIONS]
                     ],
                 },
                 {
-                    c.ID: self.audience_two[c.ID],
+                    c.OBJECT_ID: self.audience_two[c.ID],
                     c.DESTINATIONS: [
                         {c.DELIVERY_PLATFORM_ID: x}
                         for x in self.audience_two[c.DESTINATIONS]
