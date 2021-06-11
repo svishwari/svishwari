c776abf2c292feb16dd6cf05fbe5e911214241e7
soumya-pb <soumbehera@deloitte.com>
2021-06-12 03:05:45 +0530

    Convert os.environ to dict to get environment values correctly.

--- scripts/database/share.py
+++ scripts/database/share.py
@@ -15,20 +15,34 @@ def get_mongo_client() -> MongoClient:
         MongoClient: A connected mongo client, pulling settings from the environment
     """
     # Get details on MongoDB configuration.
-    host = os.environ.get("MONGO_DB_HOST")
-    port = int(os.environ["MONGO_DB_PORT"]) if "MONGO_DB_PORT" in os.environ else None
-    user_name = os.environ.get("MONGO_DB_USERNAME")
-    password = os.environ.get("MONGO_DB_PASSWORD")
+    env = dict(os.environ)
+    host = env.get("MONGO_DB_HOST")
+    port = int(env["MONGO_DB_PORT"]) if "MONGO_DB_PORT" in os.environ else None
+    user_name = env.get("MONGO_DB_USERNAME")
+    password = env.get("MONGO_DB_PASSWORD")
     # grab the SSL cert path
-    ssl_cert_path = str(Path(__file__).parent.joinpath("rds-combined-ca-bundle.pem"))
+    use_ssl = host not in ["localhost", None]
 
     # Set up the database client
-    return MongoClient(
-        host,
-        port,
-        w=1,
-        username=user_name,
-        password=password,
-        ssl=True,
-        ssl_ca_certs=ssl_cert_path,
-    )
+    if use_ssl:
+        ssl_cert_path = str(Path(__file__).parent.joinpath("rds-combined-ca-bundle.pem"))
+        db_client = MongoClient(
+            host,
+            port,
+            w=1,
+            username=user_name,
+            password=password,
+            ssl=True,
+            ssl_ca_certs=ssl_cert_path,
+        )
+    else:
+        db_client = MongoClient(
+            host,
+            port,
+            w=1,
+            username=user_name,
+            password=password,
+            ssl=use_ssl,
+        )
+
+    return db_client
