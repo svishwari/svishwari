0d0310b9666a9ce5764ea1906ea83782d4b6c368
sach9993 <sacojha@deloitte.com>
2021-06-25 11:42:50 +0530

    chartTooltip component changes

---
+++ hux/frontend/src/components/common/ChartTooltip.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-card max-width="200" height="104" :style="{ 'top': positionTop+'px', 'left':positionLeft+'px' }" tile class="rounded-lg card-style">
+      Test Data
+
+      top: {{positionTop}}
+
+      left: {{positionLeft}}
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "ChartTooltip",
+  props: {
+    positionTop: {
+      type: Number,
+      required: false,
+    },
+    positionLeft: {
+      type: Number,
+      required: false,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.card-style {
+    max-width: 308px;
+    max-height: 20px;
+}
+</style>
