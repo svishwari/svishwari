eb71d7f15db4a01820a8e23fb562c7f8261492c5
Rahul Goel <rahgoel@deloitte.com>
2021-06-29 01:29:44 +0530

    Enforced Error handling to the filters logic

--- hux/frontend/src/views/Audiences/Insight.vue
+++ hux/frontend/src/views/Audiences/Insight.vue
@@ -57,7 +57,7 @@
               >
                 <icon
                   :type="filterKey == 'general' ? 'plus' : filterKey"
-                  :size="10"
+                  :size="filterKey == 'general' ? 10 : 21"
                   class="mr-1"
                 />
                 <!-- <span class="ml-1"></span> -->
@@ -197,8 +197,9 @@ export default {
         },
       },
       modelInitial: [
-        { value: "propensity", icon: "churn" },
+        { value: "propensity", icon: "model" },
         { value: "lifetime", icon: "lifetime" },
+        { value: "churn", icon: "churn" },
       ],
     }
   },
@@ -216,41 +217,47 @@ export default {
      * on the UI transformation.
      */
     appliedFilters() {
-      let _filters = {}
-      if (this.audience && this.audience.filters) {
-        this.audience.filters.forEach((section) => {
-          section.section_filters.forEach((filter) => {
-            const model = this.modelInitial.filter((model) =>
-              filter.field.includes(model.value)
-            )
-            const filterObj = {
-              name: this.$options.filters.TitleCase(filter.field),
-              key: filter.field,
-            }
+      try {
+        let _filters = {}
+        if (this.audience && this.audience.filters) {
+          this.audience.filters.forEach((section) => {
+            section.section_filters.forEach((filter) => {
+              const model = this.modelInitial.filter(
+                (model) =>
+                  typeof filter.field === "string" &&
+                  filter.field.includes(model.value)
+              )
+              const filterObj = {
+                name: this.$options.filters.TitleCase(filter.field),
+                key: filter.field,
+              }
 
-            filterObj.name = filterObj.name.replace(/_/gi, " ")
-            if (model.length > 0) {
-              filterObj["hover"] = "Between " + filter.value.join("-")
-              if (!_filters[model[0].icon]) _filters[model[0].icon] = {}
-              if (_filters[model[0].icon][filter.field])
-                _filters[model[0].icon][filter.field]["hover"] +=
-                  "<br/> " + filterObj.hover
-              else _filters[model[0].icon][filter.field] = filterObj
-            } else {
-              if (!_filters["general"]) _filters["general"] = {}
-              filterObj["hover"] =
-                filter.type === "range"
-                  ? "Include " + filter.value.join("-")
-                  : filter.value
-              if (_filters["general"][filter.field])
-                _filters["general"][filter.field]["hover"] +=
-                  "<br/> " + filterObj.hover
-              else _filters["general"][filter.field] = filterObj
-            }
+              filterObj.name = filterObj.name.replace(/_/gi, " ")
+              if (model.length > 0) {
+                filterObj["hover"] = "Between " + filter.value.join("-")
+                if (!_filters[model[0].icon]) _filters[model[0].icon] = {}
+                if (_filters[model[0].icon][filter.field])
+                  _filters[model[0].icon][filter.field]["hover"] +=
+                    "<br/> " + filterObj.hover
+                else _filters[model[0].icon][filter.field] = filterObj
+              } else {
+                if (!_filters["general"]) _filters["general"] = {}
+                filterObj["hover"] =
+                  filter.type === "range"
+                    ? "Include " + filter.value.join("-")
+                    : filter.value
+                if (_filters["general"][filter.field])
+                  _filters["general"][filter.field]["hover"] +=
+                    "<br/> " + filterObj.hover
+                else _filters["general"][filter.field] = filterObj
+              }
+            })
           })
-        })
+        }
+        return _filters
+      } catch (error) {
+        return []
       }
-      return _filters
     },
   },
   methods: {
@@ -335,6 +342,10 @@ export default {
         height: auto;
         float: left;
         margin-left: 2%;
+        svg {
+          width: 21px;
+          height: 21px;
+        }
         span {
           .filter-title {
             &::after {
