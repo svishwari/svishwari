e37a58de39191b3733829a7c7d2e1badf82d7872
sach9993 <sacojha@deloitte.com>
2021-06-28 07:01:38 +0530

    lint fixes

--- hux/frontend/src/views/CustomerProfiles/Index.vue
+++ hux/frontend/src/views/CustomerProfiles/Index.vue
@@ -274,8 +274,10 @@ export default {
         let updatedTime = this.$options.filters
           .Date(value, "calendar")
           .split(" at ")
-        return updatedTime.length > 1 ? [updatedTime[0], updatedTime[1].replaceAll(" ", "")] : [updatedTime[0], '']
-    }
+        return updatedTime.length > 1
+          ? [updatedTime[0], updatedTime[1].replaceAll(" ", "")]
+          : [updatedTime[0], ""]
+      }
     },
     viewCustomerList() {
       this.customerProfilesDrawer = !this.customerProfilesDrawer
