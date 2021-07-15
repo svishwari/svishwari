274efdbfa01f92f6ed12ddb1bfb95ce336054ac3
Abdullah-Azad <mdazad@deloitte.com>
2021-07-15 23:04:38 +0530

    removed unused compoenent

--- hux/frontend/src/views/Audiences/Configuration/Drawers/DestinationDataExtension.vue
+++ hux/frontend/src/views/Audiences/Configuration/Drawers/DestinationDataExtension.vue
@@ -172,7 +172,6 @@ import Drawer from "@/components/common/Drawer"
 import HuxButton from "@/components/common/huxButton"
 import Logo from "@/components/common/Logo"
 import TextField from "@/components/common/TextField"
-import Tooltip from "@/components/common/Tooltip"
 import extensionInactive1 from "@/assets/logos/extension-inactive-1.svg"
 import extensionInactive2 from "@/assets/logos/extension-inactive-2.svg"
 export default {
@@ -185,7 +184,6 @@ export default {
     TextField,
     extensionInactive1,
     extensionInactive2,
-    Tooltip,
   },
 
   computed: {
