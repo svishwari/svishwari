83887470e45ef959de9979ef3b5c90bddfe6469a
Henrik Rypkema <henrikr@users.noreply.github.com>
2021-07-16 15:12:58 -0400

    Update customers drawer.

--- hux/frontend/src/components/common/Tooltip.vue
+++ hux/frontend/src/components/common/Tooltip.vue
@@ -3,10 +3,12 @@
     <template #activator="{ on }">
       <span v-on="on">
         <slot name="label-content"></slot>
+        <slot name="default"></slot>
       </span>
     </template>
     <div class="px-4 py-2 white caption">
       <slot name="hover-content"></slot>
+      <slot name="tooltip"></slot>
     </div>
   </v-menu>
 </template>
