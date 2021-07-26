60aff921581618db3d257bd34ad503566ffae0a9
panch mukesh <ppanchmukesh@deloitte.com>
2021-07-26 22:53:35 +0530

    Scheduler Function for Delivery Schedule

--- hux/api/huxunify/api/data_connectors/scheduler.py
+++ hux/api/huxunify/api/data_connectors/scheduler.py
@@ -1,29 +1,42 @@
+# Purpose of this module is to park schedule modules for delivery schedule
+"""
 schedule = {
-    "periodicity": "Daily",
+    "periodicity": "Weekly",
     "every": 2,
     "hour": 11,
     "minute": 15,
     "period": "PM",
     "day_of_month": "*",
     "month": "*",
-    "day_of_week": "*"
+    "day_of_week": ['Weekend']
 }
+"""
 
 cron_exp = {
-    "second": "*",
     "minute": "*",
     "hour": "*",
     "day_of_month": "*",
     "month": "*",
     "day_of_week": "*",
-    "year": "*"
+    "year": "*",
 }
 
 
-def generate_cron(schedule):
+def generate_cron(schedule: dict) -> str:
+    """
+
+    Args:
+        schedule: dictionary object of schedule
+
+    Returns:
+        str: cron expression
+    """
+
     cron_exp["minute"] = schedule.get("minute")
     if schedule.get("period") == "AM":
-        cron_exp["hour"] = 0 if schedule.get("hour") == 12 else schedule.get("hour")
+        cron_exp["hour"] = (
+            0 if schedule.get("hour") == 12 else schedule.get("hour")
+        )
     else:
         cron_exp["hour"] = schedule.get("hour") + 12
     cron_exp["day_of_month"] = schedule.get("day_of_month")
@@ -31,14 +44,21 @@ def generate_cron(schedule):
     if schedule["periodicity"] == "Weekly":
         cron_exp["day_of_month"] = "?"
         if schedule["every"] > 1:
-            cron_exp["day_of_week"] = ",".join(schedule.get("day_of_week"))
-            cron_exp["day_of_week"] += "{}{}".format("/", schedule["every"])
+            if schedule.get("day_of_week")[0] == "Weekend":
+                cron_exp["day_of_week"] = ",".join(["SAT", "SUN"])
+                cron_exp["day_of_week"] += "{}{}".format(
+                    "/", schedule["every"]
+                )
+            else:
+                cron_exp["day_of_week"] = ",".join(schedule.get("day_of_week"))
+                cron_exp["day_of_week"] += "{}{}".format(
+                    "/", schedule["every"]
+                )
 
     if schedule["periodicity"] == "Daily":
         cron_exp["day_of_month"] = "?"
         if schedule["every"] > 1:
-            cron_exp["day_of_week"] = "{}{}{}".format("1", "/", schedule["every"])
+            cron_exp["day_of_week"] = "{}{}{}".format(
+                "1", "/", schedule["every"]
+            )
     return " ".join([str(val) for val in cron_exp.values()])
-
-
-print(generate_cron(schedule))
