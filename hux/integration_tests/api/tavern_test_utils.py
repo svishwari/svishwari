3ebe923c7d8e34e14ffef93309a61e89b6ff6cd3
skondalraj-deloitte <skondalraj@deloitte.com>
2021-07-27 18:16:46 -0500

    HUS-733: Code review suggested changes and changes to simplify the external method by passing arguments

--- hux/integration_tests/api/tavern_test_utils.py
+++ hux/integration_tests/api/tavern_test_utils.py
@@ -1,40 +1,28 @@
 """
 purpose of this file is to house external function utilities needed for tavern integration tests
 """
+from typing import Union
 from box import Box
 
-# TODO: Check to see if there is a way to pass in other arguments
-# along with response for better modular functions
-# It doesn't look like it is currently supported as called out in
-# the "Note" section of the official documentation at
-# https://tavern.readthedocs.io/en/latest/basics.html?highlight=calling%20external#using-external-functions-for-other-things
-def get_facebook_destination_id(response):
+
+def get_destination_id(response: object, **kwargs: dict) -> Union[Box, None]:
     """
-    Purpose of this function is to get the facebook destination id,
-    from the response object and return it wrapped in a Box object
+    Purpose of this function is to get the destination id from the response object based on the
+    passed in destination_type key in the kwargs dict and return it wrapped in a Box object.
+
     Args:
-        response: response object
+        response (object): response object.
+        **kwargs (dict): function keyword arguments.
 
     Returns:
-        Box(ObjectId): destination_id of destination type facebook
+        Box: destination_id of the requested destination type facebook.
     """
+
+    destination_type = kwargs.get("destination_type", None)
+
     for json in response.json():
-        if json["type"] == "facebook":
+        if json["type"] == "facebook" and destination_type == "facebook":
             return Box({"facebook_destination_id": json["id"]})
-    return None
-
-
-def get_sfmc_destination_id(response):
-    """
-    Purpose of this function is to get the sfmc destination id,
-    from the response object and return it wrapped in a Box object
-    Args:
-        response: response object
-
-    Returns:
-        Box(ObjectId): destination_id of destination type sfmc
-    """
-    for json in response.json():
-        if json["type"] == "sfmc":
+        if json["type"] == "sfmc" and destination_type == "sfmc":
             return Box({"sfmc_destination_id": json["id"]})
     return None
