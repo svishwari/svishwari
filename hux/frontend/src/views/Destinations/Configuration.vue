33f70e36c35eaf4c993db95176048423f367e26f
mohitbans <74055271+mohitbans@users.noreply.github.com>
2021-08-02 12:09:58 +0530

    build fix

--- hux/frontend/src/views/Destinations/Configuration.vue
+++ hux/frontend/src/views/Destinations/Configuration.vue
@@ -169,7 +169,7 @@ import huxButton from "@/components/common/huxButton"
 import HuxFooter from "@/components/common/HuxFooter"
 import TextField from "@/components/common/TextField"
 
-import SFMC from "./Configuration/SFMC"
+import SFMC from "./Configuration/SFMC.vue"
 
 export default {
   name: "ConfigureDestination",
