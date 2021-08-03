763dd2ab8c801bf9e19dad5df0d67dc77ce93bef
mohitbans <74055271+mohitbans@users.noreply.github.com>
2021-08-03 14:06:10 +0530

    rename

--- hux/frontend/src/views/Engagements/Dashboard.vue
+++ hux/frontend/src/views/Engagements/Dashboard.vue
@@ -322,7 +322,7 @@ export default {
       editDeliveryDrawer: false,
       scheduleDestination: {
         name: null,
-        type: null,
+        delivery_platform_type: null,
         id: null,
       },
     }
