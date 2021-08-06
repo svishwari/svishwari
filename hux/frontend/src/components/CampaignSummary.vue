b91e07f4d2e3d9f2ef08fd0bbf35adb4f10d5a2a
svishwari <svishwari@deloitte.com>
2021-08-06 20:58:22 +0530

    lint fix

--- hux/frontend/src/components/CampaignSummary.vue
+++ hux/frontend/src/components/CampaignSummary.vue
@@ -237,7 +237,7 @@
                         ),
                       }"
                     >
-                      <span class="error--text mr-6">
+                      <span class="error--text mr-2">
                         <v-icon small color="error" class="icon-info mr-1">
                           mdi-information-outline
                         </v-icon>
