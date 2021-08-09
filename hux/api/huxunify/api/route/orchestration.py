2c8c0a5b4c65f89aa1780685eec9febb9c297645
McMahon <jimcmahon@deloitte.com>
2021-08-09 08:38:37 -0500

    pylint fix

--- hux/api/huxunify/api/route/orchestration.py
+++ hux/api/huxunify/api/route/orchestration.py
@@ -816,7 +816,7 @@ class SetLookalikeAudience(SwaggerView):
     responses.update(AUTH401_RESPONSE)
     tags = [api_c.ORCHESTRATION_TAG]
 
-    # pylint: disable=no-self-use
+    # pylint: disable=no-self-use, unsubscriptable-object
     @api_error_handler()
     @get_user_name()
     def post(self, user_name: str) -> Tuple[dict, int]:
