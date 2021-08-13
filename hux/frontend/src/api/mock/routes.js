275af868d5a6a0c1ed8851a750dec537bbde5ad4
Abdullah-Azad <mdazad@deloitte.com>
2021-08-13 14:22:25 +0530

    lint fixes

--- hux/frontend/src/api/mock/routes.js
+++ hux/frontend/src/api/mock/routes.js
@@ -337,7 +337,7 @@ export const defineRoutes = (server) => {
     return data
   })
 
-  server.get("/models/:id/version-history" , (schema, request) => {
+  server.get("/models/:id/version-history", (schema, request) => {
     const id = request.params.id
     const model = schema.models.find(id)
     return model.attrs.version_history
