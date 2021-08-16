e60c2797bc3e1be5a41736e8aea89304761d1f8d
Henrik Rypkema <henrikr@users.noreply.github.com>
2021-08-16 17:01:27 -0400

    Reuse command.

--- hux/integration_tests/frontend/cypress/integration/connections/index.spec.js
+++ hux/integration_tests/frontend/cypress/integration/connections/index.spec.js
@@ -3,35 +3,10 @@ import selector from "../../support/selectors.js"
 
 describe("Tests data souces and destinations in connections", () => {
   before(() => {
-    // opens the app
-    cy.visit(route.home)
-
-    // clicks the signin button
-    cy.get(selector.home.signin).click()
-
-    // we should now be on the login page
-    cy.location("pathname").should("eq", route.login)
-
-    // fill in the form
-    cy.get(selector.login.email).type(Cypress.env("USER_EMAIL"), { log: false })
-
-    cy.get(selector.login.password).type(Cypress.env("USER_PASSWORD"), {
-      log: false,
+    cy.signin({
+      email: Cypress.env("USER_EMAIL"),
+      password: Cypress.env("USER_PASSWORD"),
     })
-
-    // submit the form
-    cy.get(selector.login.submit).click()
-
-    // TODO: add MFA related authentication if needed
-
-    // we should no longer be on the login page
-    cy.location("pathname", { timeout: 10000 })
-      .should("not.eq", route.login)
-      .then(() => {
-        // okta does its authentication...
-        // once its done, we should be redirected back to the app
-        cy.location("pathname").should("eq", route.oktaSignInRedirectURI)
-      })
   })
 
   it("testing data management > connections > data sources", () => {
