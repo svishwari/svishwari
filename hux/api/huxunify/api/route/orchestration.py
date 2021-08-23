c1fbdcad1ab63f5e3efd36c2f34412613b557172
panch mukesh <ppanchmukesh@deloitte.com>
2021-08-23 18:51:18 +0530

    Audience Download

--- hux/api/huxunify/api/route/orchestration.py
+++ hux/api/huxunify/api/route/orchestration.py
@@ -1,17 +1,20 @@
+# pylint: disable=C0302
 """
 Paths for Orchestration API
 """
+import csv
 from http import HTTPStatus
+from pathlib import Path
 from random import uniform
 from typing import Tuple, Union
 from flasgger import SwaggerView
 from bson import ObjectId
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, Response
 from marshmallow import INCLUDE
 from pymongo import MongoClient
 
 from huxunifylib.util.general.logging import logger
-from huxunifylib.connectors import FacebookConnector
+from huxunifylib.connectors import FacebookConnector, connector_cdp
 from huxunifylib.database.notification_management import create_notification
 from huxunifylib.database import (
     delivery_platform_management as destination_management,
@@ -21,6 +24,8 @@ from huxunifylib.database import (
     data_management,
 )
 import huxunifylib.database.constants as db_c
+
+from huxunify.api.config import get_config
 from huxunify.api.schema.orchestration import (
     AudienceGetSchema,
     AudiencePutSchema,
@@ -952,4 +957,129 @@ class SetLookalikeAudience(SwaggerView):
         return (
             LookalikeAudienceGetSchema().dump(lookalike_audience),
             HTTPStatus.CREATED,
+        )
+
+
+@add_view_to_blueprint(
+    orchestration_bp,
+    f"{api_c.AUDIENCE_ENDPOINT}/<audience_id>/<download_type>",
+    "AudienceDownloadView",
+)
+class AudienceDownload(SwaggerView):
+    """
+    Audience Put view class
+    """
+
+    parameters = [
+        {
+            "name": api_c.AUDIENCE_ID,
+            "description": "Audience ID.",
+            "type": "string",
+            "in": "path",
+            "required": True,
+            "example": "5f5f7262997acad4bac4373b",
+        },
+        {
+            "name": api_c.DOWNLOAD_TYPE,
+            "description": "Download Type",
+            "type": "string",
+            "in": "path",
+            "required": True,
+            "example": api_c.GOOGLE_ADS,
+        },
+    ]
+    responses = {
+        HTTPStatus.OK.value: {
+            "description": "Download Audience.",
+        },
+        HTTPStatus.BAD_REQUEST.value: {
+            "description": "Failed to download audience.",
+        },
+    }
+
+    responses.update(AUTH401_RESPONSE)
+    tags = [api_c.ORCHESTRATION_TAG]
+
+    # pylint: disable=no-self-use
+    @api_error_handler()
+    @get_user_name()
+    def get(
+        self, audience_id: str, download_type: str, user_name: str
+    ) -> Tuple[Response, int]:
+        """Updates an audience.
+
+        ---
+        security:
+            - Bearer: ["Authorization"]
+
+        Args:
+            audience_id (str): Audience ID.
+            download_type (str): Download type
+            user_name (str): User Name
+
+        Returns:
+            Tuple[Response, int]: File Object Response, HTTP status.
+
+        """
+
+        database = get_db_client()
+        audience = orchestration_management.get_audience(
+            database, ObjectId(audience_id)
+        )
+
+        if not audience:
+            return {"message": "Audience not found"}, HTTPStatus.BAD_REQUEST
+
+        if not api_c.DOWNLOAD_TYPES.get(download_type):
+            return {"message": "Invalid download type"}, HTTPStatus.BAD_REQUEST
+
+        cdp = connector_cdp.ConnectorCDP(get_config().CDP_SERVICE)
+        column_set = [api_c.HUX_ID] + list(
+            api_c.DOWNLOAD_TYPES[download_type].keys()
+        )
+        data_batches = cdp.read_batches(
+            location_details={
+                api_c.AUDIENCE_FILTERS: audience.get(api_c.AUDIENCE_FILTERS),
+            },
+            batch_size=int(api_c.CUSTOMERS_DEFAULT_BATCH_SIZE),
+            column_set=column_set,
+        )
+
+        audience_file_name = f"{audience_id}_{download_type}.csv"
+        with open(audience_file_name, "w", newline="") as csvfile:
+            csvwriter = csv.writer(csvfile)
+            csvwriter.writerow(
+                list(api_c.DOWNLOAD_TYPES[download_type].values())
+            )
+            for dataframe_batch in data_batches:
+                dataframe_batch.to_csv(
+                    csvfile,
+                    mode="a",
+                    index=False,
+                    columns=list(api_c.DOWNLOAD_TYPES[download_type].keys()),
+                    header=False,
+                )
+
+        audience_file = Path(audience_file_name)
+        data = audience_file.read_bytes()
+        audience_file.unlink()
+
+        create_notification(
+            database,
+            db_c.NOTIFICATION_TYPE_INFORMATIONAL,
+            f'Audience "{audience[db_c.NAME]}" of {download_type} '
+            f"format downloaded by {user_name}.",
+            api_c.ORCHESTRATION_TAG,
+        )
+
+        return (
+            Response(
+                data,
+                headers={
+                    "Content-Type": "application/csv",
+                    "Content-Disposition": "attachment; filename=%s;"
+                    % audience_file_name,
+                },
+            ),
+            HTTPStatus.OK,
         )
