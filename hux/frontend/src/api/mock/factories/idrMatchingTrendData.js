a19fab91a12a4d1c32fa5fe66f45a088d903ae44
svishwari <svishwari@deloitte.com>
2021-08-23 18:47:11 +0530

    lint fix

--- hux/frontend/src/api/mock/factories/idrMatchingTrendData.js
+++ hux/frontend/src/api/mock/factories/idrMatchingTrendData.js
@@ -7,8 +7,8 @@ export const genderSpendData = () => {
 const spend = () => {
   return {
     known_ids: `${faker.datatype.number({ min: 101982, max: 155851 })}`,
-    unique_hux_ids:`${faker.datatype.number({ min: 101982, max: 155851 })}`,
+    unique_hux_ids: `${faker.datatype.number({ min: 101982, max: 155851 })}`,
     date: faker.date.between("2021-01-01", "2021-08-18"),
-    anonymous_ids: 0
+    anonymous_ids: 0,
   }
 }
