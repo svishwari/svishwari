b140ba402f8d31648b31923d07ac81a816381799
sach9993 <sacojha@deloitte.com>
2021-08-27 17:08:05 +0530

    lint fixes !!!

--- hux/frontend/src/api/mock/fixtures/totalCustomersData.js
+++ hux/frontend/src/api/mock/fixtures/totalCustomersData.js
@@ -1,402 +1,402 @@
 export default [
   {
-      date: "2021-03-01T00:00:00.000Z",
-      total_customers: 71000,
-      new_customers_added: 71000
+    date: "2021-03-01T00:00:00.000Z",
+    total_customers: 71000,
+    new_customers_added: 71000,
   },
   {
-      date: "2021-03-02T00:00:00.000Z",
-      total_customers: 72000,
-      new_customers_added: 1000
+    date: "2021-03-02T00:00:00.000Z",
+    total_customers: 72000,
+    new_customers_added: 1000,
   },
   {
-      date: "2021-03-03T00:00:00.000Z",
-      total_customers: 72000,
-      new_customers_added: 0
+    date: "2021-03-03T00:00:00.000Z",
+    total_customers: 72000,
+    new_customers_added: 0,
   },
   {
-      date: "2021-03-04T00:00:00.000Z",
-      total_customers: 74000,
-      new_customers_added: 2000
+    date: "2021-03-04T00:00:00.000Z",
+    total_customers: 74000,
+    new_customers_added: 2000,
   },
   {
-      date: "2021-03-05T00:00:00.000Z",
-      total_customers: 74020,
-      new_customers_added: 20
+    date: "2021-03-05T00:00:00.000Z",
+    total_customers: 74020,
+    new_customers_added: 20,
   },
   {
-      date: "2021-03-06T00:00:00.000Z",
-      total_customers: 75030,
-      new_customers_added: 1010
+    date: "2021-03-06T00:00:00.000Z",
+    total_customers: 75030,
+    new_customers_added: 1010,
   },
   {
-      date: "2021-03-07T00:00:00.000Z",
-      total_customers: 77070,
-      new_customers_added: 2040
+    date: "2021-03-07T00:00:00.000Z",
+    total_customers: 77070,
+    new_customers_added: 2040,
   },
   {
-      date: "2021-03-08T00:00:00.000Z",
-      total_customers: 80000,
-      new_customers_added: 2930
+    date: "2021-03-08T00:00:00.000Z",
+    total_customers: 80000,
+    new_customers_added: 2930,
   },
   {
-      date: "2021-03-09T00:00:00.000Z",
-      total_customers: 81010,
-      new_customers_added: 1010
+    date: "2021-03-09T00:00:00.000Z",
+    total_customers: 81010,
+    new_customers_added: 1010,
   },
   {
-      date: "2021-03-10T00:00:00.000Z",
-      total_customers: 83020,
-      new_customers_added: 2010
+    date: "2021-03-10T00:00:00.000Z",
+    total_customers: 83020,
+    new_customers_added: 2010,
   },
   {
-      date: "2021-03-11T00:00:00.000Z",
-      total_customers: 84000,
-      new_customers_added: 980
+    date: "2021-03-11T00:00:00.000Z",
+    total_customers: 84000,
+    new_customers_added: 980,
   },
   {
-      date: "2021-03-12T00:00:00.000Z",
-      total_customers: 85000,
-      new_customers_added: 1000
+    date: "2021-03-12T00:00:00.000Z",
+    total_customers: 85000,
+    new_customers_added: 1000,
   },
   {
-      date: "2021-03-13T00:00:00.000Z",
-      total_customers: 91000,
-      new_customers_added: 6000
+    date: "2021-03-13T00:00:00.000Z",
+    total_customers: 91000,
+    new_customers_added: 6000,
   },
   {
-      date: "2021-03-14T00:00:00.000Z",
-      total_customers: 92050,
-      new_customers_added: 1050
+    date: "2021-03-14T00:00:00.000Z",
+    total_customers: 92050,
+    new_customers_added: 1050,
   },
   {
-      date: "2021-03-15T00:00:00.000Z",
-      total_customers: 93060,
-      new_customers_added: 1010
+    date: "2021-03-15T00:00:00.000Z",
+    total_customers: 93060,
+    new_customers_added: 1010,
   },
   {
-      date: "2021-03-16T00:00:00.000Z",
-      total_customers: 95070,
-      new_customers_added: 2010
+    date: "2021-03-16T00:00:00.000Z",
+    total_customers: 95070,
+    new_customers_added: 2010,
   },
   {
-      date: "2021-03-17T00:00:00.000Z",
-      total_customers: 95080,
-      new_customers_added: 10
+    date: "2021-03-17T00:00:00.000Z",
+    total_customers: 95080,
+    new_customers_added: 10,
   },
   {
-      date: "2021-03-18T00:00:00.000Z",
-      total_customers: 94080,
-      new_customers_added: -1000
+    date: "2021-03-18T00:00:00.000Z",
+    total_customers: 94080,
+    new_customers_added: -1000,
   },
   {
-      date: "2021-03-19T00:00:00.000Z",
-      total_customers: 95080,
-      new_customers_added: 1000
+    date: "2021-03-19T00:00:00.000Z",
+    total_customers: 95080,
+    new_customers_added: 1000,
   },
   {
-      date: "2021-03-20T00:00:00.000Z",
-      total_customers: 105080,
-      new_customers_added: 10000
+    date: "2021-03-20T00:00:00.000Z",
+    total_customers: 105080,
+    new_customers_added: 10000,
   },
   {
-      date: "2021-03-21T00:00:00.000Z",
-      total_customers: 95080,
-      new_customers_added: -10000
+    date: "2021-03-21T00:00:00.000Z",
+    total_customers: 95080,
+    new_customers_added: -10000,
   },
   {
-      date: "2021-03-22T00:00:00.000Z",
-      total_customers: 95080,
-      new_customers_added: 0
+    date: "2021-03-22T00:00:00.000Z",
+    total_customers: 95080,
+    new_customers_added: 0,
   },
   {
-      date: "2021-03-23T00:00:00.000Z",
-      total_customers: 95080,
-      new_customers_added: 0
+    date: "2021-03-23T00:00:00.000Z",
+    total_customers: 95080,
+    new_customers_added: 0,
   },
   {
-      date: "2021-03-24T00:00:00.000Z",
-      total_customers: 100050,
-      new_customers_added: 4970
+    date: "2021-03-24T00:00:00.000Z",
+    total_customers: 100050,
+    new_customers_added: 4970,
   },
   {
-      date: "2021-03-25T00:00:00.000Z",
-      total_customers: 101050,
-      new_customers_added: 1000
+    date: "2021-03-25T00:00:00.000Z",
+    total_customers: 101050,
+    new_customers_added: 1000,
   },
   {
-      date: "2021-03-26T00:00:00.000Z",
-      total_customers: 102000,
-      new_customers_added: 950
+    date: "2021-03-26T00:00:00.000Z",
+    total_customers: 102000,
+    new_customers_added: 950,
   },
   {
-      date: "2021-03-27T00:00:00.000Z",
-      total_customers: 103000,
-      new_customers_added: 1000
+    date: "2021-03-27T00:00:00.000Z",
+    total_customers: 103000,
+    new_customers_added: 1000,
   },
   {
-      date: "2021-03-28T00:00:00.000Z",
-      total_customers: 103080,
-      new_customers_added: 80
+    date: "2021-03-28T00:00:00.000Z",
+    total_customers: 103080,
+    new_customers_added: 80,
   },
   {
-      date: "2021-03-29T00:00:00.000Z",
-      total_customers: 104080,
-      new_customers_added: 1000
+    date: "2021-03-29T00:00:00.000Z",
+    total_customers: 104080,
+    new_customers_added: 1000,
   },
   {
-      date: "2021-03-30T00:00:00.000Z",
-      total_customers: 105080,
-      new_customers_added: 1000
+    date: "2021-03-30T00:00:00.000Z",
+    total_customers: 105080,
+    new_customers_added: 1000,
   },
   {
-      date: "2021-04-01T00:00:00.000Z",
-      total_customers: 105080,
-      new_customers_added: 0
+    date: "2021-04-01T00:00:00.000Z",
+    total_customers: 105080,
+    new_customers_added: 0,
   },
   {
-      date: "2021-04-06T00:00:00.000Z",
-      total_customers: 108200,
-      new_customers_added: 3120
+    date: "2021-04-06T00:00:00.000Z",
+    total_customers: 108200,
+    new_customers_added: 3120,
   },
   {
-      date: "2021-04-08T00:00:00.000Z",
-      total_customers: 111100,
-      new_customers_added: 2900
+    date: "2021-04-08T00:00:00.000Z",
+    total_customers: 111100,
+    new_customers_added: 2900,
   },
   {
-      date: "2021-04-11T00:00:00.000Z",
-      total_customers: 112300,
-      new_customers_added: 1200
+    date: "2021-04-11T00:00:00.000Z",
+    total_customers: 112300,
+    new_customers_added: 1200,
   },
   {
-      date: "2021-05-13T00:00:00.000Z",
-      total_customers: 116300,
-      new_customers_added: 4000
+    date: "2021-05-13T00:00:00.000Z",
+    total_customers: 116300,
+    new_customers_added: 4000,
   },
   {
-      date: "2021-05-15T00:00:00.000Z",
-      total_customers: 117000,
-      new_customers_added: 700
+    date: "2021-05-15T00:00:00.000Z",
+    total_customers: 117000,
+    new_customers_added: 700,
   },
   {
-      date: "2021-05-20T00:00:00.000Z",
-      total_customers: 117350,
-      new_customers_added: 350
+    date: "2021-05-20T00:00:00.000Z",
+    total_customers: 117350,
+    new_customers_added: 350,
   },
   {
-      date: "2021-05-24T00:00:00.000Z",
-      total_customers: 118380,
-      new_customers_added: 1030
+    date: "2021-05-24T00:00:00.000Z",
+    total_customers: 118380,
+    new_customers_added: 1030,
   },
   {
-      date: "2021-05-25T00:00:00.000Z",
-      total_customers: 119380,
-      new_customers_added: 1000
+    date: "2021-05-25T00:00:00.000Z",
+    total_customers: 119380,
+    new_customers_added: 1000,
   },
   {
-      date: "2021-05-28T00:00:00.000Z",
-      total_customers: 120380,
-      new_customers_added: 1000
+    date: "2021-05-28T00:00:00.000Z",
+    total_customers: 120380,
+    new_customers_added: 1000,
   },
   {
-      date: "2021-06-11T00:00:00.000Z",
-      total_customers: 120500,
-      new_customers_added: 120
+    date: "2021-06-11T00:00:00.000Z",
+    total_customers: 120500,
+    new_customers_added: 120,
   },
   {
-      date: "2021-06-16T00:00:00.000Z",
-      total_customers: 122000,
-      new_customers_added: 1500
+    date: "2021-06-16T00:00:00.000Z",
+    total_customers: 122000,
+    new_customers_added: 1500,
   },
   {
-      date: "2021-06-24T00:00:00.000Z",
-      total_customers: 129000,
-      new_customers_added: 7000
+    date: "2021-06-24T00:00:00.000Z",
+    total_customers: 129000,
+    new_customers_added: 7000,
   },
   {
-      date: "2021-06-25T00:00:00.000Z",
-      total_customers: 129524,
-      new_customers_added: 524
+    date: "2021-06-25T00:00:00.000Z",
+    total_customers: 129524,
+    new_customers_added: 524,
   },
   {
-      date: "2021-06-30T00:00:00.000Z",
-      total_customers: 129594,
-      new_customers_added: 70
+    date: "2021-06-30T00:00:00.000Z",
+    total_customers: 129594,
+    new_customers_added: 70,
   },
   {
-      date: "2021-07-08T00:00:00.000Z",
-      total_customers: 129800,
-      new_customers_added: 206
+    date: "2021-07-08T00:00:00.000Z",
+    total_customers: 129800,
+    new_customers_added: 206,
   },
   {
-      date: "2021-07-09T00:00:00.000Z",
-      total_customers: 151904,
-      new_customers_added: 22104
+    date: "2021-07-09T00:00:00.000Z",
+    total_customers: 151904,
+    new_customers_added: 22104,
   },
   {
-      date: "2021-07-11T00:00:00.000Z",
-      total_customers: 142904,
-      new_customers_added: -9000
+    date: "2021-07-11T00:00:00.000Z",
+    total_customers: 142904,
+    new_customers_added: -9000,
   },
   {
-      date: "2021-07-12T00:00:00.000Z",
-      total_customers: 139904,
-      new_customers_added: -3000
+    date: "2021-07-12T00:00:00.000Z",
+    total_customers: 139904,
+    new_customers_added: -3000,
   },
   {
-      date: "2021-07-13T00:00:00.000Z",
-      total_customers: 140674,
-      new_customers_added: 770
+    date: "2021-07-13T00:00:00.000Z",
+    total_customers: 140674,
+    new_customers_added: 770,
   },
   {
-      date: "2021-07-16T00:00:00.000Z",
-      total_customers: 146110,
-      new_customers_added: 5436
+    date: "2021-07-16T00:00:00.000Z",
+    total_customers: 146110,
+    new_customers_added: 5436,
   },
   {
-      date: "2021-07-17T00:00:00.000Z",
-      total_customers: 148167,
-      new_customers_added: 2057
+    date: "2021-07-17T00:00:00.000Z",
+    total_customers: 148167,
+    new_customers_added: 2057,
   },
   {
-      date: "2021-07-18T00:00:00.000Z",
-      total_customers: 150105,
-      new_customers_added: 1938
+    date: "2021-07-18T00:00:00.000Z",
+    total_customers: 150105,
+    new_customers_added: 1938,
   },
   {
-      date: "2021-07-20T00:00:00.000Z",
-      total_customers: 152501,
-      new_customers_added: 2396
+    date: "2021-07-20T00:00:00.000Z",
+    total_customers: 152501,
+    new_customers_added: 2396,
   },
   {
-      date: "2021-07-21T00:00:00.000Z",
-      total_customers: 154500,
-      new_customers_added: 1999
+    date: "2021-07-21T00:00:00.000Z",
+    total_customers: 154500,
+    new_customers_added: 1999,
   },
   {
-      date: "2021-07-30T00:00:00.000Z",
-      total_customers: 155500,
-      new_customers_added: 1000
+    date: "2021-07-30T00:00:00.000Z",
+    total_customers: 155500,
+    new_customers_added: 1000,
   },
   {
-      date: "2021-08-01T00:00:00.000Z",
-      total_customers: 156300,
-      new_customers_added: 800
+    date: "2021-08-01T00:00:00.000Z",
+    total_customers: 156300,
+    new_customers_added: 800,
   },
   {
-      date: "2021-08-09T00:00:00.000Z",
-      total_customers: 156485,
-      new_customers_added: 185
+    date: "2021-08-09T00:00:00.000Z",
+    total_customers: 156485,
+    new_customers_added: 185,
   },
   {
-      date: "2021-08-10T00:00:00.000Z",
-      total_customers: 156485,
-      new_customers_added: 0
+    date: "2021-08-10T00:00:00.000Z",
+    total_customers: 156485,
+    new_customers_added: 0,
   },
   {
-      date: "2021-08-11T00:00:00.000Z",
-      total_customers: 156485,
-      new_customers_added: 0
+    date: "2021-08-11T00:00:00.000Z",
+    total_customers: 156485,
+    new_customers_added: 0,
   },
   {
-      date: "2021-08-12T00:00:00.000Z",
-      total_customers: 156485,
-      new_customers_added: 0
+    date: "2021-08-12T00:00:00.000Z",
+    total_customers: 156485,
+    new_customers_added: 0,
   },
   {
-      date: "2021-08-13T00:00:00.000Z",
-      total_customers: 156485,
-      new_customers_added: 0
+    date: "2021-08-13T00:00:00.000Z",
+    total_customers: 156485,
+    new_customers_added: 0,
   },
   {
-      date: "2021-08-14T00:00:00.000Z",
-      total_customers: 156485,
-      new_customers_added: 0
+    date: "2021-08-14T00:00:00.000Z",
+    total_customers: 156485,
+    new_customers_added: 0,
   },
   {
-      date: "2021-08-15T00:00:00.000Z",
-      total_customers: 156485,
-      new_customers_added: 0
+    date: "2021-08-15T00:00:00.000Z",
+    total_customers: 156485,
+    new_customers_added: 0,
   },
   {
-      date: "2021-08-16T00:00:00.000Z",
-      total_customers: 156485,
-      new_customers_added: 0
+    date: "2021-08-16T00:00:00.000Z",
+    total_customers: 156485,
+    new_customers_added: 0,
   },
   {
-      date: "2021-08-17T00:00:00.000Z",
-      total_customers: 156485,
-      new_customers_added: 0
+    date: "2021-08-17T00:00:00.000Z",
+    total_customers: 156485,
+    new_customers_added: 0,
   },
   {
-      date: "2021-08-18T00:00:00.000Z",
-      total_customers: 156485,
-      new_customers_added: 0
+    date: "2021-08-18T00:00:00.000Z",
+    total_customers: 156485,
+    new_customers_added: 0,
   },
   {
-      date: "2021-08-19T00:00:00.000Z",
-      total_customers: 156485,
-      new_customers_added: 0
+    date: "2021-08-19T00:00:00.000Z",
+    total_customers: 156485,
+    new_customers_added: 0,
   },
   {
-      date: "2021-08-20T00:00:00.000Z",
-      total_customers: 156485,
-      new_customers_added: 0
+    date: "2021-08-20T00:00:00.000Z",
+    total_customers: 156485,
+    new_customers_added: 0,
   },
   {
-      date: "2021-08-21T00:00:00.000Z",
-      total_customers: 156485,
-      new_customers_added: 0
+    date: "2021-08-21T00:00:00.000Z",
+    total_customers: 156485,
+    new_customers_added: 0,
   },
   {
-      date: "2021-08-22T00:00:00.000Z",
-      total_customers: 156485,
-      new_customers_added: 0
+    date: "2021-08-22T00:00:00.000Z",
+    total_customers: 156485,
+    new_customers_added: 0,
   },
   {
-      date: "2021-08-23T00:00:00.000Z",
-      total_customers: 156485,
-      new_customers_added: 0
+    date: "2021-08-23T00:00:00.000Z",
+    total_customers: 156485,
+    new_customers_added: 0,
   },
   {
-      date: "2021-08-24T00:00:00.000Z",
-      total_customers: 156485,
-      new_customers_added: 0
+    date: "2021-08-24T00:00:00.000Z",
+    total_customers: 156485,
+    new_customers_added: 0,
   },
   {
-      date: "2021-08-25T00:00:00.000Z",
-      total_customers: 156485,
-      new_customers_added: 0
+    date: "2021-08-25T00:00:00.000Z",
+    total_customers: 156485,
+    new_customers_added: 0,
   },
   {
-      date: "2021-08-26T00:00:00.000Z",
-      total_customers: 156485,
-      new_customers_added: 0
+    date: "2021-08-26T00:00:00.000Z",
+    total_customers: 156485,
+    new_customers_added: 0,
   },
   {
-      date: "2021-08-27T00:00:00.000Z",
-      total_customers: 156485,
-      new_customers_added: 0
+    date: "2021-08-27T00:00:00.000Z",
+    total_customers: 156485,
+    new_customers_added: 0,
   },
   {
-      date: "2021-08-28T00:00:00.000Z",
-      total_customers: 156485,
-      new_customers_added: 0
+    date: "2021-08-28T00:00:00.000Z",
+    total_customers: 156485,
+    new_customers_added: 0,
   },
   {
-      date: "2021-08-29T00:00:00.000Z",
-      total_customers: 156485,
-      new_customers_added: 0
+    date: "2021-08-29T00:00:00.000Z",
+    total_customers: 156485,
+    new_customers_added: 0,
   },
   {
-      date: "2021-08-30T00:00:00.000Z",
-      total_customers: 156485,
-      new_customers_added: 0
+    date: "2021-08-30T00:00:00.000Z",
+    total_customers: 156485,
+    new_customers_added: 0,
   },
   {
-      date: "2021-08-31T00:00:00.000Z",
-      total_customers: 156485,
-      new_customers_added: 0
-  }
+    date: "2021-08-31T00:00:00.000Z",
+    total_customers: 156485,
+    new_customers_added: 0,
+  },
 ]
