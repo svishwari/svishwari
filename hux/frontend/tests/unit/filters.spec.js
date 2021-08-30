f1f7eeb0a96d56a25872474498f204137cec8285
Singh <sameerksingh@deloitte.com>
2021-08-30 21:55:49 +0530

    lintfix

--- hux/frontend/tests/unit/filters.spec.js
+++ hux/frontend/tests/unit/filters.spec.js
@@ -16,7 +16,7 @@ describe("Filters", () => {
 
       testdate = dayjs().subtract(1, "month")
       expect(filters.Date(testdate.format(), "relative")).toEqual("a month ago")
-      
+
       testdate = dayjs().subtract(42, "minute")
       expect(filters.Date(testdate.format(), "relative", false)).toEqual(
         "42 minutes ago"
