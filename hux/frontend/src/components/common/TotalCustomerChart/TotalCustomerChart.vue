ead7f5dcd9546517e5774d9a29cb2c6212cd6ea0
sach9993 <sacojha@deloitte.com>
2021-08-31 19:13:15 +0530

    test changes !!!

--- hux/frontend/src/components/common/TotalCustomerChart/TotalCustomerChart.vue
+++ hux/frontend/src/components/common/TotalCustomerChart/TotalCustomerChart.vue
@@ -28,6 +28,7 @@ export default {
   },
   data() {
     return {
+      // change test !!!
       show: false,
       isArcHover: false,
       colorCodes: ["columbiaBlue", "info", "pantoneBlue", "success"],
