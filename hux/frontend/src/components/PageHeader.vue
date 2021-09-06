6c2f155f3cd9a255a0802f1caf8fd830fd66481a
Abdullah-Azad <mdazad@deloitte.com>
2021-09-06 14:48:42 +0530

    missed to apply

--- hux/frontend/src/components/PageHeader.vue
+++ hux/frontend/src/components/PageHeader.vue
@@ -77,6 +77,6 @@ export default {
 
 <style lang="scss" scoped>
 .page-header--wrap {
-  border-bottom: 1px solid var(--v-lightGrey-base) !important;
+  border-bottom: 1px solid var(--v-black-lighten3) !important;
 }
 </style>
