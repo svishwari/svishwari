6bfc42e81b24fb5e1aca8e376b6939c8a5f6805c
soumya-pb <soumbehera@deloitte.com>
2021-09-06 21:28:28 +0530

    Test Fix

--- hux/api/huxunify/test/data_connectors/test_cdp.py
+++ hux/api/huxunify/test/data_connectors/test_cdp.py
@@ -134,7 +134,7 @@ class CDPTest(TestCase):
         Returns:
             None
         """
-        start_date = datetime.utcnow().date() - relativedelta(months=6)
+        start_date = datetime.utcnow().date() - relativedelta(months=9)
         end_date = datetime.utcnow().date()
         expected_response = {
             "code": 200,
