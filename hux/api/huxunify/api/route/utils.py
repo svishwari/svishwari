1ece5a5ca8de5874a55cd1c23758167756cd880b
risheek-deloitte <rgchandra@deloitte.com>
2021-09-09 10:27:19 +0530

    Checking null values

--- hux/api/huxunify/api/route/utils.py
+++ hux/api/huxunify/api/route/utils.py
@@ -263,7 +263,9 @@ def group_gender_spending(gender_spending: list) -> dict:
                 constants.DATE: date_parser(
                     x[constants.MONTH], x[constants.YEAR]
                 ),
-                constants.LTV: round(x[constants.AVG_SPENT_WOMEN], 4),
+                constants.LTV: round(x[constants.AVG_SPENT_WOMEN], 4)
+                if x[constants.AVG_SPENT_WOMEN]
+                else 0,
             }
             for x in gender_spending
         ],
@@ -272,7 +274,9 @@ def group_gender_spending(gender_spending: list) -> dict:
                 constants.DATE: date_parser(
                     x[constants.MONTH], x[constants.YEAR]
                 ),
-                constants.LTV: round(x[constants.AVG_SPENT_MEN], 4),
+                constants.LTV: round(x[constants.AVG_SPENT_MEN], 4)
+                if x[constants.AVG_SPENT_MEN]
+                else 0,
             }
             for x in gender_spending
         ],
@@ -281,7 +285,9 @@ def group_gender_spending(gender_spending: list) -> dict:
                 constants.DATE: date_parser(
                     x[constants.MONTH], x[constants.YEAR]
                 ),
-                constants.LTV: round(x[constants.AVG_SPENT_OTHER], 4),
+                constants.LTV: round(x[constants.AVG_SPENT_OTHER], 4)
+                if x[constants.AVG_SPENT_OTHER]
+                else 0,
             }
             for x in gender_spending
         ],
