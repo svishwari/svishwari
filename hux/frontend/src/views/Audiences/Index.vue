ed01ad842f6e6a9ffa8ef80534da3bb654a2a878
mohitbans <74055271+mohitbans@users.noreply.github.com>
2021-09-22 13:03:27 +0530

    changed name

--- hux/frontend/src/views/Audiences/Index.vue
+++ hux/frontend/src/views/Audiences/Index.vue
@@ -378,7 +378,7 @@ export default {
         { title: "Favorite", isDisabled: true },
         { title: "Export", isDisabled: true },
         {
-          title: "Edit",
+          title: "Edit audience",
           isDisabled: false,
           onClick: () => {
             this.editAudience(audience.id)
