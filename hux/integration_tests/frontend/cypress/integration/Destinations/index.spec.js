a183af474a0dbd4a43de82a1b00fe5e8931ea418
Singh <sameerksingh@deloitte.com>
2021-09-28 00:57:03 +0530

    wait removed comments added

--- hux/integration_tests/frontend/cypress/integration/Destinations/index.spec.js
+++ hux/integration_tests/frontend/cypress/integration/Destinations/index.spec.js
@@ -27,22 +27,30 @@ describe("Tests data souces and destinations in connections", () => {
     
     cy.get("@addableDestinations").its("length").then((addableDestinations) => {
         if(addableDestinations > 0){
+            //if a destination can be added
             cy.get("@addableDestinations").eq(0).click()
+
+            //configure destination
             cy.get(selector.destination.destinationConfigDetails).get("input").each(($el) => {
                 cy.wrap($el).type("123456")
             })
             cy.get(selector.destination.validateDestination).click()
             cy.get(selector.destination.validateDestination).contains("Success")
-            cy.get(selector.destination.footer).contains("return").click()
-            cy.wait(1000)
 
+            //Add and return
+            cy.get(selector.destination.footer).contains("return").click()
             cy.location("pathname").should("eq", route.connections)
+
+            //verify if number of destinations incremented by 1
             cy.get("@destinationsCount").then((destinationsCount) => {
                 cy.get(selector.destinations).its("length").should("eq",destinationsCount + 1)
             })
         }
         else{
+            //if no destination can be added, cancel
             cy.get(selector.destination.footer).contains("Cancel").click()
+
+            //verify no change in number of destinations
             cy.get("@destinationsCount").then((destinationsCount) => {
                 cy.get(selector.destinations).its("length").should("eq",destinationsCount)
             })
