4c8e0e24a94ac36268c609158a3ae42c435a6c5f
Doug <90864188+dcl-deloitte@users.noreply.github.com>
2021-09-27 11:00:39 -0600

    added notification get function

--- lib/huxunifylib-database/huxunifylib/database/notification_management.py
+++ lib/huxunifylib-database/huxunifylib/database/notification_management.py
@@ -73,13 +73,13 @@ def create_notification(
     wait=wait_fixed(c.CONNECT_RETRY_INTERVAL),
     retry=retry_if_exception_type(pymongo.errors.AutoReconnect),
 )
-def get_notifications(
+def get_notifications_batch(
     database: DatabaseClient,
     batch_size: int,
     sort_order: int,
     batch_number: int,
 ) -> Union[dict, None]:
-    """A function to get notifications
+    """A function to get notifications per batch size.
 
     Args:
         database (DatabaseClient): A database client.
@@ -107,6 +107,50 @@ def get_notifications(
                 .sort([(c.NOTIFICATION_FIELD_CREATED, -1), (c.ID, sort_order)])
                 .skip(skips)
                 .limit(batch_size)
+            ),
+        )
+    except pymongo.errors.OperationFailure as exc:
+        logging.error(exc)
+
+    return None
+
+
+@retry(
+    wait=wait_fixed(c.CONNECT_RETRY_INTERVAL),
+    retry=retry_if_exception_type(pymongo.errors.AutoReconnect),
+)
+def get_notifications(
+    database: DatabaseClient,
+    query_filter: Union[dict, None] = None,
+    sort_order: Union[dict, None] = None,
+) -> Union[dict, None]:
+    """A function to get notifications
+
+    Args:
+        database (DatabaseClient): A database client.
+        query_filter (Union[list[Tuple], None]): Mongo filter Query.
+        sort_order (Tuple[str, int]): Mongo sort order.
+
+    Returns:
+        Union[dict, None]: MongoDB notification documents with total count of notifications .
+
+    """
+
+    # get collection
+    collection = database[c.DATA_MANAGEMENT_DATABASE][
+        c.NOTIFICATIONS_COLLECTION
+    ]
+
+    # check if filter query is None.
+    query_filter = query_filter if query_filter else {}
+
+    try:
+        return dict(
+            total_records=collection.count_documents(query_filter),
+            notifications=list(
+                collection.find(query_filter).sort(
+                    sort_order if sort_order else []
+                )
             ),
         )
     except pymongo.errors.OperationFailure as exc:
