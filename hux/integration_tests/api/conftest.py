7b54827125c5f50c2ca2d9e155032056f8545a3a
Doug <90864188+dcl-deloitte@users.noreply.github.com>
2021-09-28 01:46:13 -0600

    added param dict

--- hux/integration_tests/api/conftest.py
+++ hux/integration_tests/api/conftest.py
@@ -5,12 +5,14 @@ from _pytest.config import Config
 from get_okta_token import OktaOIDC
 
 # ENV VARS
-ORG_URL = getenv("OKTA_ISSUER")
-CLIENT_ID = getenv("OKTA_CLIENT_ID")
-USER = getenv("OKTA_TEST_USER_NAME")
-PW = getenv("OKTA_TEST_USER_PW")
-REDIRECT_URI = getenv("OKTA_REDIRECT_URI")
-SCOPES = "openid+profile+email"
+OKTA_PARAM_DICT = {
+    "org_url": getenv("OKTA_ISSUER"),
+    "user": getenv("OKTA_TEST_USER_NAME"),
+    "pw": getenv("OKTA_TEST_USER_PW"),
+    "client_id": getenv("OKTA_CLIENT_ID"),
+    "scopes": "openid+profile+email",
+    "redirect_uri": getenv("OKTA_REDIRECT_URI"),
+}
 ACCESS_TOKEN = "ACCESS_TOKEN"
 
 
@@ -28,14 +30,7 @@ def pytest_configure(config: Config):
 
     try:
         # setup the oidc class.
-        okta_oidc = OktaOIDC(
-            ORG_URL,
-            USER,
-            PW,
-            CLIENT_ID,
-            SCOPES,
-            REDIRECT_URI,
-        )
+        okta_oidc = OktaOIDC(**OKTA_PARAM_DICT)
 
         # set the token for pytest usage.
         environ[ACCESS_TOKEN] = okta_oidc.get_access_token()
