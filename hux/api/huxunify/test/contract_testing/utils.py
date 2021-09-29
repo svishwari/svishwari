f5202d6fb5a1ab33443ea44aef249616be4d5cc8
risheek-deloitte <rgchandra@deloitte.com>
2021-09-29 19:03:58 +0530

    Style fix

--- hux/api/huxunify/test/contract_testing/utils.py
+++ hux/api/huxunify/test/contract_testing/utils.py
@@ -4,6 +4,13 @@ Utils for contract testing
 from huxunifylib.util.general.logging import logger
 
 
-def upload_pact_files_to_s3(folder: str):
-    logger.info(f"Uploading pacts from {folder} to S3")
+def upload_pact_files_to_s3(folder: str) -> None:
+    """Uploads pacts to required s3 buckets.
+     Args:
+        folder (str) : Path to folder where pact files are located.
+
+    Returns:
+        None
+    """
+    logger.info("Uploading pacts from %s to S3", folder)
     # TODO Upload contracts to S3
