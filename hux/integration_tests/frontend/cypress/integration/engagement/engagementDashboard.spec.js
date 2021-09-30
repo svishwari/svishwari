cd6c081eb197932d9473f4acbdf3451e65532dcb
sach9993 <sacojha@deloitte.com>
2021-09-30 23:11:18 +0530

    dashboard changes !!!

--- hux/integration_tests/frontend/cypress/integration/engagement/engagementDashboard.spec.js
+++ hux/integration_tests/frontend/cypress/integration/engagement/engagementDashboard.spec.js
@@ -1,7 +1,7 @@
 import route from "../../support/routes.js"
 import selector from "../../support/selectors.js"
 
-describe("Orchestration > Engagement", () => {
+describe("Orchestration > Engagement > Engagement Dashboard", () => {
   before(() => {
     cy.signin({
       email: Cypress.env("USER_EMAIL"),
@@ -9,7 +9,18 @@ describe("Orchestration > Engagement", () => {
     })
   })
 
-  it("should be able to navigate to engagement and click on add engagement button", () => {
+  it("should be able to navigate to engagement and select one of the engagement", () => {
+    // after login land in the overview page
+    cy.location("pathname").should("eq", route.overview)
+    //click on engagement on side nav bar and route in engagement screen
+    cy.get(selector.engagements).click()
+    cy.location("pathname").should("eq", route.engagements)
+    //click over the first engagement name
+    cy.get("a[href='/engagements/2']").click()
+   // cy.get(selector.engagement.engagementCollection.eq(0).find('div.d-flex > button > a')).click()
+  })
+
+  it("should be able to view the engagement dashboard overview", () => {
     // after login land in the overview page
     cy.location("pathname").should("eq", route.overview)
     //click on engagement on side nav bar and route in engagement screen
@@ -17,43 +28,61 @@ describe("Orchestration > Engagement", () => {
     cy.location("pathname").should("eq", route.engagements)
     //click on add engagement button
     cy.get("a[href='/engagements/2']").click()
-    
    // cy.get(selector.engagement.engagementCollection.eq(0).find('div.d-flex > button > a')).click()
   })
 
-//   it("should fill new engagement name and description", () => {
-//     // add new engagement name
-//     cy.get(selector.engagement.enagagementName).eq(0).type("Test Engagement")
-//     // add new engagement description
-//     cy.get(selector.engagement.enagagementDescription)
-//       .eq(0)
-//       .type("Engagement for E2E testing")
-//   })
+  it("should be able to view the audiences", () => {
+    // after login land in the overview page
+    cy.location("pathname").should("eq", route.overview)
+    //click on engagement on side nav bar and route in engagement screen
+    cy.get(selector.engagements).click()
+    cy.location("pathname").should("eq", route.engagements)
+    //click on add engagement button
+    cy.get("a[href='/engagements/2']").click()
+   // cy.get(selector.engagement.engagementCollection.eq(0).find('div.d-flex > button > a')).click()
+  })
 
-//   it("should add audience to the engagement", () => {
-//     // Click on add audience icon
-//     cy.get(selector.engagement.addAudience).click()
-//     // Select the first audience from the existing ones
-//     cy.get(selector.engagement.selectAudience).eq(0).click()
-//     // Click on outside the close the add audience Drawer
-//     cy.get(selector.engagement.exitDrawer).click()
-//   })
+  it("should be able to view the audiences", () => {
+    // after login land in the overview page
+    cy.location("pathname").should("eq", route.overview)
+    //click on engagement on side nav bar and route in engagement screen
+    cy.get(selector.engagements).click()
+    cy.location("pathname").should("eq", route.engagements)
+    //click on add engagement button
+    cy.get("a[href='/engagements/2']").click()
+   // cy.get(selector.engagement.engagementCollection.eq(0).find('div.d-flex > button > a')).click()
+  })
 
-//   it("should add destination to the engagement", () => {
-//     // Click on add audience icon
-//     cy.get(selector.engagement.addDestination).click()
-//     // Select the first audience from the existing ones
-//     cy.get(selector.engagement.selectDestination).eq(0).click()
-//   })
+  it("should be able to validate the delivery history", () => {
+    // after login land in the overview page
+    cy.location("pathname").should("eq", route.overview)
+    //click on engagement on side nav bar and route in engagement screen
+    cy.get(selector.engagements).click()
+    cy.location("pathname").should("eq", route.engagements)
+    //click on add engagement button
+    cy.get("a[href='/engagements/2']").click()
+   // cy.get(selector.engagement.engagementCollection.eq(0).find('div.d-flex > button > a')).click()
+  })
 
-//   it("should add destination data extensions and verify the configuration", () => {
-//     // Add new data extension name
-//     cy.get(selector.engagement.dataExtensionName).eq(0).type("Testing")
-//     // Close the data extension drawer
-//     cy.get(selector.engagement.exitDataExtensionDrawer).click()
-//     // eslint-disable-next-line cypress/no-unnecessary-waiting
-//     cy.wait(1000)
-//     // Close the add destination drawer (as per updated flow)
-//     cy.get(selector.engagement.exitDrawer).click()
-//   })
+  it("should be able to validate the audience performance data for digital advertising", () => {
+    // after login land in the overview page
+    cy.location("pathname").should("eq", route.overview)
+    //click on engagement on side nav bar and route in engagement screen
+    cy.get(selector.engagements).click()
+    cy.location("pathname").should("eq", route.engagements)
+    //click on add engagement button
+    cy.get("a[href='/engagements/2']").click()
+   // cy.get(selector.engagement.engagementCollection.eq(0).find('div.d-flex > button > a')).click()
+  })
+
+  it("should be able to validate the audience performance data for email marketing", () => {
+    // after login land in the overview page
+    cy.location("pathname").should("eq", route.overview)
+    //click on engagement on side nav bar and route in engagement screen
+    cy.get(selector.engagements).click()
+    cy.location("pathname").should("eq", route.engagements)
+    //click on add engagement button
+    cy.get("a[href='/engagements/2']").click()
+   // cy.get(selector.engagement.engagementCollection.eq(0).find('div.d-flex > button > a')).click()
+  })
 })
