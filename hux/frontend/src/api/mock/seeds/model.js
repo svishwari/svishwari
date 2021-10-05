781c85bd9c0e8846cb5d946085371b5563fb80ca
Singh <sameerksingh@deloitte.com>
2021-10-05 22:06:42 +0530

    requested changes addressed

--- hux/frontend/src/api/mock/seeds/model.js
+++ hux/frontend/src/api/mock/seeds/model.js
@@ -4,17 +4,22 @@ export default [
     status: "Active",
     type: "ltv",
     id: "2",
+    description: "Predict the lifetime value of a customer.",
   },
   {
     name: "Propensity to Purchase",
     status: "Pending",
     type: "purchase",
     id: "3",
+    description:
+      "Propensity of a customer making a purchase after receiving an email.",
   },
   {
     name: "Propensity to Unsubscribe",
     status: "Active",
     type: "unsubscribe",
     id: "1",
+    description:
+      "Predicts the propensity of a customer to unsubscribe from an email list.",
   },
 ]
