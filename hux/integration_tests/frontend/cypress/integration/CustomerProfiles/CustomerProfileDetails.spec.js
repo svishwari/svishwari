f6fd018cc9b0bd00957a4194715197ed88bda1fe
Henrik Rypkema <henrikr@users.noreply.github.com>
2021-10-04 21:49:46 -0400

    Combine multiple assertions.
    Okta has trouble fetching an access token between test cases, so its best to combine assertions into one test case.
    It also complies with best practices
    
    See: https://docs.cypress.io/guides/references/best-practices#Creating-tiny-tests-with-a-single-assertion

--- hux/integration_tests/frontend/cypress/integration/CustomerProfiles/CustomerProfileDetails.spec.js
+++ hux/integration_tests/frontend/cypress/integration/CustomerProfiles/CustomerProfileDetails.spec.js
@@ -13,9 +13,10 @@ describe("Data management > Customer Profiles > Customer Profiles Dashboard", ()
     //click on customer profiles on side nav bar
     cy.get(selector.customerProfile.customers).click()
     cy.location("pathname").should("eq", route.customerProfiles)
-  })
 
-  it("should be able to open drawer & navigate to profile dashboard", () => {
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(5000)
+
     // click on view all customer profiles button
     cy.get(selector.customerProfile.viewAllCustomers).click()
 
@@ -23,46 +24,38 @@ describe("Data management > Customer Profiles > Customer Profiles Dashboard", ()
     cy.get(selector.customerProfile.customerID).first().click()
 
     // TODO: remove the need to re-login
-    cy.reLogin()
-  })
+    // cy.reLogin()
 
-  // Inside customer profile dashboard
-  it("should validate customer length card", () => {
+    // should validate customer length card
     cy.get(selector.customerProfile.customerlength).should("exist")
-  })
 
-  it("should have (Match Confidence, Life-Time Value, Conversion Time card)", () => {
+    // should have (Match Confidence, Life-Time Value, Conversion Time card)
     cy.get(selector.customerProfile.matchConfidence).should("exist")
     cy.get(selector.customerProfile.lifeTimeValue).should("exist")
     cy.get(selector.customerProfile.conversionTime).should("exist")
-  })
 
-  it("should have (Churn score, Last click, Last purchase date) card", () => {
+    // should have (Churn score, Last click, Last purchase date) card
     cy.get(selector.customerProfile.churnScore).should("exist")
     cy.get(selector.customerProfile.lastClick).should("exist")
     cy.get(selector.customerProfile.lastPurchaseDate).should("exist")
-  })
 
-  it("should have (Last open, Customer Insights date) card", () => {
+    // should have (Last open, Customer Insights date) card
     cy.get(selector.customerProfile.lastOpen).should("exist")
     cy.get(selector.customerProfile.customerInsights).should("exist")
-  })
 
-  it("should ++REDACTED++ value in customer insights table", () => {
+    // should ++REDACTED++ value in customer insights table
     cy.get("table").contains("td", "++REDACTED++")
-  })
 
-  it("should validate contact preferencecs card", () => {
+    // should validate contact preferencecs card
     cy.get(selector.customerProfile.contactPreferencecs).should("exist")
-  })
 
-  it("should have a customer's contact preferences (Email, Push, SMS, In-App)", () => {
+    // should have a customer's contact preferences (Email, Push, SMS, In-App)
     cy.get("table").contains("td", "Email")
     cy.get("table").contains("td", "Push")
     cy.get("table").contains("td", "SMS")
     cy.get("table").contains("td", "In-App")
-  })
-  it("should validate chord card", () => {
+
+    // should validate chord card
     cy.get(selector.customerProfile.chord).should("exist")
   })
 })
--- hux/integration_tests/frontend/cypress/integration/engagement/createEngagement.spec.js
+++ hux/integration_tests/frontend/cypress/integration/engagement/createEngagement.spec.js
@@ -9,7 +9,8 @@ describe("Orchestration > Engagement > Create Engagement", () => {
     })
   })
 
-  it("should be able to navigate to engagement and click on add engagement button", () => {
+  it("should be able to configure a new engagement", () => {
+    // should be able to navigate to engagement and click on add engagement button
     // after login land in the overview page
     cy.location("pathname").should("eq", route.overview)
     //click on engagement on side nav bar and route in engagement screen
@@ -17,27 +18,24 @@ describe("Orchestration > Engagement > Create Engagement", () => {
     cy.location("pathname").should("eq", route.engagements)
     //click on add engagement button
     cy.get(selector.engagement.addEngagements).click()
-  })
 
-  it("should fill new engagement name and description", () => {
+    // should fill new engagement name and description
     // add new engagement name
     cy.get(selector.engagement.enagagementName).eq(0).type("Test Engagement")
     // add new engagement description
     cy.get(selector.engagement.enagagementDescription)
       .eq(0)
       .type("Engagement for E2E testing")
-  })
 
-  it("should add audience to the engagement", () => {
+    // should add audience to the engagement
     // Click on add audience icon
     cy.get(selector.engagement.addAudience).click()
     // Select the first audience from the existing ones
     cy.get(selector.engagement.selectAudience).eq(0).click()
     // Click on outside the close the add audience Drawer
     cy.get(selector.engagement.exitDrawer).click()
-  })
 
-  it("should add destination to the engagement", () => {
+    // should add destination to the engagement
     // Click on add audience icon
     cy.get(selector.engagement.addDestination).click()
     // Select the first audience from the existing ones
