1cdf523ff4fc80564800c7baf7bf52f2ff085e61
panch mukesh <ppanchmukesh@deloitte.com>
2021-10-13 23:39:24 +0530

    api style fix

--- hux/api/huxunify/api/route/notifications.py
+++ hux/api/huxunify/api/route/notifications.py
@@ -30,7 +30,9 @@ from huxunify.api import constants as api_c
 from huxunify.api.schema.utils import AUTH401_RESPONSE
 
 # setup the notifications blueprint
-notifications_bp = Blueprint(api_c.NOTIFICATIONS_ENDPOINT, import_name=__name__)
+notifications_bp = Blueprint(
+    api_c.NOTIFICATIONS_ENDPOINT, import_name=__name__
+)
 
 
 @notifications_bp.before_request
@@ -196,7 +198,9 @@ class NotificationStream(SwaggerView):
                 # get the previous time, take last minute.
                 previous_time = datetime.utcnow().replace(
                     tzinfo=timezone.utc
-                ) - timedelta(minutes=int(api_c.NOTIFICATION_STREAM_TIME_SECONDS / 60))
+                ) - timedelta(
+                    minutes=int(api_c.NOTIFICATION_STREAM_TIME_SECONDS / 60)
+                )
 
                 # dump the output notification list to the notification schema.
                 yield json.dumps(
@@ -204,7 +208,9 @@ class NotificationStream(SwaggerView):
                         notification_management.get_notifications(
                             get_db_client(),
                             {
-                                db_c.NOTIFICATION_FIELD_CREATED: {"$gt": previous_time},
+                                db_c.NOTIFICATION_FIELD_CREATED: {
+                                    "$gt": previous_time
+                                },
                                 db_c.TYPE: db_c.NOTIFICATION_TYPE_SUCCESS,
                                 db_c.NOTIFICATION_FIELD_DESCRIPTION: {
                                     "$regex": "^Successfully delivered audience"
@@ -270,7 +276,9 @@ class NotificationSearch(SwaggerView):
                 "Could not find notification with id %s.",
                 notification_id,
             )
-            return {"message": api_c.NOTIFICATION_NOT_FOUND}, HTTPStatus.NOT_FOUND
+            return {
+                "message": api_c.NOTIFICATION_NOT_FOUND
+            }, HTTPStatus.NOT_FOUND
 
         return (
             NotificationSchema().dump(notification),
