ae41e6b2563c3ff31c5275b52b1a6e1446baeecd
mdazad <mdazad@deloitte.com>
2021-11-17 17:45:41 +0530

    added test case for sortby funtion in utility

--- hux/frontend/tests/unit/utils.spec.js
+++ hux/frontend/tests/unit/utils.spec.js
@@ -3,6 +3,7 @@ import {
   listOfYears,
   endOfMonth,
   arrayHasFieldWithMultipleValues,
+  sortByName,
 } from "@/utils"
 
 describe("Utils", () => {
@@ -171,4 +172,24 @@ describe("Utils", () => {
       expect(actualNullUndefined).toEqual(expectedNullUndefined)
     })
   })
+
+  describe("sortByName", () => {
+    it("Should sort data based on key provided", () => {
+      const data = {
+        'purchase': [
+          {
+            id: 1,
+            name: "Propensity to open"
+          },
+          {
+            id: 2,
+            name: "Churn"
+          },
+        ]
+      }
+      sortByName(data, 'name')
+      expect(data['purchase'][0].id).toEqual(2)
+    })
+  })
+  
 })
