8a689debb930af9e15f27e3da6479de6249c4772
risheek-deloitte <rgchandra@deloitte.com>
2021-11-24 23:35:26 +0530

    Adding period.

--- lib/huxunifylib-database/huxunifylib/database/tests/test_delivery_platform.py
+++ lib/huxunifylib-database/huxunifylib/database/tests/test_delivery_platform.py
@@ -2009,7 +2009,6 @@ class TestDeliveryPlatform(unittest.TestCase):
     @mongomock.patch(servers=(("localhost", 27017),))
     def test_set_job_in_pending_within_timeout_collection(self):
         """Test set delivery job in pending_within_timeout collection."""
-
         delivery_job_id = self._set_delivery_job()
         doc_for_delivery_job = (
             dpm.set_job_in_pending_within_timeout_collection(
@@ -2025,7 +2024,7 @@ class TestDeliveryPlatform(unittest.TestCase):
         """Test update delivery jobs status."""
 
         delivery_jobs = dpm.get_delivery_jobs(self.database)
-        # Insert a delivery job in pending_within_timeout collection
+        # Insert a delivery job in pending_within_timeout collection.
         dpm.set_job_in_pending_within_timeout_collection(
             self.database, delivery_jobs[0][c.ID]
         )
