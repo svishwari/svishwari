2696dd4b5a85e1c07970cbfe46178505fa630d22
mdazad <mdazad@deloitte.com>
2021-12-06 14:15:51 +0530

    added empty screen

--- hux/frontend/src/views/Configuration/index.vue
+++ hux/frontend/src/views/Configuration/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <page max-width="100%">
+  <page max-width="100%"  class="config-wrapper">
     <div slot="header">
       <page-header header-height="110">
         <template slot="left">
@@ -9,16 +9,38 @@
           </div>
         </template>
       </page-header>
+      <v-progress-linear :active="loading" :indeterminate="loading" />
+    </div>
+    <div class="config-content" v-if="!loading">
+      <v-row v-if="false" class="ma-0 pa-2">
+        <v-col>
+          <!-- UI to show when configuration is activated -->
+        </v-col>
+      </v-row>
+      <hux-empty
+        v-if="isConfigActivated"
+        class="config-activating"
+        icon-type="settings"
+        :icon-size="50"
+        title="Nothing to show"
+        subtitle="Configuration is currently being activated. Please check back later. Thank you!"
+      >
+      </hux-empty>
     </div>
   </page>
 </template>
 
 <script>
+import { mapGetters } from "vuex"
+
 import PageHeader from "@/components/PageHeader"
+import Page from "@/components/Page"
 import Breadcrumb from "@/components/common/Breadcrumb"
+import HuxEmpty from "@/components/common/screens/Empty"
+
 export default {
   name: "Configuration",
-  components: { PageHeader, Breadcrumb },
+  components: { PageHeader, Page, Breadcrumb, HuxEmpty },
   data() {
     return {
       breadcrumbItems: [
@@ -28,9 +50,30 @@ export default {
           icon: null,
         },
       ],
+      loading: false,
     }
+  },
+  computed: {
+    ...mapGetters({ }),
+
+    isConfigActivated() {
+      return true
+    },
   },
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.config-wrapper {
+  .config-content {
+    .config-activating {
+     ::v-deep .text-center {
+        width: 470px !important;
+        .text-body-2 {
+          line-height: 22px !important;
+        }
+      }
+    }
+  }
+}
+</style>
