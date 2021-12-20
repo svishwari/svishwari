75f6a51be7fea3f630bcca9f93828a552f4c6608
shubhamkumar74 <91964975+shubhamkumar74@users.noreply.github.com>
2021-12-20 19:17:40 +0530

    fixed lint issues

--- hux/frontend/tests/unit/filters.spec.js
+++ hux/frontend/tests/unit/filters.spec.js
@@ -55,7 +55,9 @@ describe("Filters", () => {
     })
 
     it("should show as 'calendar' format", () => {
-      expect(filters.Date("2021-12-25", "calendar")).toEqual("Saturday at 12:00 AM")
+      expect(filters.Date("2021-12-25", "calendar")).toEqual(
+        "Saturday at 12:00 AM"
+      )
       let testdate = dayjs().subtract(1, "day")
       expect(filters.Date(testdate.format(), "calendar")).toEqual(
         testdate.format("[Yesterday at] h:mm A")
