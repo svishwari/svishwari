418888273c51461a6603eff41ff7a16ab31e972d
skondalraj-deloitte <skondalraj@deloitte.com>
2021-12-23 02:03:28 +0530

    HUS-1885: Black formatting

--- hux/api/huxunify/api/constants.py
+++ hux/api/huxunify/api/constants.py
@@ -590,7 +590,9 @@ DESTINATION_PATCH_FIELDS = [
     db_c.CLIENT_ACCOUNT,
     db_c.USE_CASE,
 ]
-DESTINATION_INVALID_PATCH_MESSAGE = f"Acceptable fields are {DESTINATION_PATCH_FIELDS}."
+DESTINATION_INVALID_PATCH_MESSAGE = (
+    f"Acceptable fields are {DESTINATION_PATCH_FIELDS}."
+)
 
 ONLY_ADDED = "only_added"
 DELETE_DATASOURCES_SUCCESS = "Successfully deleted data source(s) - {}."
@@ -604,8 +606,12 @@ BSON_INVALID_ID = (
     f"12-byte input or a 24-character hex string"
 )
 MESSAGE = "message"
-FAILED_DEPENDENCY_CONNECTION_ERROR_MESSAGE = "Failed connecting to dependent API."
-FAILED_DEPENDENCY_ERROR_MESSAGE = "Failed to obtain data from dependent API endpoint."
+FAILED_DEPENDENCY_CONNECTION_ERROR_MESSAGE = (
+    "Failed connecting to dependent API."
+)
+FAILED_DEPENDENCY_ERROR_MESSAGE = (
+    "Failed to obtain data from dependent API endpoint."
+)
 EMPTY_RESPONSE_DEPENDENCY_ERROR_MESSAGE = (
     "Returned empty object from dependent API endpoint."
 )
@@ -613,8 +619,12 @@ EMPTY_RESPONSE_DEPENDENCY_ERROR_MESSAGE = (
 EMPTY_OBJECT_ERROR_MESSAGE = "Data not provided."
 DUPLICATE_NAME = "Name already exists."
 SFMC_CONFIGURATION_MISSING = "SFMC data extension config object missing."
-PERFORMANCE_METRIC_DE_NOT_ASSIGNED = "Performance metrics data extension not assigned."
-CAMPAIGN_ACTIVITY_DE_NOT_ASSIGNED = "Campaign activity data extension not assigned."
+PERFORMANCE_METRIC_DE_NOT_ASSIGNED = (
+    "Performance metrics data extension not assigned."
+)
+CAMPAIGN_ACTIVITY_DE_NOT_ASSIGNED = (
+    "Campaign activity data extension not assigned."
+)
 SAME_PERFORMANCE_CAMPAIGN_ERROR = (
     "Performance metric and Campaign activity cannot be same"
 )
@@ -632,7 +642,9 @@ DESTINATION_NOT_SUPPORTED = "Destination is not supported."
 SUCCESSFUL_DELIVERY_JOB_NOT_FOUND = "No successful delivery job found"
 ZERO_AUDIENCE_SIZE = "Sum of Audience(s) is zero"
 ENGAGEMENT_NO_AUDIENCES = "Engagement has no audiences."
-AUDIENCE_NOT_ATTACHED_TO_ENGAGEMENT = "Audience not attached to the engagement."
+AUDIENCE_NOT_ATTACHED_TO_ENGAGEMENT = (
+    "Audience not attached to the engagement."
+)
 DESTINATION_NOT_ATTACHED_ENGAGEMENT_AUDIENCE = (
     "Destination not attached to the engagement audience."
 )
@@ -867,7 +879,8 @@ ATTRIBUTE = "attribute"
 
 PARAM_STORE_PREFIX = "unified"
 PARAMETER_STORE_ERROR_MSG = (
-    "An error occurred while attempting to" " store secrets in the parameter store."
+    "An error occurred while attempting to"
+    " store secrets in the parameter store."
 )
 
 # users
@@ -1042,7 +1055,9 @@ CUSTOMER_OVERVIEW_DEFAULT_FILTER = {
     "filters": [
         {
             "section_aggregator": "ALL",
-            "section_filters": [{"field": "country", "type": "equals", "value": "US"}],
+            "section_filters": [
+                {"field": "country", "type": "equals", "value": "US"}
+            ],
         }
     ]
 }
@@ -1120,7 +1135,9 @@ AWS_BATCH_CONNECTION_HEALTH = "aws_batch_connection_health"
 AWS_S3_CONNECTION_HEALTH = "aws_s3_connection_health"
 AWS_EVENTS_CONNECTION_HEALTH = "aws_events_connection_health"
 CDM_API_CONNECTION_HEALTH = "cdm_api_connection_health"
-CDM_CONNECTION_SERVICE_CONNECTION_HEALTH = "cdm_connection_service_connection_health"
+CDM_CONNECTION_SERVICE_CONNECTION_HEALTH = (
+    "cdm_connection_service_connection_health"
+)
 
 # CDM API constants
 CDM_CONNECTIONS_ENDPOINT = "connections"
@@ -1200,7 +1217,8 @@ MODELS_STUB = [
         CATEGORY: "Email",
         TYPE: "Classification",
         NAME: "Propensity to Click",
-        DESCRIPTION: "Propensity for a customer to click " "on a link in an email.",
+        DESCRIPTION: "Propensity for a customer to click "
+        "on a link in an email.",
         ID: "aa789e0bd7edaad4c36bec4a3682f02d36441fe1",
         STATUS: STATUS_PENDING,
     },
@@ -1208,7 +1226,8 @@ MODELS_STUB = [
         CATEGORY: "Email",
         TYPE: "Unknown",
         NAME: "Email Content Optimization",
-        DESCRIPTION: "Alter email content to optimize " "email campaign performance.",
+        DESCRIPTION: "Alter email content to optimize "
+        "email campaign performance.",
         ID: "99e45e0bd7edaad4c36bec4a3682f02d36441fe1",
         STATUS: STATUS_PENDING,
     },
@@ -1270,7 +1289,8 @@ MODELS_STUB = [
         CATEGORY: "Sales forecasting",
         TYPE: "Regression",
         NAME: "Predicted Sales Per Customer",
-        DESCRIPTION: "Predicting sales for a customer over a " "defined time range.",
+        DESCRIPTION: "Predicting sales for a customer over a "
+        "defined time range.",
         ID: "bba67e0bd7edaad4c36bec4a3682f02d36441fe1",
         STATUS: STATUS_PENDING,
     },
@@ -1278,7 +1298,8 @@ MODELS_STUB = [
         CATEGORY: "Sales forecasting",
         TYPE: "Regression",
         NAME: "Predicted Sales Per Store",
-        DESCRIPTION: "Predicting sales for a store over a " "defined time range.",
+        DESCRIPTION: "Predicting sales for a store over a "
+        "defined time range.",
         ID: "a45b7e0bd7edaad4c36bec4a3682f02d36441fe1",
         STATUS: STATUS_PENDING,
     },
