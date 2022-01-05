3c561c8e523e8a6302cc4f77f2d9e16391962b11
skondalraj-deloitte <skondalraj@deloitte.com>
2022-01-06 02:54:14 +0530

    HUS-1749: Black formatting

--- lib/huxunifylib-database/huxunifylib/database/orchestration_management.py
+++ lib/huxunifylib-database/huxunifylib/database/orchestration_management.py
@@ -119,7 +119,9 @@ def get_audience_by_filter(
         InvalidValueException: If passed in limit value is invalid.
     """
 
-    collection = database[db_c.DATA_MANAGEMENT_DATABASE][db_c.AUDIENCES_COLLECTION]
+    collection = database[db_c.DATA_MANAGEMENT_DATABASE][
+        db_c.AUDIENCES_COLLECTION
+    ]
 
     # if deleted is not included in the filters, add it.
     if filter_dict:
@@ -445,7 +447,9 @@ def delete_audience(
         bool: A flag to indicate successful deletion.
     """
 
-    collection = database[db_c.DATA_MANAGEMENT_DATABASE][db_c.AUDIENCES_COLLECTION]
+    collection = database[db_c.DATA_MANAGEMENT_DATABASE][
+        db_c.AUDIENCES_COLLECTION
+    ]
 
     try:
         return collection.delete_one({db_c.ID: audience_id}).deleted_count > 0
@@ -579,7 +583,9 @@ def get_audience_insights(
                         "$group": {
                             "_id": "$_id",
                             "deliveries": {"$push": "$deliveries"},
-                            "last_delivered": {"$last": "$deliveries.update_time"},
+                            "last_delivered": {
+                                "$last": "$deliveries.update_time"
+                            },
                         }
                     },
                     {
@@ -753,14 +759,18 @@ def remove_destination_from_all_audiences(
             all audiences.
     """
 
-    collection = database[db_c.DATA_MANAGEMENT_DATABASE][db_c.AUDIENCES_COLLECTION]
+    collection = database[db_c.DATA_MANAGEMENT_DATABASE][
+        db_c.AUDIENCES_COLLECTION
+    ]
 
     try:
         collection.update_many(
             filter={f"{db_c.DESTINATIONS}.{db_c.OBJECT_ID}": destination_id},
             update={
                 "$pull": {
-                    f"{db_c.DESTINATIONS}": {db_c.OBJECT_ID: {"$in": [destination_id]}}
+                    f"{db_c.DESTINATIONS}": {
+                        db_c.OBJECT_ID: {"$in": [destination_id]}
+                    }
                 },
                 "$set": {
                     db_c.UPDATE_TIME: datetime.datetime.utcnow(),
--- lib/huxunifylib-database/huxunifylib/database/tests/test_orchestration_management.py
+++ lib/huxunifylib-database/huxunifylib/database/tests/test_orchestration_management.py
@@ -19,7 +19,9 @@ class TestAudienceManagement(unittest.TestCase):
     @mongomock.patch(servers=(("localhost", 27017),))
     def setUp(self):
 
-        self.database = DatabaseClient("localhost", 27017, None, None).connect()
+        self.database = DatabaseClient(
+            "localhost", 27017, None, None
+        ).connect()
         self.database.drop_database(db_c.DATA_MANAGEMENT_DATABASE)
 
         # write a user to the database
@@ -219,7 +221,9 @@ class TestAudienceManagement(unittest.TestCase):
         )
         self.assertTrue(doc is not None)
         self.assertTrue(db_c.AUDIENCE_NAME in doc)
-        self.assertEqual(doc[db_c.AUDIENCE_NAME], audience_doc[db_c.AUDIENCE_NAME])
+        self.assertEqual(
+            doc[db_c.AUDIENCE_NAME], audience_doc[db_c.AUDIENCE_NAME]
+        )
 
     def test_duplicate_audience_name(self):
         """Test duplicate audience name."""
@@ -279,7 +283,9 @@ class TestAudienceManagement(unittest.TestCase):
         self.assertTrue(db_c.AUDIENCE_FILTERS in doc)
         self.assertIsNotNone(doc[db_c.AUDIENCE_FILTERS][0])
         self.assertEqual(
-            doc[db_c.AUDIENCE_FILTERS][0][db_c.AUDIENCE_FILTERS_SECTION_AGGREGATOR],
+            doc[db_c.AUDIENCE_FILTERS][0][
+                db_c.AUDIENCE_FILTERS_SECTION_AGGREGATOR
+            ],
             db_c.AUDIENCE_FILTER_AGGREGATOR_ANY,
         )
         self.assertEqual(len(doc[db_c.AUDIENCE_FILTERS]), 1)
@@ -386,12 +392,18 @@ class TestAudienceManagement(unittest.TestCase):
 
         # Attribute filters.
         filters = {db_c.ATTRIBUTE: [db_c.AGE, db_c.S_TYPE_CITY]}
-        filtered_audiences = am.get_all_audiences(self.database, filters=filters)
+        filtered_audiences = am.get_all_audiences(
+            self.database, filters=filters
+        )
         self.assertEqual(len(filtered_audiences), 1)
 
         # Worked by filter.
-        filters = {db_c.WORKED_BY: self.sample_user.get(db_c.USER_DISPLAY_NAME)}
-        filtered_audiences = am.get_all_audiences(self.database, filters=filters)
+        filters = {
+            db_c.WORKED_BY: self.sample_user.get(db_c.USER_DISPLAY_NAME)
+        }
+        filtered_audiences = am.get_all_audiences(
+            self.database, filters=filters
+        )
         self.assertEqual(
             filtered_audiences[0][db_c.CREATED_BY],
             self.sample_user.get(db_c.USER_DISPLAY_NAME),
@@ -404,7 +416,9 @@ class TestAudienceManagement(unittest.TestCase):
             filters=filters,
             audience_ids=[audience_1.get(db_c.ID)],
         )
-        self.assertEqual(filtered_audiences[0][db_c.ID], audience_1.get(db_c.ID))
+        self.assertEqual(
+            filtered_audiences[0][db_c.ID], audience_1.get(db_c.ID)
+        )
 
     def test_get_all_audiences_with_users(self):
         """Test get_all_audiences with users."""
@@ -432,7 +446,9 @@ class TestAudienceManagement(unittest.TestCase):
         audiences = am.get_all_audiences(self.database)
 
         self.assertIsNotNone(audiences)
-        self.assertTrue(all(x[db_c.CREATED_BY] == self.user_name for x in audiences))
+        self.assertTrue(
+            all(x[db_c.CREATED_BY] == self.user_name for x in audiences)
+        )
 
     def test_get_all_audiences_with_deliveries(self):
         """Test get_all_audiences with deliveries."""
@@ -501,7 +517,9 @@ class TestAudienceManagement(unittest.TestCase):
                     delivery_platform_doc[db_c.DELIVERY_PLATFORM_TYPE],
                 )
                 self.assertIn(db_c.UPDATE_TIME, delivery)
-                self.assertEqual(delivery[db_c.STATUS], db_c.AUDIENCE_STATUS_DELIVERING)
+                self.assertEqual(
+                    delivery[db_c.STATUS], db_c.AUDIENCE_STATUS_DELIVERING
+                )
 
     def test_get_all_audiences_with_deliveries_filters(self):
         """Test get_all_audiences with deliveries and filters."""
@@ -581,7 +599,9 @@ class TestAudienceManagement(unittest.TestCase):
         all_audiences = am.get_all_audiences(self.database)
 
         self.assertTrue(
-            am.delete_audience(self.database, ObjectId(all_audiences[0][db_c.ID]))
+            am.delete_audience(
+                self.database, ObjectId(all_audiences[0][db_c.ID])
+            )
         )
 
         audiences = am.get_all_audiences(self.database)
