38d038f3e79866663973c48cf95d2dfc38aa500c
Shubham Kumar <shubhamkumar74@deloitte.com>
2022-01-19 16:39:55 +0530

    added modal for unauthorized error

--- hux/frontend/src/layouts/AppLayout.vue
+++ hux/frontend/src/layouts/AppLayout.vue
@@ -8,6 +8,17 @@
       </v-container>
     </v-main>
     <hux-alert />
+    <confirm-modal
+      v-model="infoModal"
+      icon="access_denied"
+      type="warning"
+      title="Access Denied"
+      body="You do not have the permission to perform this action. Please reach out to your Admin for access."
+      :show-left-button="false"
+      right-btn-text="Close"
+      @onConfirm="infoModal = !infoModal"
+    >
+    </confirm-modal>
   </v-app>
 </template>
 
@@ -15,13 +26,34 @@
 import NavBar from "@/components/NavBar"
 import SideMenu from "@/components/SideMenu"
 import HuxAlert from "@/components/common/HuxAlert.vue"
+import ConfirmModal from "@/components/common/ConfirmModal.vue"
+import { mapGetters } from "vuex"
 
 export default {
   name: "AppLayout",
-  components: { SideMenu, NavBar, HuxAlert },
+  components: { SideMenu, NavBar, HuxAlert, ConfirmModal },
   data: () => ({
     toggleMini: false,
+    infoModal: false,
   }),
+  computed: {
+    ...mapGetters({
+      alerts: "alerts/list",
+    }),
+
+    getAlerts() {
+      return this.alerts.length > 0 ? this.alerts[0] : {}
+    },
+  },
+  watch: {
+    alerts: function () {
+      if (this.alerts.length > 0 && this.alerts[0].code == 401) {
+        this.infoModal = true
+      } else {
+        this.infoModal = false
+      }
+    },
+  },
   methods: {
     toggleSidebar() {
       this.toggleMini = !this.toggleMini
