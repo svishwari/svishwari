9cfc33510b71b02f7e5349a66abe9ce014150658
risheek-deloitte <rgchandra@deloitte.com>
2022-01-19 18:39:32 +0530

    Moving DB Client to huxunifylib_database PT2.

--- lib/huxunifylib-database/huxunifylib/database/tests/test_cdp_data_source_management.py
+++ lib/huxunifylib-database/huxunifylib/database/tests/test_cdp_data_source_management.py
@@ -20,9 +20,7 @@ class TestCdpDataSourceManagement(unittest.TestCase):
         mongo_patch.start()
 
         # Connect
-        self.database = DatabaseClient(
-            "localhost", 27017, None, None
-        ).connect()
+        self.database = DatabaseClient(host="localhost", port=27017).connect()
 
         self.database.drop_database(db_c.DATA_MANAGEMENT_DATABASE)
 
