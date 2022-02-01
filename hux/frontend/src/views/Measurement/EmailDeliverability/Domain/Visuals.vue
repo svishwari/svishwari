625cf8740b96c2ab30c7589b2315606976248da9
Rahul Goel <rahgoel@deloitte.com>
2022-02-01 14:45:23 +0530

    Lint Issue

--- hux/frontend/src/views/Measurement/EmailDeliverability/Domain/Visuals.vue
+++ hux/frontend/src/views/Measurement/EmailDeliverability/Domain/Visuals.vue
@@ -1,15 +1,11 @@
 <template>
-    <div>
-
-    </div>
+  <div></div>
 </template>
 
 <script>
-    export default {
-        name: "DomainVisuals"
-    }
+export default {
+  name: "DomainVisuals",
+}
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
