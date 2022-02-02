ac559a486d8311e603b1f5d41e010619af1fbfc0
svishwari <svishwari@deloitte.com>
2022-02-02 17:34:45 +0530

    UAT feedbacks

--- hux/frontend/src/views/Audiences/Dashboard/Insight2.vue
+++ hux/frontend/src/views/Audiences/Dashboard/Insight2.vue
@@ -222,7 +222,7 @@
       <v-tabs-items v-model="tabOption" class="tabs-item">
         <v-tab-item key="delivery" class="delivery-tab">
           <v-row class="">
-            <v-col :cols="deliveryCols" class="">
+            <div class="mt-3 ml-3" :style="{transition:'0.5s', width: deliveryCols + '%' }">
               <delivery
                 :sections="relatedEngagements"
                 section-type="engagement"
@@ -247,8 +247,12 @@
                 :audience="audience"
                 @onAddStandaloneDestination="addStandaloneDestination($event)"
               />
-            </v-col>
-            <v-col :cols="advertisingCols" class="">
+            </div>
+            <div :style="{ width: '1.5%' }"></div>
+            <div
+              class="mt-3"
+              :style="{ width: advertisingCols + '%'}"
+            >
               <div
                 class="collapsible-bar"
                 :class="{
@@ -300,7 +304,7 @@
                 >
                 </error>
               </v-card>
-            </v-col>
+            </div>
           </v-row>
         </v-tab-item>
         <v-tab-item key="insights" class="insights-tab">
@@ -466,8 +470,8 @@ export default {
       showEditConfirmModal: false,
       newAudienceName: "",
       showAdvertising: true,
-      deliveryCols: 7,
-      advertisingCols: 5,
+      deliveryCols: '57',
+      advertisingCols: '40',
 
       tabOption: 0,
       showLookAlikeDrawer: false,
@@ -1131,12 +1135,12 @@ export default {
     },
     toggleAd() {
       if (this.showAdvertising) {
-        this.deliveryCols = 11
-        this.advertisingCols = 0
+        this.deliveryCols = '96'
+        this.advertisingCols = '1.5'
         this.showAdvertising = false
       } else {
-        this.deliveryCols = 7
-        this.advertisingCols = 5
+        this.deliveryCols = '57'
+        this.advertisingCols = '40'
         this.showAdvertising = true
       }
     },
@@ -1301,7 +1305,6 @@ export default {
   margin-top: 16px;
   width: 24px;
   height: 380px;
-
   cursor: pointer;
   float: left;
   position: relative;
