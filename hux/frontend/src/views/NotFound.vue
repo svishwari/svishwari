cb4d62ac43231f1c03e0ebbb2538c2ec85a1417f
shahlakhani <shahlakhani-deloitte@users.noreply.github.com>
2022-03-02 11:00:43 -0600

    reduces code size

--- hux/frontend/src/views/NotFound.vue
+++ hux/frontend/src/views/NotFound.vue
@@ -1,41 +1,50 @@
 <template>
   <div class="error-wrap">
     <div class="ta-center">
-      <icon type="404" color="black" />
+      <icon :type="404" :size="150" color="black" />
       <div class="fs-56">404</div>
       <div class="text-h1 mt-4">Uh oh we’ve lost this page.</div>
       <div class="ta-center text-h3 mt-2">
         Sorry about that. The reason we don’t find this page:
       </div>
-      <span class="text-h3 ml-17 ta-left">
+      <div class="text-h3 ml-17 ta-left mt-1 gap">
         &bull; the page has moved<br />
         &bull; the page no longer exist<br />
         &bull; you got distracted and made the typo in the address<br />
         &bull; you like visiting 404 pages and actually wanted to be here<br />
-      </span>
-      <v-btn
-        :to="{ name: 'Home' }"
+      </div>
+      <huxButton
+        variant="primary base"
+        size="small"
+        is-tile
+        class="btn-launch text-button mt-12"
         data-e2e="launch"
-        elevation="2"
-        small
-        tile
-        class="btn-launch"
-        color="primary"
+        @click="launch"
       >
         Launch Hux
-      </v-btn>
+      </huxButton>
     </div>
   </div>
 </template>
 
 <script>
 import Icon from "@/components/common/Icon.vue"
+import huxButton from "@/components/common/huxButton"
 
 export default {
   name: "NotFound",
 
   components: {
     Icon,
+    huxButton,
+  },
+
+  methods: {
+    launch: function () {
+      this.$router.push({
+        name: "Home",
+      })
+    },
   },
 }
 </script>
@@ -47,28 +56,15 @@ export default {
   background: var(--v-white-base);
   justify-content: center;
   align-items: center;
-  svg {
-    min-width: 150px;
-    min-height: 150px;
-    width: 150px;
-    height: 150px;
-  }
   .fs-56 {
     font-size: 56px;
     line-height: 76px;
     font-weight: 300;
   }
-  span {
+  .gap {
     line-height: 31px;
     display: flex;
     align-items: flex-start;
-    margin-top: 5px;
-  }
-  .btn-launch {
-    min-height: 40px;
-    min-width: 142px;
-    font-size: 16px;
-    margin-top: 45px;
   }
 }
 </style>
