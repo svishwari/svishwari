73c5ad8fca298179d77d31c52c96fcd52731c09a
mdazad <mdazad@deloitte.com>
2022-03-04 00:59:39 +0530

    UI fixes

--- hux/frontend/src/views/Audiences/Dashboard/Insight2.vue
+++ hux/frontend/src/views/Audiences/Dashboard/Insight2.vue
@@ -8,6 +8,7 @@
       @favoriteAudience="(data) => favoriteAudience(data)"
       @openDownloadDrawer="() => openDownloadDrawer()"
       @openLookalikeEditModal="() => openLookalikeEditModal()"
+      @editAudience="(data) => editAudience(data)"
     />
     <v-progress-linear :active="loading" :indeterminate="loading" />
     <div v-if="audience && audience.is_lookalike === true" class="pa-8">
@@ -895,6 +896,12 @@ export default {
             audienceId: this.audienceId,
           })
           break
+        case "edit audience":
+          this.$router.push({
+            name: "AudienceUpdate",
+            params: { id: this.audienceId },
+          })
+          break
         default:
           break
       }
@@ -1196,6 +1203,16 @@ export default {
         "By deleting this audience you will not be able to recover it and it may impact any associated engagements."
       this.confirmDialog.actionType = "remove audience"
       this.showConfirmModal = true
+    },
+    editAudience(data) {
+      this.showConfirmModal = true
+      ;(this.confirmDialog.title = "Edit"),
+        (this.confirmDialog.btnText = "Yes, edit")
+      this.confirmDialog.icon = "edit"
+      ;(this.confirmDialog.subtitle = data.name),
+        (this.confirmDialog.type = "error")
+      this.confirmDialog.body = "Are you sure you want to edit this audience?"
+      this.confirmDialog.actionType = "edit audience"
     },
     favoriteAudience(data) {
       let param
