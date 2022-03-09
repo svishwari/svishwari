3cc0b69d100e1745ca5d92f37ecf75012f2a959a
Singh <sameerksingh@deloitte.com>
2022-03-09 17:01:38 +0530

    first commit

--- hux/frontend/src/views/Login.vue
+++ hux/frontend/src/views/Login.vue
@@ -6,11 +6,9 @@
       </v-col>
       <v-col cols="6" class="right-section">
         <logo />
-        <h1 class="text-h4 font-weight-regular">
-          Access your <span class="altcolor">360&deg;</span> view
-        </h1>
-        <p class="font-weight-regular">
-          Jump right in where you left off, and leave the hard work on us!
+        <h1>Access your <span class="altcolor">360&deg;</span> view</h1>
+        <p class="body-1">
+          Jump right in where you left off, and leave the hard work to us!
         </p>
         <div class="login-form">
           <div id="okta-signin-container" class="okta-container"></div>
@@ -39,16 +37,14 @@ export default {
         redirectUri: window.location.origin + "/login/callback",
         logo: require("@/assets/images/logo.png"),
         language: "en",
-        i18n: {
-          en: {
-            "primaryauth.title": "Login | HUX Unified UI",
-          },
-        },
         authParams: {
           pkce: true,
           issuer: config.default.oidc.issuer,
           display: "page",
           scopes: ["openid", "email", "profile", "groups"],
+        },
+        features: {
+          showPasswordToggleOnSignInPage: true,
         },
       })
 
@@ -88,38 +84,34 @@ export default {
   }
   .right-section {
     padding-left: 7% !important;
-    padding-top: 6% !important;
+    padding-top: 5% !important;
     padding-right: 6% !important;
     svg {
       width: 95px;
       height: 95px;
     }
     h1 {
-      font-family: "Open Sans" !important;
+      font-size: 28px;
+      line-height: 40px;
       font-weight: 300 !important;
-      font-size: 24px !important;
       margin-top: 30px;
-      line-height: 43px;
-      color: var(--v-black-darken2);
+      color: #333333;
       .altcolor {
-        color: var(--v-pinkLittleDark-base);
+        color: #ff007f;
       }
     }
     p {
-      font-size: 14px !important;
-      line-height: 22px;
-      color: var(--v-black-darken1);
-      margin-bottom: 20px;
+      color: var(--v-black-lighten4);
+      margin-bottom: 13px;
     }
     .login-form {
       #okta-signin-container {
         ::v-deep .auth-container {
-          font-family: "Open Sans", sans-serif !important;
+          font-family: "Open Sans" !important;
           border: 0;
           margin: 0;
           background: transparent;
           width: 100%;
-          padding-left: 5px;
           .okta-sign-in-header {
             display: none;
           }
@@ -130,11 +122,73 @@ export default {
               visibility: hidden;
             }
             .okta-form-title {
-              text-align: left;
+              display: none;
             }
-            .o-form-label {
+            .o-form-label-top {
+              &:first-child {
+                .o-form-label {
+                  padding: 6px 0px 2px 5px !important;
+                }
+              }
+              .o-form-label {
+                padding: 12px 0px 2px 5px !important;
+                label {
+                  font-style: normal;
+                  font-weight: normal;
+                  font-size: 14px;
+                  line-height: 16px;
+                  color: var(--v-black-base);
+                }
+              }
+            }
+            .o-form-control {
+              input {
+                font-style: normal;
+                font-weight: normal;
+                font-size: 16px;
+                line-height: 22px;
+              }
+            }
+            .o-form-content {
+              padding-bottom: 24px !important;
+            }
+            .o-form-input-name-remember {
+              input {
+                top: 2px;
+                height: 18px;
+                width: 18px;
+              }
               label {
-                color: var(--v-black-darken4);
+                font-style: normal;
+                font-weight: normal;
+                font-size: 16px;
+                line-height: 16px;
+              }
+              .custom-checkbox {
+                box-shadow: none;
+              }
+            }
+            .email-button {
+              background: var(--v-primary-base) !important;
+            }
+            .password-reset-email-sent {
+              .o-form {
+                .o-form-content {
+                  p {
+                    font-style: normal;
+                    font-weight: normal;
+                    font-size: 12px;
+                    line-height: 16px;
+                    color: var(--v-black-lighten4) !important;
+                    margin-top: 6px !important;
+                    margin-bottom: 16px !important;
+                  }
+                  .o-form-fieldset-container {
+                    .button {
+                      background: var(--v-primary-base) !important;
+                    }
+                  }
+                }
               }
             }
             .o-form-input {
@@ -144,12 +198,20 @@ export default {
                   padding-left: 0;
                 }
               }
+              .o-form-input-name-password {
+                .password-toggle {
+                  .visibility-16:before,
+                  .visibility-off-16:before {
+                    color: var(--v-black-base) !important;
+                  }
+                }
+              }
             }
             .okta-form-input-field {
               border: none;
               background: transparent;
               input {
-                padding: 10px 16px;
+                padding: 9px 16px;
                 background: var(--v-white-base);
                 border: 1px solid var(--v-black-lighten3);
                 box-sizing: border-box;
@@ -168,20 +230,34 @@ export default {
               }
             }
             .o-form-button-bar {
-              width: 90px;
+              width: 107px;
               input {
-                font-family: "Open Sans", sans-serif !important;
-                width: 90px;
+                padding-left: 27px !important;
+                padding-right: 27px !important;
+                font-family: "Open Sans" !important;
                 height: 40px;
                 box-sizing: border-box;
                 background: var(--v-primary-base);
                 box-shadow: 0px 1px 5px rgba(0, 0, 0, 0.25);
-                content: "Log in";
               }
             }
           }
           .auth-footer {
-            display: none;
+            padding-top: 7px !important;
+            font-style: normal;
+            font-weight: normal;
+            font-size: 16px;
+            line-height: 22px;
+            .link {
+              &.help {
+                box-shadow: none !important;
+                color: var(--v-black-lighten4);
+                &.js-back {
+                  position: relative;
+                  bottom: 21px !important;
+                }
+              }
+            }
           }
         }
       }
