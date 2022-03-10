466518da328c96d175002b875effaea351235096
sach9993 <sacojha@deloitte.com>
2022-03-10 17:34:29 +0530

    hx trust id base page added !!!

---
+++ hux/frontend/src/views/HXTrustId/Index.vue
@@ -0,0 +1,73 @@
+<template>
+  <page max-width="100%" class="hx-trust-id-wrapper">
+    <template #header>
+      <page-header
+        class="page-header py-5"
+        header-min-height="110"
+        header-max-height="120"
+      >
+        <template #left>
+          <div>
+            <breadcrumb
+              :items="[
+                {
+                  text: 'HX TrustID',
+                  disabled: true,
+                  href: '/hx-trustid',
+                  icon: 'hx-trustid',
+                  iconSize: 36,
+                  iconColor: 'black',
+                  iconColorVariant: 'base',
+                },
+              ]"
+            />
+          </div>
+          <div class="text-subtitle-1 font-weight-regular pt-0 pl-0">
+              Measure the signals of trust, predict how trust sentiment will impact customer &nbsp; employee behaviors, and identify actions to (re)build trust.
+          </div>
+        </template>
+      </page-header>
+      <v-progress-linear :active="loading" :indeterminate="loading" />
+    </template>
+  </page>
+</template>
+
+<script>
+import Breadcrumb from "@/components/common/Breadcrumb.vue"
+import Page from "@/components/Page.vue"
+import PageHeader from "@/components/PageHeader.vue"
+
+export default {
+  name: "EmailDeliverability",
+  components: {
+    Page,
+    PageHeader,
+    Breadcrumb,
+  },
+  data() {
+    return {
+      loading: false,
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.hx-trust-id-wrapper {
+  ::v-deep .v-breadcrumbs {
+    li {
+      font-family: Open Sans Light;
+      font-size: 28px;
+      font-style: normal;
+      font-weight: 400 !important;
+      line-height: 40px;
+      letter-spacing: 0px;
+      text-align: left;
+    }
+  }
+
+  .overview-card {
+    border-radius: 12px !important;
+  }
+}
+</style>
