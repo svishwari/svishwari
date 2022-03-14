4f966f20f4bd4d14791c0fa583ffcfdccc273854
shahlakhani <shahlakhani-deloitte@users.noreply.github.com>
2022-03-14 09:51:04 -0500

    storing changes

--- hux/frontend/src/components/common/RhombusNumber.vue
+++ hux/frontend/src/components/common/RhombusNumber.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="num-style">{{ value }}</div>
+  <v-card-text class="card-style">
+    <div>{{ value }}</div>
+  </v-card-text>
 </template>
 
 <script>
@@ -13,14 +15,17 @@ export default {
   },
 }
 </script>
-
 <style lang="scss" scoped>
-.num-style {
+.card-style {
+  // num style
   font: Open Sans SemiBold;
   font-weight: 600;
   font-style: normal;
   font-size: 16px;
   line-height: 21.79px;
   text-align: center;
+  // card style
+  width: 50px;
+  height: 50px;
 }
 </style>
