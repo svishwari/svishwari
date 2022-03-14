3f327076e0e61ace26d8890b3c3eba95b297d463
mdazad <mdazad@deloitte.com>
2022-03-14 21:33:36 +0530

    style fix

--- hux/frontend/src/components/common/Charts/SingleHorizontalStackBarChart/groupData.js
+++ hux/frontend/src/components/common/Charts/SingleHorizontalStackBarChart/groupData.js
@@ -2,24 +2,23 @@
 import * as d3Scale from "d3-scale"
 
 export default function (data, total) {
-    // use scale to get percent values
-    const percent = d3Scale
-        .scaleLinear()
-        .domain([0, total])
-        .range([0, 100])
-    // filter out data that has zero values
-    // also get mapping for next placement
-    // (save having to format data for d3 stack)
-    let cumulative = 0
-    const _data = data.map(d => {
-        cumulative += d.value
-        return {
-            value: d.value,
-            // want the cumulative to prior value (start of rect)
-            cumulative: cumulative - d.value,
-            label: d.label,
-            percent: percent(d.value)
-        }
-    }).filter(d => d.value > 0)
-    return _data
+  // use scale to get percent values
+  const percent = d3Scale.scaleLinear().domain([0, total]).range([0, 100])
+  // filter out data that has zero values
+  // also get mapping for next placement
+  // (save having to format data for d3 stack)
+  let cumulative = 0
+  const _data = data
+    .map((d) => {
+      cumulative += d.value
+      return {
+        value: d.value,
+        // want the cumulative to prior value (start of rect)
+        cumulative: cumulative - d.value,
+        label: d.label,
+        percent: percent(d.value),
+      }
+    })
+    .filter((d) => d.value > 0)
+  return _data
 }
