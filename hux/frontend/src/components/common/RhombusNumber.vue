4d822dc806657b7c8b80732d5f383c1374a954a4
shahlakhani <shahlakhani-deloitte@users.noreply.github.com>
2022-03-17 10:32:49 -0500

    most styling is done

--- hux/frontend/src/components/common/RhombusNumber.vue
+++ hux/frontend/src/components/common/RhombusNumber.vue
@@ -1,7 +1,7 @@
 <template>
   <v-card class="card-style" shaped outlined>
     <v-card-text class="text-style">
-      <div>{{ value }}</div>
+      {{ value }}
     </v-card-text>
   </v-card>
 </template>
@@ -21,8 +21,11 @@ export default {
 <style lang="scss" scoped>
 .card-style {
   transform: rotate(45deg);
-  max-width: 54px;
-  max-height: 54px;
+  width: 40px;
+  height: 40px;
+  border-color: blue;
+  border-radius: 0 !important;
+  border-width: thick;
 }
 .text-style {
   // num style
@@ -30,8 +33,10 @@ export default {
   font-weight: 600;
   font-style: normal;
   font-size: 16px;
-  line-height: 21.79px;
+  word-wrap: normal;
+  transform: translate(0%, 35%) rotate(-45deg);
+  padding: 0px;
+  display: block;
   text-align: center;
-  transform: rotate(-45deg);
 }
 </style>
