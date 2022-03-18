ba958553def9d38254ebdbbdb1a8928654088352
Doug <90864188+dcl-deloitte@users.noreply.github.com>
2022-03-18 02:16:50 -0600

    resolve e2e tests

---
+++ hux/integration_tests/api/test_data_sources.py
@@ -0,0 +1,37 @@
+"""Purpose of this file is to test data sources."""
+from unittest import TestCase
+import pytest
+import requests
+from conftest import Crud
+
+
+class TestDataSources(TestCase):
+    """Test Datasources."""
+
+    DATA_SOURCES = "data-sources"
+    COLLECTION = "cdp_data_sources"
+
+    def test_create(self):
+        """Test creating a datasource."""
+
+        # setup the request mock post
+        response = requests.post(
+            f"{pytest.API_URL}/{self.DATA_SOURCES}",
+            json=[
+                {
+                    "name": "E2E Test Data source",
+                    "type": "dataSource",
+                    "status": "Active",
+                    "category": "CRM",
+                }
+            ],
+            headers=pytest.HEADERS,
+        )
+
+        # test success
+        self.assertEqual(200, response.status_code)
+
+        # add the crud object to pytest for cleaning after.
+        pytest.CRUD_OBJECTS += [
+            Crud(self.COLLECTION, response.json()[0]["id"])
+        ]
