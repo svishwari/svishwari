b6dd2fd2f1e8f87998616b8b41905081156c7eaf
skondalraj-deloitte <skondalraj@deloitte.com>
2022-03-20 05:47:14 -0400

    HUS-2644: Black formatting

--- lib/huxunifylib-database/huxunifylib/database/tests/test_engagement_audience_mgmt.py
+++ lib/huxunifylib-database/huxunifylib/database/tests/test_engagement_audience_mgmt.py
@@ -55,7 +55,9 @@ class TestEngagementAudienceMgmt(unittest.TestCase):
         audience_id = ObjectId()
         destination_id = ObjectId()
         engagement_id = (
-            self.database[db_c.DATA_MANAGEMENT_DATABASE][db_c.ENGAGEMENTS_COLLECTION]
+            self.database[db_c.DATA_MANAGEMENT_DATABASE][
+                db_c.ENGAGEMENTS_COLLECTION
+            ]
             .insert_one(
                 {
                     "name": "arkells",
@@ -128,7 +130,9 @@ class TestEngagementAudienceMgmt(unittest.TestCase):
         audience_id = ObjectId()
         destination_id = ObjectId()
         engagement_id = (
-            self.database[db_c.DATA_MANAGEMENT_DATABASE][db_c.ENGAGEMENTS_COLLECTION]
+            self.database[db_c.DATA_MANAGEMENT_DATABASE][
+                db_c.ENGAGEMENTS_COLLECTION
+            ]
             .insert_one(
                 {
                     "name": "arkells 2",
@@ -203,7 +207,9 @@ class TestEngagementAudienceMgmt(unittest.TestCase):
         audience_id = ObjectId()
         destination_id = ObjectId()
         engagement_id = (
-            self.database[db_c.DATA_MANAGEMENT_DATABASE][db_c.ENGAGEMENTS_COLLECTION]
+            self.database[db_c.DATA_MANAGEMENT_DATABASE][
+                db_c.ENGAGEMENTS_COLLECTION
+            ]
             .insert_one(
                 {
                     "name": "Test Engagement Audience Management",
@@ -291,7 +297,9 @@ class TestEngagementAudienceMgmt(unittest.TestCase):
         audience_id = ObjectId()
         destination_id = ObjectId()
         engagement_id = (
-            self.database[db_c.DATA_MANAGEMENT_DATABASE][db_c.ENGAGEMENTS_COLLECTION]
+            self.database[db_c.DATA_MANAGEMENT_DATABASE][
+                db_c.ENGAGEMENTS_COLLECTION
+            ]
             .insert_one(
                 {
                     "name": "Test Engagement Audience Management",
@@ -424,7 +432,9 @@ class TestEngagementAudienceMgmt(unittest.TestCase):
     def test_get_all_engagement_audience_deliveries(self):
         """Test get all deliveries for engagement and audience pairs."""
 
-        audiences = [self.generate_audience_delivery_jobs(i) for i in range(11)]
+        audiences = [
+            self.generate_audience_delivery_jobs(i) for i in range(11)
+        ]
 
         # get all audiences and deliveries
         audience_deliveries = eam.get_all_engagement_audience_deliveries(
@@ -451,7 +461,9 @@ class TestEngagementAudienceMgmt(unittest.TestCase):
                     self.destination[db_c.DELIVERY_PLATFORM_NAME],
                 )
                 self.assertIn(db_c.UPDATE_TIME, delivery)
-                self.assertEqual(delivery[db_c.STATUS], db_c.AUDIENCE_STATUS_DELIVERING)
+                self.assertEqual(
+                    delivery[db_c.STATUS], db_c.AUDIENCE_STATUS_DELIVERING
+                )
 
     def test_get_all_audience_engagement_id_pairs(self):
         """Test get all get_all_audience_engagement_id_pairs."""
@@ -485,7 +497,9 @@ class TestEngagementAudienceMgmt(unittest.TestCase):
         self.assertEqual(4, len(recent_deliveries))
 
         for delivery in recent_deliveries:
-            self.assertEqual(db_c.AUDIENCE_STATUS_DELIVERING, delivery.get(db_c.STATUS))
+            self.assertEqual(
+                db_c.AUDIENCE_STATUS_DELIVERING, delivery.get(db_c.STATUS)
+            )
 
     def test_align_audience_engagement_deliveries(self):
         """Test get all align_audience_engagement_deliveries."""
