6da3211e6cc622426f254982b09b072bf8c6449c
mdazad <mdazad@deloitte.com>
2022-03-28 23:42:05 +0530

    code clean

--- hux/frontend/src/api/mock/fixtures/trustIdOverview.js
+++ hux/frontend/src/api/mock/fixtures/trustIdOverview.js
@@ -9,51 +9,51 @@ export default {
       overall_customer_rating: {
         total_customers: 190909,
         rating: {
-          agree: { percentage: 0.82, count: 156545 },
-          neutral: { percentage: 0.07, count: 13363 },
-          disagree: { percentage: 0.11, count: 21001 },
+          agree: { percentage: 0.25, count: 156545 },
+          neutral: { percentage: 0.35, count: 13363 },
+          disagree: { percentage: 0.4, count: 21001 },
         },
       },
     },
     {
       attribute_name: "transparency",
-      attribute_score: 71,
+      attribute_score: 51,
       attribute_description:
         "Transparency is openly sharing all information, motives, and choices in straightforward and plain language. It is scored on a scale between -100 to 100",
       overall_customer_rating: {
         total_customers: 190909,
         rating: {
-          agree: { percentage: 0.82, count: 156545 },
-          neutral: { percentage: 0.07, count: 13363 },
-          disagree: { percentage: 0.11, count: 21001 },
+          agree: { percentage: 0.3, count: 156545 },
+          neutral: { percentage: 0.28, count: 13363 },
+          disagree: { percentage: 0.48, count: 21001 },
         },
       },
     },
     {
       attribute_name: "capability",
-      attribute_score: 78,
+      attribute_score: 48,
       attribute_description:
         "Capability is creating quality products, services, and/or experiences. It is scored on a scale between -100 to 100",
       overall_customer_rating: {
         total_customers: 190909,
         rating: {
-          agree: { percentage: 0.88, count: 168000 },
-          neutral: { percentage: 0.02, count: 3818 },
-          disagree: { percentage: 0.1, count: 19091 },
+          agree: { percentage: 0.25, count: 168000 },
+          neutral: { percentage: 0.28, count: 3818 },
+          disagree: { percentage: 0.42, count: 19091 },
         },
       },
     },
     {
       attribute_name: "reliability",
-      attribute_score: 71,
+      attribute_score: 87,
       attribute_description:
         "Reliability is consistently and dependably delivering on promises. It is scored on a scale between -100 to 100",
       overall_customer_rating: {
         total_customers: 190909,
         rating: {
-          agree: { percentage: 0.82, count: 156545 },
-          neutral: { percentage: 0.07, count: 13363 },
-          disagree: { percentage: 0.11, count: 21001 },
+          agree: { percentage: 0.3, count: 156545 },
+          neutral: { percentage: 0.38, count: 13363 },
+          disagree: { percentage: 0.32, count: 21001 },
         },
       },
     },
--- hux/frontend/src/views/HXTrustId/Index.vue
+++ hux/frontend/src/views/HXTrustId/Index.vue
@@ -83,7 +83,7 @@
               </v-row>
             </v-tab-item>
             <v-tab-item key="attributes" class="tab-item">
-              <trust-id-attributes :data="attributeData"> </trust-id-attributes>
+              <trust-id-attributes :data="overviewData.attributes" />
             </v-tab-item>
           </v-tabs-items>
         </div>
@@ -166,64 +166,6 @@ export default {
       addSegmentData: addSegmentData,
       segmentScores: segmentScores,
       overviewData: overviewData,
-      attributeData: [
-        {
-          attribute_name: "humanity",
-          attribute_score: 71,
-          attribute_description:
-            "Humanity is demonstrating empathy and kindness towards customers, and treating everyone fairly. It is scored on a scale between -100 to 100",
-          overall_customer_rating: {
-            total_customers: 190909,
-            rating: {
-              agree: { percentage: 0.25, count: 156545 },
-              neutral: { percentage: 0.35, count: 13363 },
-              disagree: { percentage: 0.4, count: 21001 },
-            },
-          },
-        },
-        {
-          attribute_name: "transparency",
-          attribute_score: 51,
-          attribute_description:
-            "Transparency is openly sharing all information, motives, and choices in straightforward and plain language. It is scored on a scale between -100 to 100",
-          overall_customer_rating: {
-            total_customers: 190909,
-            rating: {
-              agree: { percentage: 0.3, count: 156545 },
-              neutral: { percentage: 0.28, count: 13363 },
-              disagree: { percentage: 0.48, count: 21001 },
-            },
-          },
-        },
-        {
-          attribute_name: "capability",
-          attribute_score: 48,
-          attribute_description:
-            "Capability is creating quality products, services, and/or experiences. It is scored on a scale between -100 to 100",
-          overall_customer_rating: {
-            total_customers: 190909,
-            rating: {
-              agree: { percentage: 0.25, count: 168000 },
-              neutral: { percentage: 0.28, count: 3818 },
-              disagree: { percentage: 0.42, count: 19091 },
-            },
-          },
-        },
-        {
-          attribute_name: "reliability",
-          attribute_score: 87,
-          attribute_description:
-            "Reliability is consistently and dependably delivering on promises. It is scored on a scale between -100 to 100",
-          overall_customer_rating: {
-            total_customers: 190909,
-            rating: {
-              agree: { percentage: 0.3, count: 156545 },
-              neutral: { percentage: 0.38, count: 13363 },
-              disagree: { percentage: 0.32, count: 21001 },
-            },
-          },
-        },
-      ],
     }
   },
   computed: {
