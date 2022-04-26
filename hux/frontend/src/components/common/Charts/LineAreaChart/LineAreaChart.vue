28ccb8c9942db87e8b4bb8fe1f64acc2a56bbc29
sach9993 <sacojha@deloitte.com>
2022-04-26 16:24:15 +0530

    charts hover issue fixes !!!

--- hux/frontend/src/components/common/Charts/LineAreaChart/LineAreaChart.vue
+++ hux/frontend/src/components/common/Charts/LineAreaChart/LineAreaChart.vue
@@ -261,51 +261,53 @@ export default {
           (element) => dateFormatter(element.date) == dateD
         )
 
-        svg
-          .selectAll(".hover-line-y")
-          .attr("x1", finalXCoordinate)
-          .attr("x2", finalXCoordinate)
-          .attr("y1", 0)
-          .attr("y2", h)
-          .style("display", "block")
-
-        svg.selectAll(".dot").each(function () {
-          if (this.getAttribute("cx") == finalXCoordinate) {
-            let yPosition = this.getAttribute("cy")
-            yData = yPosition
-            svg
-              .append("circle")
-              .classed("parent-hover-circle", true)
-              .attr("cx", finalXCoordinate)
-              .attr("cy", yPosition)
-              .attr("r", 9)
-              .style("stroke", "white")
-              .style("stroke-opacity", "1")
-              .style("stroke-width", 1)
-              .style("fill", "white")
-              .style("pointer-events", "none")
-
-            svg
-              .append("circle")
-              .classed("child-hover-circle", true)
-              .attr("cx", finalXCoordinate)
-              .attr("cy", yPosition)
-              .attr("r", 7)
-              .style("stroke", "#0C9DDB")
-              .style("stroke-opacity", "1")
-              .style("stroke-width", 2)
-              .style("fill", "white")
-              .style("pointer-events", "none")
-          }
-        })
-
         if (dataToolTip) {
-          dataToolTip.xPosition = finalXCoordinate
-          // Invert tooltip positioning to avoid cut off
-          dataToolTip.invertPosition =
-            finalXCoordinate > maxRightLimit ? true : false
-          dataToolTip.yPosition = yData
-          this.tooltipDisplay(true, dataToolTip)
+          svg
+            .selectAll(".hover-line-y")
+            .attr("x1", finalXCoordinate)
+            .attr("x2", finalXCoordinate)
+            .attr("y1", 0)
+            .attr("y2", h)
+            .style("display", "block")
+
+          svg.selectAll(".dot").each(function () {
+            if (this.getAttribute("cx") == finalXCoordinate) {
+              let yPosition = this.getAttribute("cy")
+              yData = yPosition
+              svg
+                .append("circle")
+                .classed("parent-hover-circle", true)
+                .attr("cx", finalXCoordinate)
+                .attr("cy", yPosition)
+                .attr("r", 9)
+                .style("stroke", "white")
+                .style("stroke-opacity", "1")
+                .style("stroke-width", 1)
+                .style("fill", "white")
+                .style("pointer-events", "none")
+
+              svg
+                .append("circle")
+                .classed("child-hover-circle", true)
+                .attr("cx", finalXCoordinate)
+                .attr("cy", yPosition)
+                .attr("r", 7)
+                .style("stroke", "#0C9DDB")
+                .style("stroke-opacity", "1")
+                .style("stroke-width", 2)
+                .style("fill", "white")
+                .style("pointer-events", "none")
+            }
+          })
+
+          if (dataToolTip) {
+            dataToolTip.xPosition = finalXCoordinate
+            // Invert tooltip positioning to avoid cut off
+            dataToolTip.invertPosition =
+              finalXCoordinate > maxRightLimit ? true : false
+            dataToolTip.yPosition = yData
+            this.tooltipDisplay(true, dataToolTip)
+          }
         }
       }
     },
