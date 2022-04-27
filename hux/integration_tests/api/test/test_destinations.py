754acb42c7d6544dbf1b987955ee243b47ca0b23
skondalraj-deloitte <skondalraj@deloitte.com>
2022-04-27 13:08:29 -0500

    NO-JIRA: Fix failing destination tests and update Dockerfile dependency

--- hux/integration_tests/api/test/test_destinations.py
+++ hux/integration_tests/api/test/test_destinations.py
@@ -28,22 +28,26 @@ class TestDestinations(TestCase):
     def test_get_destination(self):
         """Test get destination."""
 
-        response = requests.get(
+        get_all_response = requests.get(
             f"{pytest.API_URL}/{self.DESTINATIONS}",
             headers=pytest.HEADERS,
         )
-        self.assertEqual(HTTPStatus.OK, response.status_code)
-        self.assertEqual(response.json(), list)
 
-        destinations = response.json()
+        self.assertEqual(HTTPStatus.OK, get_all_response.status_code)
+        self.assertIsInstance(get_all_response.json(), list)
 
-        destination_response = requests.get(
-            f"{pytest.API_URL}/{self.DESTINATIONS}/{destinations[0]['id']}",
+        destination = get_all_response.json()[0]
+
+        get_individual_destination_response = requests.get(
+            f"{pytest.API_URL}/{self.DESTINATIONS}/{destination['id']}",
             headers=pytest.HEADERS,
         )
+
         # test success
-        self.assertEqual(HTTPStatus.OK, destination_response.status_code)
-        self.assertIsInstance(destination_response.json(), dict)
+        self.assertEqual(
+            HTTPStatus.OK, get_individual_destination_response.status_code
+        )
+        self.assertIsInstance(get_individual_destination_response.json(), dict)
 
     def test_get_destination_constants(self):
         """Test get destination constants."""
@@ -52,50 +56,55 @@ class TestDestinations(TestCase):
             f"{pytest.API_URL}/{self.DESTINATIONS}/constants",
             headers=pytest.HEADERS,
         )
+
         self.assertEqual(HTTPStatus.OK, response.status_code)
-        self.assertEqual(response.json(), dict)
+        self.assertIsInstance(response.json(), dict)
 
     def test_get_destination_data_extension(self):
         """Test get destination data extensions."""
-        response = requests.get(
+
+        get_all_response = requests.get(
             f"{pytest.API_URL}/{self.DESTINATIONS}",
             headers=pytest.HEADERS,
         )
-        self.assertEqual(HTTPStatus.OK, response.status_code)
-        self.assertEqual(response.json(), list)
+        self.assertEqual(HTTPStatus.OK, get_all_response.status_code)
+        self.assertIsInstance(get_all_response.json(), list)
 
-        destinations = response.json()
+        destinations = get_all_response.json()
         sfmc_destination = [
             destination
             for destination in destinations
             if destination["type"] == self.DELIVERY_PLATFORM_SFMC
         ][0]
 
-        response = requests.get(
-            f"{pytest.API_URL}/{self.DESTINATIONS}/{sfmc_destination['id']}/data-extensions",
+        get_dest_data_ext = requests.get(
+            f"{pytest.API_URL}/{self.DESTINATIONS}/{sfmc_destination['id']}/"
+            f"data-extensions",
             headers=pytest.HEADERS,
         )
 
-        self.assertEqual(HTTPStatus.OK, response.status_code)
-        self.assertIsInstance(response.json(), list)
-        self.assertGreaterEqual(len(response.json()), 1)
+        self.assertEqual(HTTPStatus.OK, get_dest_data_ext.status_code)
+        self.assertIsInstance(get_dest_data_ext.json(), list)
+        self.assertGreaterEqual(len(get_dest_data_ext.json()), 1)
 
     def test_update_destination(self):
         """Test update destination."""
-        response = requests.get(
+
+        get_all_response = requests.get(
             f"{pytest.API_URL}/{self.DESTINATIONS}",
             headers=pytest.HEADERS,
         )
-        self.assertEqual(HTTPStatus.OK, response.status_code)
-        self.assertEqual(response.json(), list)
 
-        destination = response.json()[0]
+        self.assertEqual(HTTPStatus.OK, get_all_response.status_code)
+        self.assertIsInstance(get_all_response.json(), list)
 
-        response = requests.patch(
+        destination = get_all_response.json()[0]
+
+        update_response = requests.patch(
             f"{pytest.API_URL}/{self.DESTINATIONS}/{destination['id']}",
             json={"enabled": destination["is_enabled"]},
             headers=pytest.HEADERS,
         )
-        self.assertEqual(HTTPStatus.OK, response.status_code)
-        self.assertIsInstance(response.json(), list)
-        self.assertGreaterEqual(len(response.json()), 1)
+
+        self.assertEqual(HTTPStatus.OK, update_response.status_code)
+        self.assertIsInstance(update_response.json(), dict)
