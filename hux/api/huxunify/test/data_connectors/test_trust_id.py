9ba3f481dd87ec17e4f67e2b19fb088417c9714d
risheek-deloitte <rgchandra@deloitte.com>
2022-04-29 19:33:40 +0530

    Adding test for Trust ID.

--- hux/api/huxunify/test/data_connectors/test_trust_id.py
+++ hux/api/huxunify/test/data_connectors/test_trust_id.py
@@ -3,7 +3,9 @@ from unittest import TestCase
 
 import mongomock
 
+from huxunify.api import constants as api_c
 from huxunify.api.data_connectors.trust_id import aggregate_attributes
+
 from huxunifylib.database.client import DatabaseClient
 from huxunifylib.database.survey_metrics_management import (
     set_survey_responses_bulk,
@@ -30,8 +32,16 @@ class TrustIDTest(TestCase):
 
     def test_aggregate_attributes(self):
         """Test aggregate_attributes method."""
+
         survey_responses = get_survey_responses(self.database)
         aggregated_attributes = aggregate_attributes(survey_responses)
-
         self.assertIsInstance(aggregated_attributes, dict)
+
         # Ensure all factors are present.
+        api_c.LIST_OF_FACTORS.sort()
+        self.assertEqual(api_c.LIST_OF_FACTORS,
+                         sorted(list(aggregated_attributes.keys())))
+
+        self.assertIsInstance(aggregated_attributes[api_c.LIST_OF_FACTORS[0]],
+                              dict)
+
