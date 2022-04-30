af03147d7843f996221f33530edb401b7b0abb26
panch mukesh <ppanchmukesh@deloitte.com>
2022-04-30 09:15:40 +0530

    Adding Ad Platform Flag to Standalone Deliveries

--- hux/api/huxunify/api/route/orchestration.py
+++ hux/api/huxunify/api/route/orchestration.py
@@ -262,6 +262,9 @@ def get_audience_standalone_deliveries(audience: dict) -> list:
                 api_c.SIZE: 0,
                 db_c.DELIVERY_PLATFORM_ID: x,
                 db_c.LINK: destination_dict.get(x).get(db_c.LINK),
+                db_c.IS_AD_PLATFORM: destination_dict.get(
+                    job.get(db_c.DELIVERY_PLATFORM_ID)
+                ).get(db_c.IS_AD_PLATFORM),
             }
         )
         for x in destination_ids
