2fdd34127e2a6242c6f42b5f27f88943ac504721
skondalraj-deloitte <skondalraj@deloitte.com>
2022-05-10 08:31:07 -0500

    HUS-2849: Code review suggestion to use constants

--- hux/api/huxunify/api/schema/notifications.py
+++ hux/api/huxunify/api/schema/notifications.py
@@ -15,29 +15,29 @@ class NotificationSchema(Schema):
 
     id = Str(attribute=db_c.ID, example="60e5c7be3b080a75959d6282")
     notification_type = Str(
-        attribute="type",
+        attribute=db_c.NOTIFICATION_FIELD_TYPE,
         validate=[OneOf(choices=db_c.NOTIFICATION_TYPES)],
         required=True,
         example=db_c.NOTIFICATION_TYPE_CRITICAL,
     )
     description = Str(
-        attribute="description",
+        attribute=db_c.NOTIFICATION_FIELD_DESCRIPTION,
         required=True,
         example="Facebook Delivery Stopped",
     )
     create_time = DateTimeWithZ(
-        attribute="create_time",
+        attribute=db_c.NOTIFICATION_FIELD_CREATE_TIME,
         required=True,
         allow_none=False,
     )
     category = Str(
-        attribute="category",
+        attribute=db_c.NOTIFICATION_FIELD_CATEGORY,
         validate=[OneOf(choices=db_c.NOTIFICATION_CATEGORIES)],
         required=True,
         example=db_c.NOTIFICATION_CATEGORY_DELIVERY,
     )
     username = Str(
-        attribute="username",
+        attribute=db_c.NOTIFICATION_FIELD_USERNAME,
         required=True,
         example="Username",
         allow_none=False,
