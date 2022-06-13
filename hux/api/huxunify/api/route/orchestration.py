5f899c9a4926a22bed31aeb8d32549586d4de2e6
soumbehera <soumbehera@deloitte.com>
2022-06-13 20:37:59 +0530

    Refactoring api constants

--- hux/api/huxunify/api/route/orchestration.py
+++ hux/api/huxunify/api/route/orchestration.py
@@ -338,7 +338,7 @@ class AudienceView(SwaggerView):
             "example": "age",
         },
         {
-            "name": api_c.EVENT,
+            "name": api_c.EVENTS,
             "description": "Only return audiences matching the selected filters",
             "in": "query",
             "type": "array",
@@ -420,11 +420,11 @@ class AudienceView(SwaggerView):
             filter_dict[api_c.WORKED_BY] = user[api_c.USER_NAME]
 
         attribute_list = request.args.getlist(api_c.ATTRIBUTE)
-        events_list = request.args.getlist(api_c.EVENT)
+        events_list = request.args.getlist(api_c.EVENTS)
         # set the attribute_list to filter_dict only if it is populated and
         # validation is successful
         if events_list:
-            filter_dict[api_c.EVENT] = events_list
+            filter_dict[db_c.EVENT] = events_list
             attribute_list.append(db_c.EVENT)
 
         if attribute_list:
