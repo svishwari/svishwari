5bfc6ac0e80abeb29bef154360016325b0f99221
skondalraj-deloitte <skondalraj@deloitte.com>
2022-06-22 16:48:13 -0500

    HUS-3244: Black formatting

--- hux/api/huxunify/api/constants.py
+++ hux/api/huxunify/api/constants.py
@@ -672,8 +672,12 @@ BSON_INVALID_ID = (
     f"12-byte input or a 24-character hex string"
 )
 MESSAGE = "message"
-FAILED_DEPENDENCY_CONNECTION_ERROR_MESSAGE = "Failed connecting to dependent API."
-FAILED_DEPENDENCY_ERROR_MESSAGE = "Failed to obtain data from dependent API endpoint."
+FAILED_DEPENDENCY_CONNECTION_ERROR_MESSAGE = (
+    "Failed connecting to dependent API."
+)
+FAILED_DEPENDENCY_ERROR_MESSAGE = (
+    "Failed to obtain data from dependent API endpoint."
+)
 EMPTY_RESPONSE_DEPENDENCY_ERROR_MESSAGE = (
     "Returned empty object from dependent API endpoint."
 )
@@ -681,8 +685,12 @@ EMPTY_RESPONSE_DEPENDENCY_ERROR_MESSAGE = (
 EMPTY_OBJECT_ERROR_MESSAGE = "Data not provided."
 DUPLICATE_NAME = "Name already exists."
 SFMC_CONFIGURATION_MISSING = "SFMC data extension config object missing."
-PERFORMANCE_METRIC_DE_NOT_ASSIGNED = "Performance metrics data extension not assigned."
-CAMPAIGN_ACTIVITY_DE_NOT_ASSIGNED = "Campaign activity data extension not assigned."
+PERFORMANCE_METRIC_DE_NOT_ASSIGNED = (
+    "Performance metrics data extension not assigned."
+)
+CAMPAIGN_ACTIVITY_DE_NOT_ASSIGNED = (
+    "Campaign activity data extension not assigned."
+)
 SAME_PERFORMANCE_CAMPAIGN_ERROR = (
     "Performance metric and Campaign activity cannot be same"
 )
@@ -700,7 +708,9 @@ DESTINATION_NOT_SUPPORTED = "Destination is not supported."
 SUCCESSFUL_DELIVERY_JOB_NOT_FOUND = "No successful delivery job found"
 ZERO_AUDIENCE_SIZE = "Sum of Audience(s) is zero"
 ENGAGEMENT_NO_AUDIENCES = "Engagement has no audiences."
-AUDIENCE_NOT_ATTACHED_TO_ENGAGEMENT = "Audience not attached to the engagement."
+AUDIENCE_NOT_ATTACHED_TO_ENGAGEMENT = (
+    "Audience not attached to the engagement."
+)
 DESTINATION_NOT_ATTACHED_ENGAGEMENT_AUDIENCE = (
     "Destination not attached to the engagement audience."
 )
@@ -1160,7 +1170,9 @@ CUSTOMER_OVERVIEW_DEFAULT_FILTER = {
     "filters": [
         {
             "section_aggregator": "ALL",
-            "section_filters": [{"field": "country", "type": "equals", "value": "US"}],
+            "section_filters": [
+                {"field": "country", "type": "equals", "value": "US"}
+            ],
         }
     ]
 }
@@ -1299,7 +1311,8 @@ MODELS_STUB = [
         CATEGORY: "Email",
         TYPE: "Classification",
         NAME: "Propensity to Click",
-        DESCRIPTION: "Propensity for a customer to click " "on a link in an email.",
+        DESCRIPTION: "Propensity for a customer to click "
+        "on a link in an email.",
         ID: "aa789e0bd7edaad4c36bec4a3682f02d36441fe1",
         STATUS: STATUS_PENDING,
     },
@@ -1307,7 +1320,8 @@ MODELS_STUB = [
         CATEGORY: "Email",
         TYPE: "Unknown",
         NAME: "Email Content Optimization",
-        DESCRIPTION: "Alter email content to optimize " "email campaign performance.",
+        DESCRIPTION: "Alter email content to optimize "
+        "email campaign performance.",
         ID: "99e45e0bd7edaad4c36bec4a3682f02d36441fe1",
         STATUS: STATUS_PENDING,
     },
@@ -1369,7 +1383,8 @@ MODELS_STUB = [
         CATEGORY: "Sales forecasting",
         TYPE: "Regression",
         NAME: "Predicted Sales Per Customer",
-        DESCRIPTION: "Predicting sales for a customer over a " "defined time range.",
+        DESCRIPTION: "Predicting sales for a customer over a "
+        "defined time range.",
         ID: "bba67e0bd7edaad4c36bec4a3682f02d36441fe1",
         STATUS: STATUS_PENDING,
     },
@@ -1377,7 +1392,8 @@ MODELS_STUB = [
         CATEGORY: "Sales forecasting",
         TYPE: "Regression",
         NAME: "Predicted Sales Per Store",
-        DESCRIPTION: "Predicting sales for a store over a " "defined time range.",
+        DESCRIPTION: "Predicting sales for a store over a "
+        "defined time range.",
         ID: "a45b7e0bd7edaad4c36bec4a3682f02d36441fe1",
         STATUS: STATUS_PENDING,
     },
@@ -1631,7 +1647,8 @@ MODEL_PIPELINE_PERFORMANCE_STUB = {
         "run_duration": [
             {
                 "status": "Success",
-                "timestamp": datetime.datetime.now() - datetime.timedelta(days=x),
+                "timestamp": datetime.datetime.now()
+                - datetime.timedelta(days=x),
                 "duration": "12m 41s",
                 "label": f"{x} run of last 10",
             }
@@ -1646,7 +1663,8 @@ MODEL_PIPELINE_PERFORMANCE_STUB = {
         "run_duration": [
             {
                 "status": "Success",
-                "timestamp": datetime.datetime.now() - datetime.timedelta(days=x),
+                "timestamp": datetime.datetime.now()
+                - datetime.timedelta(days=x),
                 "duration": "12m 41s",
                 "label": f"{x} run of last 10",
             }
@@ -2046,10 +2064,14 @@ MODEL_NAME_TAGS_MAP = {
     "Propensity to view shirt products": dict(industry=[RETAIL]),
     "Propensity to view women’s products": dict(industry=[RETAIL]),
     "Propensity to view wool products": dict(industry=[RETAIL]),
-    "Product Portfolio Marketing": dict(industry=[RETAIL, HOSPITALITY, AUTOMOTIVE]),
+    "Product Portfolio Marketing": dict(
+        industry=[RETAIL, HOSPITALITY, AUTOMOTIVE]
+    ),
     "Product Recommendation": dict(industry=[RETAIL, HOSPITALITY, AUTOMOTIVE]),
     "Product Return": dict(industry=[RETAIL, AUTOMOTIVE]),
-    "Promotion Attributable Analysis": dict(industry=[RETAIL, HOSPITALITY, AUTOMOTIVE]),
+    "Promotion Attributable Analysis": dict(
+        industry=[RETAIL, HOSPITALITY, AUTOMOTIVE]
+    ),
     "Sales Forecasting model": dict(industry=ALL_INDUSTRY_TYPES),
     "Uplift Model": dict(industry=ALL_INDUSTRY_TYPES),
 }
