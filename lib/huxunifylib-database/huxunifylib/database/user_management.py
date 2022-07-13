bce3c1167c1582382f82ecad4e72e8cf0e2105eb
skondalraj-deloitte <skondalraj@deloitte.com>
2022-07-13 01:47:10 -0500

    HUS-3371: Black formatting

--- lib/huxunifylib-database/huxunifylib/database/user_management.py
+++ lib/huxunifylib-database/huxunifylib/database/user_management.py
@@ -369,7 +369,9 @@ def delete_favorite_from_all_users(
     return False
 
 
-def update_all_users(database: DatabaseClient, update_doc: dict) -> Union[dict, None]:
+def update_all_users(
+    database: DatabaseClient, update_doc: dict
+) -> Union[dict, None]:
     """Function to update all users.
 
     Args:
@@ -658,7 +660,9 @@ def add_applications_to_users(
     return None
 
 
-def get_user_applications(database: DatabaseClient, okta_id: str) -> Union[list, None]:
+def get_user_applications(
+    database: DatabaseClient, okta_id: str
+) -> Union[list, None]:
     """A function to fetch user applications.
 
     Args:
@@ -787,7 +791,9 @@ def remove_user_trust_id_segments(
         return collection.find_one_and_update(
             {db_c.OKTA_ID: okta_id},
             {
-                "$pull": {db_c.TRUST_ID_SEGMENTS: {db_c.SEGMENT_NAME: segment_name}},
+                "$pull": {
+                    db_c.TRUST_ID_SEGMENTS: {db_c.SEGMENT_NAME: segment_name}
+                },
                 "$set": {
                     db_c.UPDATE_TIME: datetime.datetime.utcnow(),
                 },
@@ -816,7 +822,9 @@ def get_user_trust_id_segments(
 
     try:
         return list(
-            collection.find_one({db_c.OKTA_ID: okta_id}).get(db_c.TRUST_ID_SEGMENTS, [])
+            collection.find_one({db_c.OKTA_ID: okta_id}).get(
+                db_c.TRUST_ID_SEGMENTS, []
+            )
         )
     except pymongo.errors.OperationFailure as exc:
         logging.error(exc)
