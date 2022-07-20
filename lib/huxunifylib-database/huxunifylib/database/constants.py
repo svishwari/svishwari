339e51f40199e2e41f93fe83ea76d04437760e7b
sairajput-deloitte <61450311+sairajput-deloitte@users.noreply.github.com>
2022-07-20 14:32:34 +0530

    Added trigger for ORCH integration tests on KEDA

--- lib/huxunifylib-database/huxunifylib/database/constants.py
+++ lib/huxunifylib-database/huxunifylib/database/constants.py
@@ -36,6 +36,9 @@ MAX = "max"
 LINK = "link"
 USERNAME = "username"
 
+# ORCH integration test User
+ORCH_INTEGRATION_TEST_USER = "orch_integration_test_user"
+
 # Data Management Defines
 DATA_MANAGEMENT_DATABASE = "data_management"
 CONVERSIONS_DATABASE = "conversions"
