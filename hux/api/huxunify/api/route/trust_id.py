6ef3ea13c4ab76292038c4b6c8a3e2ed909eac20
84175319+soumbehera-deloitte@users.noreply.github.com <soumbehera@deloitte.com>
2022-07-21 20:31:48 +0530

    Uncomment api error handler

--- hux/api/huxunify/api/route/trust_id.py
+++ hux/api/huxunify/api/route/trust_id.py
@@ -140,7 +140,7 @@ class TrustIdAttributes(SwaggerView):
     responses.update(AUTH401_RESPONSE)
     tags = [api_c.TRUST_ID_TAG]
 
-    # @api_error_handler()
+    @api_error_handler()
     @requires_access_levels(api_c.TRUST_ID_ROLE_ALL)
     def get(self, user: dict) -> Tuple[list, int]:
         """Retrieves Trust ID trust_id_attributes data.
