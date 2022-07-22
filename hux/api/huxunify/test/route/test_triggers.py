32630b9c5106ae6b172793d1e04b9ef46ee54785
sairajput-deloitte <61450311+sairajput-deloitte@users.noreply.github.com>
2022-07-22 17:03:39 +0530

    Update test_triggers.py

--- hux/api/huxunify/test/route/test_triggers.py
+++ hux/api/huxunify/test/route/test_triggers.py
@@ -50,10 +50,13 @@ class TestTriggersRoute(TestCase):
 
         self.assertEqual(HTTPStatus.OK, response.status_code)
         self.assertDictEqual(
-            {api_c.DELIVERIES: {api_c.PENDING_JOBS: 0}}, response.json
-        )
-        self.assertDictEqual(
-            {api_c.DELIVERIES: {api_c.ORCH_INTEGRATION_TEST: 0}}, response.json
+            {
+                api_c.DELIVERIES: {
+                    api_c.PENDING_JOBS: 0,
+                    api_c.ORCH_INTEGRATION_TEST: 0,
+                }
+            },
+            response.json,
         )
 
     def test_delivery_pending_jobs(self):
@@ -73,9 +76,11 @@ class TestTriggersRoute(TestCase):
 
             self.assertEqual(HTTPStatus.OK, response.status_code)
             self.assertDictEqual(
-                {api_c.DELIVERIES: {api_c.PENDING_JOBS: 58}}, response.json
-            )
-            self.assertDictEqual(
-                {api_c.DELIVERIES: {api_c.ORCH_INTEGRATION_TEST: 58}},
+                {
+                    api_c.DELIVERIES: {
+                        api_c.PENDING_JOBS: 58,
+                        api_c.ORCH_INTEGRATION_TEST: 58,
+                    }
+                },
                 response.json,
             )
