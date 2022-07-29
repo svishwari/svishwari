b792be15ea387b7dd98b9bef328a61ed80937b41
svishwari <svishwari@deloitte.com>
2022-07-29 15:22:40 +0530

    Bug Resolve

--- hux/frontend/src/views/HXTrustId/Overview.vue
+++ hux/frontend/src/views/HXTrustId/Overview.vue
@@ -198,7 +198,7 @@ export default {
 <style lang="scss" scoped>
 .v-application {
   .overview-card {
-    width: 98.4%;
+    width: 100%;
     border-radius: 12px !important;
     ::v-deep {
       .text-h3 {
